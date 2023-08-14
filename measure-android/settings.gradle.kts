e86f19c87d5aef249d7c06e57b8684148a1be3c4
Abhay Sood <soodabhay23@gmail.com>
2023-08-14 15:07:13 +0530

    Initialize Android SDK

---
+++ measure-android/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "measure-android"
+include(":sample")
+include(":measure")
