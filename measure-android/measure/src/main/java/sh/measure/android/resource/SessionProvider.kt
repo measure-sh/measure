da949931ef903f9e938e6d5e0e85f4b5ed3d72fc
Abhay Sood <soodabhay23@gmail.com>
2023-09-05 11:25:31 +0530

    chore: improve kdoc

--- measure-android/measure/src/main/java/sh/measure/android/resource/SessionProvider.kt
+++ measure-android/measure/src/main/java/sh/measure/android/resource/SessionProvider.kt
@@ -3,7 +3,7 @@ package sh.measure.android.resource
 import sh.measure.android.id.IdProvider
 
 /**
- * Manages the session ID. The session ID is immutable and generated only once.
+ * Manages the session ID. The session ID is created lazily, and is immutable.
  */
 internal class SessionProvider(private val idProvider: IdProvider) {
     val sessionId by lazy { createSession() }
