643ff43a6188693ad0d48b5c3202726dd30fae77
Anup Cowkur <anupcowkur89@gmail.com>
2023-09-21 15:50:27 +0530

    chore: remove unused imports

--- measure-web-app/app/layout.tsx
+++ measure-web-app/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Abril_Fatface, Arvo, Averia_Sans_Libre, Comfortaa, Courier_Prime, Fira_Code, Fira_Mono, Fira_Sans, Fira_Sans_Condensed, Fragment_Mono, Gudea, Halant, Hanken_Grotesk, Hind, IBM_Plex_Mono, Inconsolata, Inter, Josefin_Sans, Josefin_Slab, Kanit, Karla, Khula, Lato, Lekton, Lora, Maitree, Mandali, Montserrat, Noto_Sans_Mono, Nunito_Sans, Open_Sans, Oswald, Palanquin, Poppins, Quattrocento, Quattrocento_Sans, Raleway, Red_Hat_Mono, Roboto_Condensed, Roboto_Mono, Sono, Source_Code_Pro, Source_Sans_3, Space_Grotesk, Space_Mono, Work_Sans } from 'next/font/google'
-import localFont from 'next/font/local'
+import {Josefin_Sans, Space_Mono } from 'next/font/google'
 
 const display = Josefin_Sans({ subsets: ['latin'], display: 'swap', weight:['100','200', '300', '400', '500', '600', '700'],
 variable: '--font-display'})
