6a31e4b964da8e2657be2380f6239f5891a9b99d
Anup Cowkur <anupcowkur89@gmail.com>
2023-09-21 14:21:32 +0530

    fix: use hover only when supported
    fixes #24

--- measure-web-app/tailwind.config.ts
+++ measure-web-app/tailwind.config.ts
@@ -6,6 +6,9 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {
       fontFamily: {
