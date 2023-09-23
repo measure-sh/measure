768e8d94ad9589930fd6e79d14a3b5c5b2d438db
Anup Cowkur <anupcowkur89@gmail.com>
2023-09-23 20:42:19 +0530

    chore: Improve landing page copy
    closes #29

--- measure-web-app/app/page.tsx
+++ measure-web-app/app/page.tsx
@@ -9,15 +9,15 @@ export default function Home() {
         <div className="py-24"/>
         <p className="font-display font-regular text-black text-8xl max-w-6xl text-center">measure</p>
         <div className="py-2"/>
-        <p className="text-lg leading-relaxed font-sans text-black max-w-2xl text-center">open source monitoring platform for mobile teams</p>
+        <p className="text-lg leading-relaxed font-sans text-black max-w-2xl text-center">open source app monitoring for mobile teams</p>
         <div className="py-4 md:py-12"/>
         <div className="border border-black aspect-square w-96 md:w-full bg-yellow-200"/>
         <div className="py-12 md:py-16"/>
         <div className="flex flex-col md:flex-row md:w-full items-center">
           <div className="flex flex-col md:w-3/6 items-center md:items-start">
-            <p className="text-4xl font-display font-regular text-black">Measure what matters</p>
+            <p className="text-4xl font-display font-regular text-black">App health at a glance</p>
             <div className="py-2"/>
-            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Monitor important release metrics and core user flows to stay on top of app health. Filter by various system or custom attributes for detailed segmentation.</p>
+            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Monitor core user flows and important metrics to stay on top of app health. Filter by various system or custom attributes to dive deeper.</p>
           </div>
           <div className="py-4 md:py-0 md:w-1/6"/>
           <div className="border border-black w-96 md:w-2/6 aspect-square bg-violet-200"/>
@@ -25,9 +25,9 @@ export default function Home() {
         <div className="py-12"/>
         <div className="flex flex-col md:flex-row md:w-full items-center">
           <div className="flex flex-col md:w-3/6 items-center md:items-start">
-            <p className="text-4xl font-display font-regular text-black">Crashes</p>
+            <p className="text-4xl font-display font-regular text-black">Crash debugging simplified</p>
             <div className="py-2"/>
-            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Track crashes, prioritise them by impact and get clean, de-obfuscated stack traces for precise debugging.</p>
+            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Track crashes and app hangs automatically, prioritise them by impact and use detailed event timelines to zoom in on production issues.</p>
           </div>
           <div className="py-4 md:py-0 md:w-1/6"/>
           <div className="border border-black w-96 md:w-2/6 aspect-square bg-pink-200"/>
@@ -35,19 +35,9 @@ export default function Home() {
         <div className="py-12"/>
         <div className="flex flex-col md:flex-row md:w-full items-center">
           <div className="flex flex-col md:w-3/6 items-center md:items-start">
-            <p className="text-4xl font-display font-regular text-black">ANRs & App hangs</p>
+            <p className="text-4xl font-regular text-black font-display">Buttery smooth performance </p>
             <div className="py-2"/>
-            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Capture detailed system info to investigate annoying app hangs. Export captured trace data to your favorite analyzer to dig in further. </p>
-          </div>
-          <div className="py-4 md:py-0 md:w-1/6"/>
-          <div className="border border-black w-96 md:w-2/6 aspect-square bg-orange-200"/>
-        </div>
-        <div className="py-12"/>
-        <div className="flex flex-col md:flex-row md:w-full items-center">
-          <div className="flex flex-col md:w-3/6 items-center md:items-start">
-            <p className="text-4xl font-regular text-black font-display">Performance</p>
-            <div className="py-2"/>
-            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Automatically trace app startup, network calls, database queries and slow page loads. Use custom traces to measure any part of your app.</p>
+            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Automatically trace app startups, app hangs, network calls, database queries and slow page loads. Use custom traces to measure what matters in any part of your app.</p>
           </div>
           <div className="py-4 md:py-0 md:w-1/6"/>
           <div className="border border-black w-96 md:w-2/6 aspect-square bg-blue-200"/>
@@ -55,9 +45,9 @@ export default function Home() {
         <div className="py-12"/>
         <div className="flex flex-col md:flex-row md:w-full items-center">
           <div className="flex flex-col md:w-3/6 items-center md:items-start">
-            <p className="text-4xl font-display font-regular text-black ">Logs</p>
+            <p className="text-4xl font-display font-regular text-black ">Detailed logging</p>
             <div className="py-2"/>
-            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Capture standard output/Logcat output automatically. Add custom logs anywhere in your code to effectively diagnose issues in production.</p>
+            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Capture standard output/Logcat output automatically. Add custom logs anywhere in your code for easy debugging in production.</p>
           </div>
           <div className="py-4 md:py-0 md:w-1/6"/>
           <div className="border border-black w-96 md:w-2/6 aspect-square bg-green-200"/>
@@ -65,29 +55,9 @@ export default function Home() {
         <div className="py-12"/>
         <div className="flex flex-col md:flex-row md:w-full items-center">
           <div className="flex flex-col md:w-3/6 items-center md:items-start">
-            <p className="text-4xl font-display font-regular text-black">Session replays</p>
+            <p className="text-4xl font-display font-regular text-black">Built by and for mobile devs</p>
             <div className="py-2"/>
-            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Get play-by-play event timelines with user gestures, network calls, logs, navigation events, screenshots and custom events to root cause production issues quickly.</p>
-          </div>
-          <div className="py-4 md:py-0 md:w-1/6"/>
-          <div className="border border-black w-96 md:w-2/6 aspect-square bg-purple-200"/>
-        </div>
-        <div className="py-12"/>
-        <div className="flex flex-col md:flex-row md:w-full items-center">
-          <div className="flex flex-col md:w-3/6 items-center md:items-start">
-            <p className="text-4xl font-display font-regular text-black">AI assistance</p>
-            <div className="py-2"/>
-            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Get AI guidance with full context of your code and platform to help you resolve production bugs. Spend less time on StackOverflow and more time building features.</p>
-          </div>
-          <div className="py-4 md:py-0 md:w-1/6"/>
-          <div className="border border-black w-96 md:w-2/6 aspect-square bg-rose-200"/>
-        </div>
-        <div className="py-12"/>
-        <div className="flex flex-col md:flex-row md:w-full items-center">
-          <div className="flex flex-col md:w-3/6 items-center md:items-start">
-            <p className="text-4xl font-display font-regular text-black">Open source</p>
-            <div className="py-2"/>
-            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Open roadmap. Contributions, issues and pull requests welcome. Standard Apache 2.0 license. No weird clauses. No funny business. No rug pulls.</p>
+            <p className="text-lg text-center md:text-left leading-relaxed font-sans text-black">Open source platform with a welcoming community. Built by mobile devs who have shipped apps to hundreds of millions of users since the early days of iOS and Android.</p>
           </div>
           <div className="py-4 md:py-0 md:w-1/6"/>
           <div className="border border-black w-96 md:w-2/6 aspect-square bg-indigo-200"/>
@@ -124,7 +94,7 @@ export default function Home() {
         <div className="py-12 md:py-24"/>
         <p className="font-display font-regular text-black text-6xl max-w-4xl text-center">Early access</p>
         <div className="py-2"/>
-        <p className="font-sans text-black text-xl leading-relaxed max-w-4xl text-center">Monitoring mobile apps in production doesn&apos;t have to be hard. Sign up to get early access to measure!</p>
+        <p className="font-sans text-black text-xl leading-relaxed max-w-4xl text-center">We are building measure for mobile devs like you. Help us make it the best tool for the job!</p>
         <div className="py-4"/>
         <EmailWaitlist/>
         <div className="py-24"/>
