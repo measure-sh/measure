4584b04e2dcd6e49b94b3d90b15a2ef3d48bc4d6
Anup Cowkur <anupcowkur89@gmail.com>
2023-09-28 16:02:51 +0530

    fix(webapp): set header z-index so it's always on top

--- measure-web-app/app/components/header.tsx
+++ measure-web-app/app/components/header.tsx
@@ -44,7 +44,7 @@ export default function Header() {
   const router = useRouter();
 
   return (
-    <header onFocus={()=>setIsFocused(true)} onBlur={()=>setIsFocused(false)} className={`w-full flex flex-col bg-white fixed top-0 transition-transform duration-100 ease-in-out ${scrollDir === 'scrolling down' && isFocused === false ? '-translate-y-full' : 'translate-y-0'}`}>
+    <header onFocus={()=>setIsFocused(true)} onBlur={()=>setIsFocused(false)} className={`w-full flex flex-col z-50 bg-white fixed top-0 transition-transform duration-100 ease-in-out ${scrollDir === 'scrolling down' && isFocused === false ? '-translate-y-full' : 'translate-y-0'}`}>
       <div className="w-full flex flex-col md:flex-row space-between items-center py-4 px-4">
         <button className="outline-none border border-black rounded-full hover:bg-yellow-200 focus-visible:bg-yellow-200 active:bg-yellow-300 font-display text-black font-display transition-colors duration-100 py-1 px-2.5" onClick={() => router.push('/')}>m</button>
         <div className="py-2 md:py-0 md:flex md:grow"/>
