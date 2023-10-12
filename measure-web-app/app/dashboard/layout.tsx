758b3dfffb514c71cb151a30e8b0bf01de3a22d3
Anup Cowkur <anupcowkur89@gmail.com>
2023-10-12 13:45:17 +0530

    chore(webapp): change side nav selected button color to neutral-950

--- measure-web-app/app/dashboard/layout.tsx
+++ measure-web-app/app/dashboard/layout.tsx
@@ -42,7 +42,7 @@ export default function DashboardLayout({
                     <ul>
                         {menuItems.map(({ href, title }) => (
                             <li key={title}>
-                                <Link href={href} className={`m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200  border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4 ${pathName === href && 'bg-yellow-100'}`}>{title}</Link>
+                                <Link href={href} className={`m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4 ${pathName === href && 'bg-neutral-950 text-white hover:text-black'}`}>{title}</Link>
                             </li>
                         ))}
                     </ul>
