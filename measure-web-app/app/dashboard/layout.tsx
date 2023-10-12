9cbcbc3bfc442c63824f34de9a0eaca0e879b40f
Anup Cowkur <anupcowkur89@gmail.com>
2023-10-12 16:17:00 +0530

    fix(webapp): change text to black on side nav button on focus visible

--- measure-web-app/app/dashboard/layout.tsx
+++ measure-web-app/app/dashboard/layout.tsx
@@ -46,7 +46,7 @@ export default function DashboardLayout({
                     <ul>
                         {menuItems.map(({ href, title }) => (
                             <li key={title}>
-                                <Link href={href} className={`m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4 ${pathName === href && 'bg-neutral-950 text-white hover:text-black'}`}>{title}</Link>
+                                <Link href={href} className={`m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4 ${pathName === href && 'bg-neutral-950 text-white focus-visible:text-black hover:text-black'}`}>{title}</Link>
                             </li>
                         ))}
                     </ul>
