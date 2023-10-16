cd433654e6c2bfacdd3975dffc83cf744df48975
Anup Cowkur <anupcowkur89@gmail.com>
2023-10-16 15:26:06 +0530

    chore(webapp): add info circles to overview

---
+++ measure-web-app/app/components/info_circle_exception_rate.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+interface InfoCircleExceptionRateExceptionRateProps {
+  value: number;
+  delta: number;
+  title: string;
+}
+
+const InfoCircleExceptionRate = ({ value, delta, title }: InfoCircleExceptionRateExceptionRateProps) => {
+    return (
+        <div className="flex flex-col items-center">
+            <div className={`flex flex-col items-center justify-center w-64 aspect-square rounded-full border border-4 ${value > 95? 'border-green-400': value > 85? 'border-yellow-400': 'border-red-400'}`}>
+                <p className="text-black font-sans text-xl">{value}%</p>
+                <p className={`font-sans text-xl ${delta > 0? 'text-green-600': delta < 0? 'text-red-400': 'opacity-0'}`}>{delta>0? '+':''}{delta}%</p>
+            </div>
+            <div className="py-2"/>
+            <p className="text-black font-display text-lg">{title}</p>
+        </div>
+  );
+};
+
+export default InfoCircleExceptionRate;
--- measure-web-app/app/dashboard/overview/page.tsx
+++ measure-web-app/app/dashboard/overview/page.tsx
@@ -1,4 +1,7 @@
 import Dropdown from "@/app/components/dropdown";
+import InfoCircleAppAdoption from "@/app/components/info_circle_app_adoption";
+import InfoCircleAppStartTime from "@/app/components/info_circle_app_start_time";
+import InfoCircleExceptionRate from "@/app/components/info_circle_exception_rate";
 
 export default function Overview() {
   const today = new Date();
@@ -7,24 +10,31 @@ export default function Overview() {
   const sevenDaysAgo = new Date(today.setDate(today.getDate() - 7));
   const startDate = `${sevenDaysAgo.getFullYear()}-${(sevenDaysAgo.getMonth()+1).toString().padStart(2, '0')}-${sevenDaysAgo.getDate().toString().padStart(2, '0')}`;
 
-
   return (
-    <main className="flex flex-col selection:bg-yellow-200/75">
-      <div className="flex flex-col items-start w-full px-16">
-        <div className="py-4"/>
-        <p className="font-display font-regular text-black text-4xl max-w-6xl text-center">Overview</p>
-        <div className="py-4"/>
-        <div className="flex flex-wrap gap-x-16 gap-y-4 items-center">
-          <Dropdown items={['Readly prod', 'Readly alpha','Readly debug']}/>
-          <div className="flex flex-row items-center">
-            <input type="date" value={startDate} className="font-display text-black border border-black rounded-md p-2"/>
-            <p className="text-black font-display px-2">to</p>
-            <input type="date" value={endDate} className="font-display text-black border border-black rounded-md p-2"/>
-          </div>
-          <Dropdown items={['Version 13.2.1', 'Version 13.2.2','Version 13.3.7']}/>
+    <main className="flex flex-col selection:bg-yellow-200/75 items-start p-16">
+      <div className="py-4"/>
+      <p className="font-display font-regular text-black text-4xl max-w-6xl text-center">Overview</p>
+      <div className="py-4"/>
+      <div className="flex flex-wrap gap-x-16 gap-y-4 items-center">
+        <Dropdown items={['Readly prod', 'Readly alpha','Readly debug']}/>
+        <div className="flex flex-row items-center">
+          <input type="date" value={startDate} className="font-display text-black border border-black rounded-md p-2"/>
+          <p className="text-black font-display px-2">to</p>
+          <input type="date" value={endDate} className="font-display text-black border border-black rounded-md p-2"/>
         </div>
-        <div className="py-24"/>
-      </div>  
+        <Dropdown items={['Version 13.2.1', 'Version 13.2.2','Version 13.3.7']}/>
+      </div>
+      <div className="py-8"/>
+      <div className="border border-black w-full h-96"/>
+      <div className="py-8"/>
+      <div className="flex flex-wrap gap-x-32 gap-y-16 items-center">
+        <InfoCircleAppAdoption title="App adoption" value={81}/>
+        <InfoCircleExceptionRate title="Crash free users" value={98.5} delta={0.73}/>
+        <InfoCircleExceptionRate title="Perceived crash free rate" value={91.3} delta={-0.51}/>
+        <InfoCircleExceptionRate title="ANR free users" value={98.5} delta={0.73}/>
+        <InfoCircleExceptionRate title="Perceived ANR free rate" value={91.3} delta={0.27}/>
+        <InfoCircleAppStartTime title="App start time" value={700} delta={-200}/>
+      </div>
     </main>
   )
 }
