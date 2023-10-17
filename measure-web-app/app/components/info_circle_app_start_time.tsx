cb82c80cdf722c7df48efdbb7efbb31e6ea93ae4
Anup Cowkur <anupcowkur89@gmail.com>
2023-10-17 14:21:15 +0530

    chore(webapp): reduce text size for delta value in info circles

--- measure-web-app/app/components/info_circle_app_start_time.tsx
+++ measure-web-app/app/components/info_circle_app_start_time.tsx
@@ -12,7 +12,8 @@ const InfoCircleAppStartTime = ({ value, delta, title }: InfoCircleAppStartTimeP
         <div className="flex flex-col items-center">
             <div className={`flex flex-col items-center justify-center w-64 aspect-square rounded-full border border-4 ${value < 800? 'border-green-400': value < 1200? 'border-yellow-400': 'border-red-400'}`}>
                 <p className="text-black font-sans text-xl">{value}ms</p>
-                <p className={`font-sans text-xl ${delta < 0? 'text-green-600': delta > 0? 'text-red-400': 'opacity-0'}`}>{delta}ms</p>
+                <div className="py-1"/>
+                <p className={`font-sans text-sm ${delta < 0? 'text-green-600': delta > 0? 'text-red-400': 'opacity-0'}`}>{delta>0? '+':''}{delta}ms</p>
             </div>
             <div className="py-2"/>
             <p className="text-black font-display text-lg">{title}</p>
