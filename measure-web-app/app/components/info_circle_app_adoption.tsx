7ae0cda6bb97f448a6f39d45b4ff9573159ad76f
Anup Cowkur <anupcowkur89@gmail.com>
2023-10-18 15:43:13 +0530

    chore(webapp): make tooltip show up only on hover over info circle
    Tooltips were showing up when users hovered over either the circle
    or the circle title. This commit fixes it so that only hovering over
    the circle itself shows the tooltip.

--- measure-web-app/app/components/info_circle_app_adoption.tsx
+++ measure-web-app/app/components/info_circle_app_adoption.tsx
@@ -12,20 +12,18 @@ const formatter = Intl.NumberFormat('en', { notation: 'compact' });
 
 const InfoCircleAppAdoption = ({ value, users, totalUsers, title }: InfoCircleAppAdoptionProps) => {
     return (
-      <div className="group relative">
         <div className="flex flex-col items-center">
-            <div className={`flex flex-col items-center justify-center w-64 aspect-square rounded-full border border-black border-4 hover:bg-neutral-800/25 transition-colors duration-100`}>
+            <div className={`flex flex-col group relative items-center justify-center w-64 aspect-square rounded-full border border-black border-4 hover:bg-neutral-800/25 transition-colors duration-100`}>
                 <p className="text-black font-sans text-xl">{value}%</p>
                 <div className="py-1"/>
                 <p className="text-black font-sans text-sm">{formatter.format(users)}/{formatter.format(totalUsers)} users</p>
+                <span className="pointer-events-none z-50 max-w-xl absolute font-sans text-sm text-white rounded-md p-4 bg-neutral-800 -top-28 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100">
+                  Adoption =  (Users of selected app version in selected time period / Users of all app versions in selected time period) * 100
+                </span>
             </div>
             <div className="py-2"/>
             <p className="text-black font-display text-lg">{title}</p>
         </div>
-        <span className="pointer-events-none z-50 max-w-xl absolute font-sans text-sm text-white rounded-md p-4 bg-neutral-800 -top-24 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100">
-          Adoption =  (Users of selected app version in selected time period / Users of all app versions in selected time period) * 100
-        </span>
-      </div>
   );
 };
 
--- measure-web-app/app/components/info_circle_app_start_time.tsx
+++ measure-web-app/app/components/info_circle_app_start_time.tsx
@@ -9,19 +9,17 @@ interface InfoCircleAppStartTimeProps {
 
 const InfoCircleAppStartTime = ({ value, delta, title }: InfoCircleAppStartTimeProps) => {
     return (
-      <div className="group relative">
-        <div className="flex flex-col items-center">
-            <div className={`flex flex-col items-center justify-center w-64 aspect-square rounded-full border border-4 transition-colors duration-100 ${value < 800? 'border-green-400 hover:bg-green-400/25': value < 1200? 'border-yellow-400 hover:bg-yellow-400/25': 'border-red-400 hover:bg-red-400/25'}`}>
-                <p className="text-black font-sans text-xl">{value}ms</p>
-                <div className="py-1"/>
-                <p className={`font-sans text-sm ${delta < 0? 'text-green-600': delta > 0? 'text-red-400': 'opacity-0'}`}>{delta>0? '+':''}{delta}ms</p>
-            </div>
-            <div className="py-2"/>
-            <p className="text-black font-display text-lg">{title}</p>
-        </div>
-        <span className="pointer-events-none z-50 max-w-xl absolute font-sans text-sm text-white rounded-md p-4 bg-neutral-800 -top-40 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100">
-          App start time = p95 Cold launch time of selected app version in selected time period<br/><br/>Delta value = p95 Cold launch time of selected app version in selected time period - p95 Cold launch time of all app versions in selected time period
-        </span>
+      <div className="flex flex-col items-center">
+          <div className={`flex flex-col group relative items-center justify-center w-64 aspect-square rounded-full border border-4 transition-colors duration-100 ${value < 800? 'border-green-400 hover:bg-green-400/25': value < 1200? 'border-yellow-400 hover:bg-yellow-400/25': 'border-red-400 hover:bg-red-400/25'}`}>
+              <p className="text-black font-sans text-xl">{value}ms</p>
+              <div className="py-1"/>
+              <p className={`font-sans text-sm ${delta < 0? 'text-green-600': delta > 0? 'text-red-400': 'opacity-0'}`}>{delta>0? '+':''}{delta}ms</p>
+              <span className="pointer-events-none z-50 max-w-xl absolute font-sans text-sm text-white rounded-md p-4 bg-neutral-800 -top-44 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100">
+                App start time = p95 Cold launch time of selected app version in selected time period<br/><br/>Delta value = p95 Cold launch time of selected app version in selected time period - p95 Cold launch time of all app versions in selected time period
+              </span>
+          </div>
+          <div className="py-2"/>
+          <p className="text-black font-display text-lg">{title}</p>
       </div>
   );
 };
