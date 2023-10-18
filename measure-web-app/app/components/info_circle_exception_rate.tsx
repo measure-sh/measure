74741d2bec4ce7ccada6bd59cdb165b6b5740f8c
Anup Cowkur <anupcowkur89@gmail.com>
2023-10-18 15:43:13 +0530

    chore(webapp): make tooltip show up only on hover over info circle
    Tooltips were showing up when users hovered over either the circle
    or the circle title. This commit fixes it so that only hovering over
    the circle itself shows the tooltip.

--- measure-web-app/app/components/info_circle_exception_rate.tsx
+++ measure-web-app/app/components/info_circle_exception_rate.tsx
@@ -11,17 +11,15 @@ interface InfoCircleExceptionRateExceptionRateProps {
 
 const InfoCircleExceptionRate = ({ value, delta, title, tooltipMsgLine1, tooltipMsgLine2 }: InfoCircleExceptionRateExceptionRateProps) => {
     return (
-      <div className="group relative">
-        <div className="flex flex-col items-center">
-            <div className={`flex flex-col items-center justify-center w-64 aspect-square rounded-full border border-4 transition-colors duration-100 ${value > 95? 'border-green-400 hover:bg-green-400/25': value > 85? 'border-yellow-400 hover:bg-yellow-400/25': 'border-red-400 hover:bg-red-400/25'}`}>
-                <p className="text-black font-sans text-xl">{value}%</p>
-                <div className="py-1"/>
-                <p className={`font-sans text-sm ${delta > 0? 'text-green-600': delta < 0? 'text-red-400': 'opacity-0'}`}>{delta>0? '+':''}{delta}%</p>
-            </div>
-            <div className="py-2"/>
-            <p className="text-black font-display text-lg">{title}</p>
-        </div>
-        <span className="pointer-events-none z-50 max-w-xl absolute font-sans text-sm text-white rounded-md p-4 bg-neutral-800 -top-44 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100">{tooltipMsgLine1}<br/><br/>{tooltipMsgLine2}</span>
+      <div className="flex flex-col items-center">
+          <div className={`flex flex-col group relative items-center justify-center w-64 aspect-square rounded-full border border-4 transition-colors duration-100 ${value > 95? 'border-green-400 hover:bg-green-400/25': value > 85? 'border-yellow-400 hover:bg-yellow-400/25': 'border-red-400 hover:bg-red-400/25'}`}>
+              <p className="text-black font-sans text-xl">{value}%</p>
+              <div className="py-1"/>
+              <p className={`font-sans text-sm ${delta > 0? 'text-green-600': delta < 0? 'text-red-400': 'opacity-0'}`}>{delta>0? '+':''}{delta}%</p>
+              <span className="pointer-events-none z-50 max-w-xl absolute font-sans text-sm text-white rounded-md p-4 bg-neutral-800 -top-48 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100">{tooltipMsgLine1}<br/><br/>{tooltipMsgLine2}</span>
+          </div>
+          <div className="py-2"/>
+          <p className="text-black font-display text-lg">{title}</p>
       </div>
   );
 };
