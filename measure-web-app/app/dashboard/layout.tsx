1699a1d67a2fe59f9713899cc501de4fa9a38a9a
Anup Cowkur <anupcowkur89@gmail.com>
2023-10-19 14:15:18 +0530

    chore(webapp): make dashboard side nav stick on medium+ screen sizes

--- measure-web-app/app/dashboard/layout.tsx
+++ measure-web-app/app/dashboard/layout.tsx
@@ -37,9 +37,11 @@ export default function DashboardLayout({
     const router = useRouter();
 
     return (
-        <div className="flex flex-col md:flex-row flex-1">
-            <aside className="border-black border-r w-full md:w-60">
-                <nav className="flex flex-col p-2 md:h-screen">
+      <div>
+        // Side nav and main content layout on normal+ size screens
+        <div className="hidden md:flex">
+            <aside className="border-black border-r sticky top-0 h-screen">
+                <nav className="flex flex-col p-2 h-full w-60">
                     <div className="py-4"/>
                     <TeamSwitcher items={['Anup', 'Measure','LeftShift']}/>
                     <div className="py-4"/>
@@ -56,5 +58,27 @@ export default function DashboardLayout({
             </aside>
             <main className="flex-1">{children}</main>
         </div>
+
+        // Side nav and main content layout on small screens
+        <div className="flex flex-col md:hidden">
+            <aside className="border-black border-r">
+                <nav className="flex flex-col p-2 h-full w-screen">
+                    <div className="py-4"/>
+                    <TeamSwitcher items={['Anup', 'Measure','LeftShift']}/>
+                    <div className="py-4"/>
+                    <ul>
+                        {menuItems.map(({ href, title }) => (
+                            <li key={title}>
+                                <Link href={href} className={`m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4 ${pathName === href && 'bg-neutral-950 text-white focus-visible:text-black hover:text-black'}`}>{title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="grow"/>
+                    <button className="m-4 mt-8 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200  border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4" onClick={() => router.push('/')}>Logout</button>
+                </nav>
+            </aside>
+            <main className="flex-1">{children}</main>
+        </div>
+      </div>
     );
 }
