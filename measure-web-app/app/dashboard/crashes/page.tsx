23a4c3b09cf8f7a7739ea5f3cf9b086985b7800b
Anup Cowkur <anupcowkur89@gmail.com>
2023-10-20 16:30:31 +0530

    chore(webapp): add crash rate line chart and app version checkbox dropdown to crashes page

--- measure-web-app/app/dashboard/crashes/page.tsx
+++ measure-web-app/app/dashboard/crashes/page.tsx
@@ -1,11 +1,38 @@
-export default function Crashes() {
+import CheckboxDropdown from "@/app/components/checkbox_dropdown";
+import Dropdown from "@/app/components/dropdown";
+import ExceptionRateChart from "@/app/components/exception_rate_chart";
+import InfoCircleAppAdoption from "@/app/components/info_circle_app_adoption";
+import InfoCircleAppSize from "@/app/components/info_circle_app_size";
+import InfoCircleAppStartTime from "@/app/components/info_circle_app_start_time";
+import InfoCircleExceptionRate from "@/app/components/info_circle_exception_rate";
+import UserFlow from "@/app/components/user_flow";
+
+export default function Overview() {
+  const today = new Date();
+  const endDate = `${today.getFullYear()}-${(today.getMonth()+1).toString().padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')}`;
+
+  const sevenDaysAgo = new Date(today.setDate(today.getDate() - 7));
+  const startDate = `${sevenDaysAgo.getFullYear()}-${(sevenDaysAgo.getMonth()+1).toString().padStart(2, '0')}-${sevenDaysAgo.getDate().toString().padStart(2, '0')}`;
+
   return (
-    <main className="flex flex-col items-center justify-between selection:bg-yellow-200/75">
-      <div className="flex flex-col items-center md:w-4/5 2xl:w-3/5 px-16">
-        <div className="py-24"/>
-        <p className="text-black">Crashes</p>
-        <div className="py-24"/>
-      </div>  
-    </main>
+    <div className="flex flex-col selection:bg-yellow-200/75 items-start p-16">
+      <div className="py-4"/>
+      <p className="font-display font-regular text-black text-4xl max-w-6xl text-center">Crashes</p>
+      <div className="py-4"/>
+      <div className="flex flex-wrap gap-x-16 gap-y-4 items-center">
+        <Dropdown items={['Readly prod', 'Readly alpha','Readly debug']}/>
+        <div className="flex flex-row items-center">
+          <input type="date" value={startDate} className="font-display text-black border border-black rounded-md p-2"/>
+          <p className="text-black font-display px-2">to</p>
+          <input type="date" value={endDate} className="font-display text-black border border-black rounded-md p-2"/>
+        </div>
+        <CheckboxDropdown title="App versions" items={['Version 13.2.1', 'Version 13.2.2','Version 13.3.7']}/>
+      </div>
+      <div className="py-8"/>
+      <div className="border border-black text-black font-sans text-sm w-full h-[40rem]">
+        <ExceptionRateChart/>
+      </div>
+      <div className="py-8"/>
+    </div>
   )
 }
