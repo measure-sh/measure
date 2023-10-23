cfeeffc6343f2ee110bcf8966a40e7e6b4ceb609
Anup Cowkur <anupcowkur89@gmail.com>
2023-10-23 15:25:19 +0530

    chore(webapp): add crash list to crashes page

--- measure-web-app/app/dashboard/crashes/page.tsx
+++ measure-web-app/app/dashboard/crashes/page.tsx
@@ -6,6 +6,70 @@ import InfoCircleAppSize from "@/app/components/info_circle_app_size";
 import InfoCircleAppStartTime from "@/app/components/info_circle_app_start_time";
 import InfoCircleExceptionRate from "@/app/components/info_circle_exception_rate";
 import UserFlow from "@/app/components/user_flow";
+import Link from "next/link";
+
+const crashes = [
+  {
+    id: 'asldkfjlk34343',
+    title: 'NullPointerException.kt',
+    count: 87928,
+    percentage: 15,
+  },
+  {
+    id: 'sldfkjsklf898',
+    title: 'LayoutInflaterException.kt',
+    count: 56789,
+    percentage: 9,
+  },
+  {
+    id: 'asafdasfd9900',
+    title: 'RuntimeException.kt',
+    count: 7890,
+    percentage: 3,
+  },
+  {
+    id: 'bnflkjfg8989',
+    title: 'MainThreadException.kt',
+    count: 4546,
+    percentage: 1.5,
+  },
+  {
+    id: 'cbcmvncmvn89898',
+    title: 'InvalidDataException.kt',
+    count: 1031,
+    percentage: 0.76,
+  },
+  {
+    id: 'sldkjkjdf8989',
+    title: 'JsonParsingException.kt',
+    count: 1031,
+    percentage: 0.76,
+  },
+  {
+    id: 'sbxcbvcv898',
+    title: 'InvalidParameterException.kt',
+    count: 1002,
+    percentage: 0.73,
+  },
+  {
+    id: 'asdfsdgsdg90909',
+    title: 'InvalidDimensionsException.kt',
+    count: 856,
+    percentage: 0.61,
+  },
+  {
+    id: 'ckvjdfsfjh78aswe',
+    title: 'InvalidPaymentMethodException.kt',
+    count: 768,
+    percentage: 0.51,
+  },
+  {
+    id: 'askjbljhdkfe5435',
+    title: 'ApiResponseException.kt',
+    count: 652,
+    percentage: 0.43,
+  },
+];
 
 export default function Overview() {
   const today = new Date();
@@ -37,10 +101,28 @@ export default function Overview() {
         </div>
       </div>
       <div className="py-6"/>
-      <div className="border border-black text-black font-sans text-sm w-5/6 h-[36rem]">
+      <div className="border border-black text-black font-sans text-sm w-full h-[36rem]">
         <ExceptionRateChart/>
       </div>
       <div className="py-8"/>
+      <div className="table text-black font-sans border border-black w-full">
+        <div className="table-header-group border border-black">
+          <div className="table-row">
+            <div className="table-cell border border-black p-2 font-display">Crash Name</div>
+            <div className="table-cell border border-black p-2 font-display text-center">Instances</div>
+            <div className="table-cell border border-black p-2 font-display text-center">Percentage contribution</div>
+          </div>
+        </div>
+        <div className="table-row-group">
+          {crashes.map(({ id, title, count, percentage }) => (
+              <Link key={id} href={`/dashboard/overview/crashes/${id}`} className="table-row hover:bg-yellow-200 active:bg-yellow-300">
+                <div className="table-cell border border-black p-2 hover:bg-yellow-200 active:bg-yellow-300">{title}</div>
+                <div className="table-cell border border-black p-2 text-center">{count} instances</div>
+                <div className="table-cell border border-black p-2 text-center">{percentage}%</div>
+              </Link>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
