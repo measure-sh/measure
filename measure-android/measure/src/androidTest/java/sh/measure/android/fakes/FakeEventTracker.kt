47855101d338253c9db3d692e18b1395073fa7f6
Abhay Sood <soodabhay23@gmail.com>
2023-10-24 13:44:19 +0530

    feat(android): update espresso tests

--- measure-android/measure/src/androidTest/java/sh/measure/android/fakes/FakeEventTracker.kt
+++ measure-android/measure/src/androidTest/java/sh/measure/android/fakes/FakeEventTracker.kt
@@ -5,13 +5,20 @@ import sh.measure.android.exceptions.MeasureException
 import sh.measure.android.gestures.ClickEvent
 import sh.measure.android.gestures.LongClickEvent
 import sh.measure.android.gestures.ScrollEvent
+import sh.measure.android.lifecycle.ActivityLifecycleEvent
+import sh.measure.android.lifecycle.ApplicationLifecycleEvent
+import sh.measure.android.lifecycle.FragmentLifecycleEvent
 
+@Suppress("MemberVisibilityCanBePrivate")
 internal class FakeEventTracker: EventTracker {
     val trackedUnhandledExceptions = mutableListOf<MeasureException>()
     val trackedAnrs = mutableListOf<MeasureException>()
     val trackedClicks = mutableListOf<ClickEvent>()
     val trackedLongClicks = mutableListOf<LongClickEvent>()
     val trackedScrolls = mutableListOf<ScrollEvent>()
+    val trackedActivityLifecycleEvents = mutableListOf<ActivityLifecycleEvent>()
+    val trackedFragmentLifecycleEvents = mutableListOf<FragmentLifecycleEvent>()
+    val trackedApplicationLifecycleEvents = mutableListOf<ApplicationLifecycleEvent>()
 
     override fun trackUnhandledException(measureException: MeasureException) {
         trackedUnhandledExceptions.add(measureException)
@@ -31,5 +38,17 @@ internal class FakeEventTracker: EventTracker {
 
     override fun trackScroll(scroll: ScrollEvent) {
         trackedScrolls.add(scroll)
+    }
+
+    override fun trackActivityLifecycleEvent(event: ActivityLifecycleEvent) {
+        trackedActivityLifecycleEvents.add(event)
+    }
+
+    override fun trackFragmentLifecycleEvent(event: FragmentLifecycleEvent) {
+        trackedFragmentLifecycleEvents.add(event)
+    }
+
+    override fun trackApplicationLifecycleEvent(event: ApplicationLifecycleEvent) {
+        trackedApplicationLifecycleEvents.add(event)
     }
 }
