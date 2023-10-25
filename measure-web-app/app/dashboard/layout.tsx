c9c9d39aa19bd013e43ab42d33736557beafd175
Anup Cowkur <anupcowkur89@gmail.com>
2023-10-25 16:47:18 +0530

    chore(webapp): keep side nav link highlighted even when navigating to sub paths

--- measure-web-app/app/dashboard/layout.tsx
+++ measure-web-app/app/dashboard/layout.tsx
@@ -48,7 +48,7 @@ export default function DashboardLayout({
                     <ul>
                         {menuItems.map(({ href, title }) => (
                             <li key={title}>
-                                <Link href={href} className={`m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4 ${pathName === href && 'bg-neutral-950 text-white focus-visible:text-black hover:text-black'}`}>{title}</Link>
+                                <Link href={href} className={`m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4 ${pathName.includes(href) && 'bg-neutral-950 text-white focus-visible:text-black hover:text-black'}`}>{title}</Link>
                             </li>
                         ))}
                     </ul>
@@ -69,7 +69,7 @@ export default function DashboardLayout({
                     <ul>
                         {menuItems.map(({ href, title }) => (
                             <li key={title}>
-                                <Link href={href} className={`m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4 ${pathName === href && 'bg-neutral-950 text-white focus-visible:text-black hover:text-black'}`}>{title}</Link>
+                                <Link href={href} className={`m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4 ${pathName.includes(href) && 'bg-neutral-950 text-white focus-visible:text-black hover:text-black'}`}>{title}</Link>
                             </li>
                         ))}
                     </ul>
