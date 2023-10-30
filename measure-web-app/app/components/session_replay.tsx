9304a78229d67798397cd0c2fad2478733ead1eb
Anup Cowkur <anupcowkur89@gmail.com>
2023-10-30 14:32:14 +0530

    chore(webapp): combine thread events into single chart in session replay

--- measure-web-app/app/components/session_replay.tsx
+++ measure-web-app/app/components/session_replay.tsx
@@ -96,112 +96,105 @@ const cpuData = [
   }
 ]
 
-const uiThreadData = [
+const threadPoints = [
+  {
+    "x": date1,
+    "y": "UI thread",
+    "color": "hsl(0, 0%, 9%)",
+    "event": "Type: Screen Open\nScreen name: HomeActivity.java\nView dimensions: 640x480",
+  },
+  {
+    "x": date2,
+    "y": "UI thread",
+    "color": "hsl(142, 76%, 36%)",
+    "event": "Type: Scroll\nView name: HomeList.java\nView dimensions: 640x320"
+  },
+  {
+    "x": date3,
+    "y": "UI thread",
+    "color": "hsl(142, 76%, 36%)",
+    "event": "Type: Scroll\nView name: HomeList.java\nView dimensions: 640x320"
+  },
+  {
+    "x": date4,
+    "y": "UI thread",
+    "color": "hsl(142, 76%, 36%)",
+    "event": "Type: Scroll\nView name: HomeList.java\nView dimensions: 640x320"
+  },
+  {
+    "x": date5,
+    "y": "UI thread",
+    "color": "hsl(142, 76%, 36%)",
+    "event": "Type: Click\nView name: ItemDetailButton.java\nView dimensions: 464x180"
+  },
+  {
+    "x": date6,
+    "y": "UI thread",
+    "color": "hsl(0, 0%, 9%)",
+    "event": "Type: Screen Open\nScreen name: ItemDetailFragment.java\nView dimensions: 640x480"
+  },
+  {
+    "x": date7,
+    "y": "UI thread",
+    "color": "hsl(142, 76%, 36%)",
+    "event": "Type: Scroll\nView name: ItemDescriptionScrollView.java\nView dimensions: 640x320"
+  },
+  {
+    "x": date8,
+    "y": "UI thread",
+    "color": "hsl(0, 72%, 51%)",
+    "event": "Type: Crash\nView name: NullPointerException.java\n"
+  },
+  {
+    "x": date4,
+    "y": "Thread 1",
+    "color": "hsl(200, 98%, 39%)",
+    "event": "Type: Network Request\nURL: /home/list/items/\nHeaders: {x-auth-id: alsdkfjsldfj}"
+  },
+  {
+    "x": date6,
+    "y": "Thread 1",
+    "color": "hsl(200, 98%, 39%)",
+    "event": "Type: Network Response\nURL: /home/list/items/\nResponse: {data:[items: [...]]}"
+  },
+  {
+    "x": date3,
+    "y": "Thread 2",
+    "color": "hsl(0, 0%, 9%)",
+    "event": "Type: Background job start\nJob name: SyncUserPreferences.java"
+  }
+
+]
+
+const threadData = [
   {
     "id": "UI thread",
     "color": "hsl(0, 0%, 100%)",
     "data": [
-      {
-        "x": date1,
-        "y": 1,
-        "color": "hsl(0, 0%, 9%)",
-        "event": "Type: Screen Open\nScreen name: HomeActivity.java\nView dimensions: 640x480",
-      },
-      {
-        "x": date2,
-        "y": 1,
-        "color": "hsl(142, 76%, 36%)",
-        "event": "Type: Scroll\nView name: HomeList.java\nView dimensions: 640x320"
-      },
-      {
-        "x": date3,
-        "y": 1,
-        "color": "hsl(142, 76%, 36%)",
-        "event": "Type: Scroll\nView name: HomeList.java\nView dimensions: 640x320"
-      },
-      {
-        "x": date4,
-        "y": 1,
-        "color": "hsl(142, 76%, 36%)",
-        "event": "Type: Scroll\nView name: HomeList.java\nView dimensions: 640x320"
-      },
-      {
-        "x": date5,
-        "y": 1,
-        "color": "hsl(142, 76%, 36%)",
-        "event": "Type: Click\nView name: ItemDetailButton.java\nView dimensions: 464x180"
-      },
-      {
-        "x": date6,
-        "y": 1,
-        "color": "hsl(0, 0%, 9%)",
-        "event": "Type: Screen Open\nScreen name: ItemDetailFragment.java\nView dimensions: 640x480"
-      },
-      {
-        "x": date7,
-        "y": 1,
-        "color": "hsl(142, 76%, 36%)",
-        "event": "Type: Scroll\nView name: ItemDescriptionScrollView.java\nView dimensions: 640x320"
-      },
-      {
-        "x": date8,
-        "y": 1,
-        "color": "hsl(0, 72%, 51%)",
-        "event": "Type: Crash\nView name: NullPointerException.java\n"
-      }
+      threadPoints[0],
+      threadPoints[1],
+      threadPoints[2],
+      threadPoints[3],
+      threadPoints[4],
+      threadPoints[5],
+      threadPoints[6],
+      threadPoints[7],
     ]
-  }
-]
-
-const thread1Data = [
+  },
   {
     "id": "Thread 1",
     "color": "hsl(0, 0%, 100%)",
     "data": [
-      {
-        "x": date4,
-        "y": 1,
-        "color": "hsl(200, 98%, 39%)",
-        "event": "Type: Network Request\nURL: /home/list/items/\nHeaders: {x-auth-id: alsdkfjsldfj}"
-      },
-      {
-        "x": date6,
-        "y": 1,
-        "color": "hsl(200, 98%, 39%)",
-        "event": "Type: Network Response\nURL: /home/list/items/\nResponse: {data:[items: [...]]}"
-      }
+      threadPoints[8],
+      threadPoints[9],
     ]
-  }
-]
-
-const thread2Data = [
+  },
   {
     "id": "Thread 2",
     "color": "hsl(0, 0%, 100%)",
     "data": [
-      {
-        "x": date3,
-        "y": 1,
-        "color": "hsl(0, 0%, 9%)",
-        "event": "Type: Background job start\nJob name: SyncUserPreferences.java"
-      }
+      threadPoints[10],
     ]
-  }
-]
-
-const thread3Data = [
-  {
-    "id": "Thread 3",
-    "color": "hsl(0, 0%, 100%)",
-    "data": []
-  }
-]
-
-const thread4Data = [
-  {
-    "id": "Thread 4",
-    "color": "hsl(0, 0%, 100%)",
-    "data": []
   }
 ]
 
@@ -347,10 +340,10 @@ const SessionReplay = () => {
                 </div>}
           />
         </div>
-        {/* UI thread line */}
-        <div className="h-56">
+        {/* Threads line */}
+        <div className="h-[24rem]">
           <ResponsiveLine
-            data={uiThreadData}
+            data={threadData}
             margin={{ top: 40, right: 160, bottom: 80, left: 120 }}
             xFormat="time:%Y-%m-%d %H:%M:%S"
             xScale={{
@@ -362,212 +355,34 @@ const SessionReplay = () => {
               useUTC: false
             }}
             yScale={{
-                type: 'linear',
-                min: 'auto',
-                max: 'auto'
+                type: 'point',  
             }}
-            yFormat=" >-.2f"
             axisTop={null}
             axisRight={null}
             axisBottom={{
                 format: '%H:%M:%S',
                 tickValues: 'every 1 minutes',
-                legendPosition: 'middle'
+                legendPosition: 'middle',
+                tickPadding:20
             }}
             axisLeft={{
-                tickSize: 1,
-                tickPadding: 5,
-                legend: 'UI thread',
+                legend: 'Threads',
                 legendOffset: -80,
                 legendPosition: 'middle'
             }}
             pointSize={1}
             pointBorderWidth={16}
-            pointBorderColor={(point: Point) => uiThreadData[0].data[point.index].color}
+            pointBorderColor={(point: Point) => threadPoints[point.index].color}
             pointLabelYOffset={-12}
             useMesh={true}
             enableGridX={true}
             enableGridY={false}
-            colors={uiThreadData.map((i) => i.color)}
+            colors={threadData.map((i) => i.color)}
             tooltip={({
               point
             }) => <div className="pointer-events-none z-50 rounded-md p-4 bg-neutral-800">
-                     <p className="font-sans text-white whitespace-pre-wrap">{uiThreadData[0].data[point.index].event} </p>
+                     <p className="font-sans text-white whitespace-pre-wrap">{threadPoints[point.index].event} </p>
                   </div>}
-          />
-        </div>
-        {/* Thread 1 line */}
-        <div className="h-56">
-          <ResponsiveLine
-            data={thread1Data}
-            margin={{ top: 40, right: 160, bottom: 80, left: 120 }}
-            xFormat="time:%Y-%m-%d %H:%M:%S"
-            xScale={{
-              format: '%Y-%m-%d %H:%M:%S',
-              precision: 'second',
-              type: 'time',
-              min: minDate,
-              max: maxDate,
-              useUTC: false
-            }}
-            yScale={{
-                type: 'linear',
-                min: 'auto',
-                max: 'auto'
-            }}
-            yFormat=" >-.2f"
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-                format: '%H:%M:%S',
-                tickValues: 'every 1 minutes',
-                legendPosition: 'middle'
-            }}
-            axisLeft={{
-                tickSize: 1,
-                tickPadding: 5,
-                legend: 'Thread 1',
-                legendOffset: -80,
-                legendPosition: 'middle'
-            }}
-            pointSize={1}
-            pointBorderWidth={16}
-            pointBorderColor={(point: Point) => thread1Data[0].data[point.index].color}
-            pointLabelYOffset={-12}
-            useMesh={true}
-            enableGridX={true}
-            enableGridY={false}
-            colors={thread1Data.map((i) => i.color)}
-            tooltip={({
-              point
-            }) => <div className="pointer-events-none z-50 rounded-md p-4 bg-neutral-800">
-                     <p className="font-sans text-white whitespace-pre-wrap">{thread1Data[0].data[point.index].event} </p>
-                  </div>}
-          />
-        </div>
-        {/* Thread 2 line */}
-        <div className="h-56">
-          <ResponsiveLine
-            data={thread2Data}
-            margin={{ top: 40, right: 160, bottom: 80, left: 120 }}
-            xFormat="time:%Y-%m-%d %H:%M:%S"
-            xScale={{
-              format: '%Y-%m-%d %H:%M:%S',
-              precision: 'second',
-              type: 'time',
-              min: minDate,
-              max: maxDate,
-              useUTC: false
-            }}
-            yScale={{
-                type: 'linear',
-                min: 'auto',
-                max: 'auto'
-            }}
-            yFormat=" >-.2f"
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-                format: '%H:%M:%S',
-                tickValues: 'every 1 minutes',
-                legendPosition: 'middle'
-            }}
-            axisLeft={{
-                tickSize: 1,
-                tickPadding: 5,
-                legend: 'Thread 2',
-                legendOffset: -80,
-                legendPosition: 'middle'
-            }}
-            pointSize={1}
-            pointBorderWidth={16}
-            pointBorderColor={(point: Point) => thread2Data[0].data[point.index].color}
-            pointLabelYOffset={-12}
-            useMesh={true}
-            enableGridX={true}
-            enableGridY={false}
-            colors={thread2Data.map((i) => i.color)}
-            tooltip={({
-              point
-            }) => <div className="pointer-events-none z-50 rounded-md p-4 bg-neutral-800">
-                     <p className="font-sans text-white whitespace-pre-wrap">{thread2Data[0].data[point.index].event}</p>
-                  </div>}
-          />
-        </div>
-        {/* Thread 3 line */}
-        <div className="h-56">
-          <ResponsiveLine
-            data={thread3Data}
-            margin={{ top: 40, right: 160, bottom: 80, left: 120 }}
-            xFormat="time:%Y-%m-%d %H:%M:%S"
-            xScale={{
-              format: '%Y-%m-%d %H:%M:%S',
-              precision: 'second',
-              type: 'time',
-              min: minDate,
-              max: maxDate,
-              useUTC: false
-            }}
-            yScale={{
-                type: 'linear',
-                min: 'auto',
-                max: 'auto'
-            }}
-            yFormat=" >-.2f"
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-                format: '%H:%M:%S',
-                tickValues: 'every 1 minutes',
-                legendPosition: 'middle'
-            }}
-            axisLeft={{
-                tickSize: 1,
-                tickPadding: 5,
-                legend: 'Thread 3',
-                legendOffset: -80,
-                legendPosition: 'middle'
-            }}
-            enableGridX={true}
-            enableGridY={false}
-          />
-        </div>
-        {/* Thread 4 line */}
-        <div className="h-56">
-          <ResponsiveLine
-            data={thread4Data}
-            margin={{ top: 40, right: 160, bottom: 80, left: 120 }}
-            xFormat="time:%Y-%m-%d %H:%M:%S"
-            xScale={{
-              format: '%Y-%m-%d %H:%M:%S',
-              precision: 'second',
-              type: 'time',
-              min: minDate,
-              max: maxDate,
-              useUTC: false
-            }}
-            yScale={{
-                type: 'linear',
-                min: 'auto',
-                max: 'auto'
-            }}
-            yFormat=" >-.2f"
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-                format: '%H:%M:%S',
-                tickValues: 'every 1 minutes',
-                legendPosition: 'middle'
-            }}
-            axisLeft={{
-                tickSize: 1,
-                tickPadding: 5,
-                legend: 'Thread 4',
-                legendOffset: -80,
-                legendPosition: 'middle'
-            }}
-            enableGridX={true}
-            enableGridY={false}
           />
         </div>
     </div>
