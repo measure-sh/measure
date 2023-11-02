f4d2160bc000676c5a0d62fa4322e3c5679d89e4
Abhay Sood <soodabhay23@gmail.com>
2023-11-03 04:04:20 +0530

    feat(android): add attachments to session report

--- measure-android/measure/src/main/java/sh/measure/android/session/SessionReport.kt
+++ measure-android/measure/src/main/java/sh/measure/android/session/SessionReport.kt
@@ -1,6 +1,7 @@
 package sh.measure.android.session
 
 import kotlinx.serialization.json.JsonElement
+import sh.measure.android.attachment.AttachmentPacket
 import java.io.File
 
 internal data class SessionReport(
@@ -8,4 +9,5 @@ internal data class SessionReport(
     val timestamp: String,
     val resource: JsonElement,
     val eventsFile: File,
+    val attachments: List<AttachmentPacket>,
 )
