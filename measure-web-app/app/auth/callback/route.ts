de3f9896990366951cdd49d85b3fcd5f4780bcdc
Debjeet Biswas <debjeet12@gmail.com>
2023-11-03 07:46:05 +0530

    chore(webapp): minor format
    - minor whitespace format for consistency

--- measure-web-app/app/auth/callback/route.ts
+++ measure-web-app/app/auth/callback/route.ts
@@ -6,11 +6,11 @@ export const dynamic = 'force-dynamic'
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
-  
+
   if (code) {
     const supabase = createClient()
     await supabase.auth.exchangeCodeForSession(code)
   }
-  
+
   return NextResponse.redirect(`${requestUrl.origin}/dashboard/overview`)
 }
