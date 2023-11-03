31aa36c07f7693067466ddd5052ea716c59acaa4
Debjeet Biswas <debjeet12@gmail.com>
2023-11-03 23:21:27 +0530

    chore(webapp): fix layout shift
    - adjusted layout shift using simple spacing

--- measure-web-app/app/auth/login/google-sign-in.tsx
+++ measure-web-app/app/auth/login/google-sign-in.tsx
@@ -8,6 +8,6 @@ export default function GoogleSignIn() {
     setIsClient(true)
   }, [])
   return (isClient ?
-    <script src="https://accounts.google.com/gsi/client" async></script> : <p className="font-display text-center" style={{ height: "2.5rem" }}>Loading social logins...</p>
+    <script src="https://accounts.google.com/gsi/client" async></script> : <p className="font-display text-center" style={{ height: "0.73rem" }}>Loading social logins...</p>
   )
 }
