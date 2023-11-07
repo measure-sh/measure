640190b3e5540cb4f1152c1b701e70ff0fba304b
Anup Cowkur <anupcowkur89@gmail.com>
2023-11-07 17:16:13 +0530

    chore(webapp): remove interactivity from FilterPills
    they should only be used for informational display and hence do not
    need to be interactive.

--- measure-web-app/app/components/filter_pill.tsx
+++ measure-web-app/app/components/filter_pill.tsx
@@ -6,12 +6,9 @@ interface FilterPillProps {
 
 const FilterPill: React.FC<FilterPillProps> = ({ title }) => {
   return (
-    <button
-      type="button"
-      className="px-2 py-1 text-white hover:text-black focus:text-black bg-neutral-950 font-display text-sm border border-black rounded-full outline-none hover:bg-yellow-200 focus:bg-yellow-200 active:bg-yellow-300"
-    >
-      {title} x
-    </button>
+    <p className="px-2 py-1 text-white bg-neutral-950 font-display text-sm border border-black rounded-full outline-none">
+      {title}
+    </p>
     );
 };
 
