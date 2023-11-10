061126100964f3e869eb49bc5e97fc9043d70e2a
Anup Cowkur <anupcowkur89@gmail.com>
2023-11-10 19:24:52 +0530

    feat(webapp): fetch app launch time overview metrics from API

--- measure-web-app/app/components/info_circle_app_start_time.tsx
+++ measure-web-app/app/components/info_circle_app_start_time.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 
 
 interface InfoCircleAppStartTimeProps {
+  message: string,
   value: number;
   delta: number;
   title: string;
   launchType: string;
 }
 
-const InfoCircleAppStartTime = ({ value, delta, title, launchType }: InfoCircleAppStartTimeProps) => {
+const InfoCircleAppStartTime = ({ message, value, delta, title, launchType }: InfoCircleAppStartTimeProps) => {
     return (
       <div className="flex flex-col items-center">
-          <div className={`flex flex-col group relative items-center justify-center w-64 aspect-square rounded-full border border-4 transition-colors duration-100 ${value < 800? 'border-green-400 hover:bg-green-400/25': value < 1200? 'border-yellow-400 hover:bg-yellow-400/25': 'border-red-400 hover:bg-red-400/25'}`}>
-              <p className="text-black font-sans text-xl">{value}ms</p>
+          <div className={`flex flex-col group relative items-center justify-center w-64 aspect-square rounded-full border border-4 transition-colors duration-100 ${message!==""? 'border-black hover:bg-neutral-800/25': value < 800? 'border-green-400 hover:bg-green-400/25': value < 1200? 'border-yellow-400 hover:bg-yellow-400/25': 'border-red-400 hover:bg-red-400/25'}`}>
+              <p className="text-black font-sans text-xl">{message === ""? value+'ms': message}</p>
               <div className="py-1"/>
-              <p className={`font-sans text-sm ${delta < 0? 'text-green-600': delta > 0? 'text-red-400': 'opacity-0'}`}>{delta>0? '+':''}{delta}ms</p>
+              {message==="" && <p className={`font-sans text-sm ${delta < 0? 'text-green-600': delta > 0? 'text-red-400': 'opacity-0'}`}>{delta>0? '+':''}{delta}ms</p>}
               <span className="pointer-events-none z-50 max-w-xl absolute font-sans text-sm text-white rounded-md p-4 bg-neutral-800 -top-44 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100">
                 App start time = p95 {launchType} launch time of selected app version in selected time period<br/><br/>Delta value = p95 {launchType} launch time of selected app version in selected time period - p95 {launchType} launch time of all app versions in selected time period
               </span>
