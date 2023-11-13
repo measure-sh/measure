05a881b9cffa5657c43f183000c245488e5a90b4
Debjeet Biswas <detj@users.noreply.github.com>
2023-11-13 13:37:57 +0530

    chore(backend): app request filtering (#198)
    - add app filter struct
    - validate app id
    - validate from & to date range
    - validate app version
    - parse app's issue journey map request parameters
    - parse app's metrics request parameters
    - validate app's issue journey map request parameters
    - validate app's metrics request parameters
    
    related to #184

---
+++ measure-backend/measure-go/appFilter.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// DefaultDuration is the time duration to
+// set between the from and to time
+const DefaultDuration = time.Hour * 24 * 7
+
+// AppFilter validates and stores the request
+// parameters to query the app's issue journey map,
+// metrics, exceptions and ANRs
+type AppFilter struct {
+	// these fields should be exportable
+	// otherwise gin doesn't bind them
+	// and fails silently
+	AppID   uuid.UUID
+	From    time.Time `form:"from" time_format:"2006-01-02T15:04:05.000Z" time_utc:"1"`
+	To      time.Time `form:"to" time_format:"2006-01-02T15:04:05.000Z" time_utc:"1"`
+	Version string    `form:"version"`
+}
+
+// validate validates the app id, time range and
+// version of the AppFilter
+func (af *AppFilter) validate() error {
+	// app UUID validations
+	if af.AppID == uuid.Nil {
+		return fmt.Errorf("app id is invalid or empty")
+	}
+
+	if af.AppID.Version().String() != "VERSION_4" {
+		return fmt.Errorf("app id version is not UUID v4")
+	}
+
+	// time validations
+	if af.To.Before(af.From) {
+		return fmt.Errorf("`to` must be later time than `from`")
+	}
+
+	if af.From.After(time.Now().UTC()) {
+		return fmt.Errorf("`from` cannot be later than now")
+	}
+
+	if af.From.IsZero() && !af.To.IsZero() {
+		return fmt.Errorf("both `from` and `to` time must be set")
+	}
+
+	if af.To.IsZero() && !af.From.IsZero() {
+		return fmt.Errorf("both `from` and `to` time must be set")
+	}
+
+	return nil
+}
+
+// hasVersion returns true if the request sets
+// a version, otherwise false
+func (af *AppFilter) hasVersion() bool {
+	return af.Version != ""
+}
+
+// hasTimeRange returns true if the request sets
+// a valid time range, otherwise false
+func (af *AppFilter) hasTimeRange() bool {
+	return !af.From.IsZero() && !af.To.IsZero()
+}
+
+// setDefaultTimeRange sets the time range to last
+// default duration from current UTC time
+func (af *AppFilter) setDefaultTimeRange() {
+	to := time.Now().UTC()
+	from := to.Add(-DefaultDuration)
+
+	af.From = from
+	af.To = to
+}
+
+// setDefaultVersion sets the version to the
+// latest app version if available
+func (af *AppFilter) setDefaultVersion() {
+	af.Version = "1.2.3"
+}
--- measure-backend/measure-go/apps.go
+++ measure-backend/measure-go/apps.go
@@ -1,13 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func getAppJourney(c *gin.Context) {
+	var af AppFilter
+
+	id, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		msg := `id invalid or missing`
+		fmt.Println(msg, err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": msg})
+		return
+	}
+	af.AppID = id
+
+	if err := c.ShouldBindQuery(&af); err != nil {
+		fmt.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := af.validate(); err != nil {
+		msg := "app journey request validation failed"
+		fmt.Println(msg, err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": msg, "details": err.Error()})
+		return
+	}
+
+	if !af.hasTimeRange() {
+		af.setDefaultTimeRange()
+	}
+
+	if !af.hasVersion() {
+		af.setDefaultVersion()
+	}
+
+	// fmt.Println("journey request app id", af.AppID)
+	// fmt.Println("journey request from", af.From)
+	// fmt.Println("journey request to", af.To)
+	// fmt.Println("journey request version", af.Version)
+
 	data1 := `{"nodes":[{"id":"Home Screen","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Order History","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Order Status","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Support","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"List Of Items","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Sales Offer","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"View Item Images","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"View Item Detail","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Cyber Monday Sale Items List","nodeColor":"hsl(0, 72%, 51%)","issues":{"crashes":[{"title":"NullPointerException.java","count":37893},{"title":"LayoutInflaterException.java","count":12674}],"anrs":[{"title":"CyberMondayActivity.java","count":97321},{"title":"CyberMondayFragment.kt","count":8005}]}},{"id":"Add To Cart","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Pay","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Explore Discounts","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}}],"links":[{"source":"Home Screen","target":"Order History","value":50000},{"source":"Home Screen","target":"List Of Items","value":73356},{"source":"Home Screen","target":"Cyber Monday Sale Items List","value":97652},{"source":"Order History","target":"Order Status","value":9782},{"source":"Order History","target":"Support","value":2837},{"source":"List Of Items","target":"Sales Offer","value":14678},{"source":"List Of Items","target":"View Item Detail","value":23654},{"source":"Cyber Monday Sale Items List","target":"View Item Detail","value":43889},{"source":"Cyber Monday Sale Items List","target":"Explore Discounts","value":34681},{"source":"Sales Offer","target":"View Item Images","value":12055},{"source":"View Item Detail","target":"View Item Images","value":16793},{"source":"View Item Detail","target":"Add To Cart","value":11537},{"source":"Add To Cart","target":"Pay","value":10144},{"source":"Add To Cart","target":"Explore Discounts","value":4007}]}`
 
 	data2 := `{"nodes":[{"id":"Home Screen","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Order History","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Order Status","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Support","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"List Of Items","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Sales Offer","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"View Item Images","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"View Item Detail","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Cyber Monday Sale Items List","nodeColor":"hsl(0, 72%, 51%)","issues":{"crashes":[{"title":"NullPointerException.java","count":32893},{"title":"LayoutInflaterException.java","count":12874}],"anrs":[{"title":"CyberMondayActivity.java","count":77321},{"title":"CyberMondayFragment.kt","count":6305}]}},{"id":"Add To Cart","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Pay","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}},{"id":"Explore Discounts","nodeColor":"hsl(142, 69%, 58%)","issues":{"crashes":[],"anrs":[]}}],"links":[{"source":"Home Screen","target":"Order History","value":60000},{"source":"Home Screen","target":"List Of Items","value":53356},{"source":"Home Screen","target":"Cyber Monday Sale Items List","value":96652},{"source":"Order History","target":"Order Status","value":9822},{"source":"Order History","target":"Support","value":2287},{"source":"List Of Items","target":"Sales Offer","value":12628},{"source":"List Of Items","target":"View Item Detail","value":53254},{"source":"Cyber Monday Sale Items List","target":"View Item Detail","value":43889},{"source":"Cyber Monday Sale Items List","target":"Explore Discounts","value":34681},{"source":"Sales Offer","target":"View Item Images","value":12055},{"source":"View Item Detail","target":"View Item Images","value":12793},{"source":"View Item Detail","target":"Add To Cart","value":16537},{"source":"Add To Cart","target":"Pay","value":10144},{"source":"Add To Cart","target":"Explore Discounts","value":3007}]}`
@@ -28,6 +67,43 @@ func getAppJourney(c *gin.Context) {
 }
 
 func getAppMetrics(c *gin.Context) {
+	var af AppFilter
+
+	id, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		msg := `id invalid or missing`
+		fmt.Println(msg, err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": msg})
+		return
+	}
+	af.AppID = id
+
+	if err := c.ShouldBindQuery(&af); err != nil {
+		fmt.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := af.validate(); err != nil {
+		msg := "app journey request validation failed"
+		fmt.Println(msg, err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": msg, "details": err.Error()})
+		return
+	}
+
+	if !af.hasTimeRange() {
+		af.setDefaultTimeRange()
+	}
+
+	if !af.hasVersion() {
+		af.setDefaultVersion()
+	}
+
+	// fmt.Println("journey request app id", af.AppID)
+	// fmt.Println("journey request from", af.From)
+	// fmt.Println("journey request to", af.To)
+	// fmt.Println("journey request version", af.Version)
+
 	data1 := `{"adoption":{"users":40000,"totalUsers":200000,"value":20},"app_size":{"value":20,"delta":3.18},"crash_free_users":{"value":98.5,"delta":0.73},"perceived_crash_free_users":{"value":91.3,"delta":-0.51},"multiple_crash_free_users":{"value":76.37,"delta":0.62},"anr_free_users":{"value":98.5,"delta":0.73},"perceived_anr_free_users":{"value":91.3,"delta":0.27},"multiple_anr_free_users":{"value":97.88,"delta":-3.13},"app_cold_launch":{"value":937,"delta":34},"app_warm_launch":{"value":600,"delta":-87},"app_hot_launch":{"value":250,"delta":-55}}`
 	data2 := `{"adoption":{"users":49000,"totalUsers":200000,"value":28},"app_size":{"value":20,"delta":3.18},"crash_free_users":{"value":98.2,"delta":0.71},"perceived_crash_free_users":{"value":92.8,"delta":-0.81},"multiple_crash_free_users":{"value":75.49,"delta":0.38},"anr_free_users":{"value":98.3,"delta":0.43},"perceived_anr_free_users":{"value":91.9,"delta":0.77},"multiple_anr_free_users":{"value":97.26,"delta":-2.85},"app_cold_launch":{"value":900,"delta":-200},"app_warm_launch":{"value":600,"delta":-127},"app_hot_launch":{"value":300,"delta":-50}}`
 
