13bcee28dd37f112092b7721656d18433d16cc40
Debjeet Biswas <debjeet12@gmail.com>
2023-11-13 05:49:48 +0530

    chore(backend): add more sample apps
    - add couple of more sample apps
    - fix incorrect team id

--- measure-backend/measure-go/teams.go
+++ measure-backend/measure-go/teams.go
@@ -19,7 +19,7 @@ func getTeams(c *gin.Context) {
 		  "name": "Measure"
 		},
 		{
-		  "id": "25226540-72cf-4982-a16f-9b3c85912b65",
+		  "id": "93848f57-9cdf-4b21-87e9-1cad562684b6",
 		  "name": "Leftshift"
 		}
 	  ]`
@@ -41,6 +41,34 @@ func getApps(c *gin.Context) {
 			{
 			  "id": "bae4fb9e-07cd-4435-a42e-d99986830c2c",
 			  "name": "Readly debug"
+			}
+		  ]`,
+		"25226540-72cf-4982-a16f-9b3c85912b65": `[
+			{
+			  "id": "c6643110-d3e5-4b1c-bfcc-75b46b52ae79",
+			  "name": "Passwordly prod"
+			},
+			{
+			  "id": "e2abe28a-f6bc-4f57-88fe-81f10d1c5afc",
+			  "name": "Passwordly alpha"
+			},
+			{
+			  "id": "b17f7003-4ab6-4b1a-a5d8-ed5a72cb4569",
+			  "name": "Passwordly debug"
+			}
+		  ]`,
+		"93848f57-9cdf-4b21-87e9-1cad562684b6": `[
+			{
+			  "id": "20014be8-aaa9-4e56-8810-9f1a48ec1099",
+			  "name": "Musicly prod"
+			},
+			{
+			  "id": "463c959c-94c2-4f49-bd2b-6caab360c152",
+			  "name": "Musicly alpha"
+			},
+			{
+			  "id": "2a7f230e-6d5e-4036-b4e6-1102c22f4433",
+			  "name": "Musicly debug"
 			}
 		  ]`,
 	}
