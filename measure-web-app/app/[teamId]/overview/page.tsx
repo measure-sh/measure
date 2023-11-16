b2924408edb53983960a5e64dfaa8fec4c6b2171
Anup Cowkur <anupcowkur89@gmail.com>
2023-11-16 16:43:32 +0530

    chore(webapp): fetch apps list in overview from API

--- measure-web-app/app/[teamId]/overview/page.tsx
+++ measure-web-app/app/[teamId]/overview/page.tsx
@@ -7,16 +7,23 @@ import UserFlow from "@/app/components/user_flow";
 import MetricsOverview from '@/app/components/metrics_overview';
 
 export default function Overview({ params }: { params: { teamId: string } }) {
+  enum AppsApiStatus {
+    Loading,
+    Success,
+    Error
+  }
+
   enum FiltersApiStatus {
     Loading,
     Success,
     Error
   }
   
+  const [appsApiStatus, setAppsApiStatus] = useState(AppsApiStatus.Loading);
   const [filtersApiStatus, setFiltersApiStatus] = useState(FiltersApiStatus.Loading);
 
-  var apps = [{'id':'59ba1c7f-2a42-4b7f-b9cb-735d25146675', 'name': 'Readly prod'}, {'id':'243f3214-0f41-4361-8ef3-21d8f5d99a70', 'name': 'Readly alpha'}, {'id':'bae4fb9e-07cd-4435-a42e-d99986830c2c', 'name': 'Readly debug'}];
-  const [selectedApp, setSelectedApp] = useState(apps[0].id);
+  const [apps, setApps] = useState([] as {'id': string, 'name':string}[]);
+  const [selectedApp, setSelectedApp] = useState('');
 
   const [versions, setVersions] = useState([] as string[]);
   const [selectedVersion, setSelectedVersion] = useState(versions[0]);
@@ -35,6 +42,32 @@ export default function Overview({ params }: { params: { teamId: string } }) {
     setFormattedStartDate(new Date(startDate).toLocaleDateString());
     setFormattedEndDate(new Date(endDate).toLocaleDateString());
   }, [startDate, endDate]);
+
+  const getApps = async (authToken:string, teamId:string, ) => {
+    setAppsApiStatus(AppsApiStatus.Loading)
+
+    const origin = "https://frosty-fog-7165.fly.dev"
+    const opts = {
+      headers: {
+        "Authorization": `Bearer ${authToken}`
+      }
+    };
+
+    const res = await fetch(`${origin}/teams/${teamId}/apps`, opts);
+    if(!res.ok) {
+      setAppsApiStatus(AppsApiStatus.Error)
+      return
+    } 
+    const data = await res.json()
+
+    setApps(data)
+    setSelectedApp(data[0].id)
+    setAppsApiStatus(AppsApiStatus.Success)
+  }
+
+  useEffect(() => {
+    getApps("abcde123", params.teamId)
+  }, []);
 
   const getFilters = async (authToken:string, appId:string, ) => {
     setFiltersApiStatus(FiltersApiStatus.Loading)
@@ -68,26 +101,29 @@ export default function Overview({ params }: { params: { teamId: string } }) {
       <p className="font-display font-regular text-black text-4xl max-w-6xl text-center">Overview</p>
       <div className="py-4" />
       <div className="flex flex-wrap gap-8 items-center">
-        <Dropdown items={apps.map((e) => e.name)} onChangeSelectedItem={(item) => setSelectedApp(apps.find((e) => e.name === item)!.id)} />
-        <div className="flex flex-row items-center">
-          <input type="date" defaultValue={startDate} max={endDate} className="font-display text-black border border-black rounded-md p-2" onChange={(e) => setStartDate(e.target.value)} />
-          <p className="text-black font-display px-2">to</p>
-          <input type="date" defaultValue={endDate} min={startDate} className="font-display text-black border border-black rounded-md p-2" onChange={(e) => setEndDate(e.target.value)} />
-        </div>
-        {filtersApiStatus === FiltersApiStatus.Success && <Dropdown items={versions} onChangeSelectedItem={(item) => setSelectedVersion(item)} />}
+        {appsApiStatus === AppsApiStatus.Loading && <p className="text-lg font-display">Updating Apps...</p>}
+        {appsApiStatus === AppsApiStatus.Error && <p className="text-lg font-display">Error fetching apps, please refresh page to try again</p>}
+        {appsApiStatus === AppsApiStatus.Success && <Dropdown items={apps.map((e) => e.name)} onChangeSelectedItem={(item) => setSelectedApp(apps.find((e) => e.name === item)!.id)} />}
+        {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && 
+          <div className="flex flex-row items-center">
+            <input type="date" defaultValue={startDate} max={endDate} className="font-display text-black border border-black rounded-md p-2" onChange={(e) => setStartDate(e.target.value)} />
+            <p className="text-black font-display px-2">to</p>
+            <input type="date" defaultValue={endDate} min={startDate} className="font-display text-black border border-black rounded-md p-2" onChange={(e) => setEndDate(e.target.value)} />
+          </div>}
+        {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Loading && <p className="text-lg font-display">Updating filters...</p>}
+        {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <Dropdown items={versions} onChangeSelectedItem={(item) => setSelectedVersion(item)} />}
       </div>
       <div className="py-4" />
       <div className="flex flex-wrap gap-2 items-center w-5/6">
-        <FilterPill title={apps.find((e) => e.id === selectedApp)!.name} />
-        <FilterPill title={`${formattedStartDate} to ${formattedEndDate}`} />
-        {filtersApiStatus === FiltersApiStatus.Success && <FilterPill title={selectedVersion} />}
+        {appsApiStatus === AppsApiStatus.Success && <FilterPill title={apps.find((e) => e.id === selectedApp)!.name} />}
+        {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <FilterPill title={`${formattedStartDate} to ${formattedEndDate}`} />}
+        {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <FilterPill title={selectedVersion} />}
       </div>
       <div className="py-8" />
-      {filtersApiStatus === FiltersApiStatus.Loading && <p className="text-lg font-display">Updating filters...</p>}
-      {filtersApiStatus === FiltersApiStatus.Error && <p className="text-lg font-display">Error fetching filters, please try again</p>}
-      {filtersApiStatus === FiltersApiStatus.Success && <UserFlow authToken="abcde123" appId={selectedApp} startDate={startDate} endDate={endDate} appVersion={selectedVersion} />}
+      {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Error && <p className="text-lg font-display">Error fetching filters, please refresh page or select a different app to try again</p>}
+      {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <UserFlow authToken="abcde123" appId={selectedApp} startDate={startDate} endDate={endDate} appVersion={selectedVersion} />}
       <div className="py-8" />
-      {filtersApiStatus === FiltersApiStatus.Success && <MetricsOverview authToken="abcde123" appId={selectedApp} startDate={startDate} endDate={endDate} appVersion={selectedVersion} />}
+      {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <MetricsOverview authToken="abcde123" appId={selectedApp} startDate={startDate} endDate={endDate} appVersion={selectedVersion} />}
     </div>
   )
 }
