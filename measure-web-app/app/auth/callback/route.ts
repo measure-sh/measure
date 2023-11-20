ae28468d2c0ac541eb09fbd85741634c013f2310
Debjeet Biswas <debjeet12@gmail.com>
2023-11-20 17:25:25 +0530

    chore(backend): udpate email auth behavior
    - redirect to `/:teamId/overview` after email signin
    - handle all errors

--- measure-web-app/app/auth/callback/route.ts
+++ measure-web-app/app/auth/callback/route.ts
@@ -5,12 +5,55 @@ export const dynamic = 'force-dynamic'
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
+  const errRedirectUrl = `${requestUrl.origin}/auth/login?err=Could not authenticate with email`
   const code = requestUrl.searchParams.get('code')
 
-  if (code) {
-    const supabase = createClient()
-    await supabase.auth.exchangeCodeForSession(code)
+  if (!code) {
+    console.log("email signin redirection failed, no code found")
+    return NextResponse.redirect(errRedirectUrl, { status: 302 })
   }
 
-  return NextResponse.redirect(`${requestUrl.origin}/dashboard/overview`)
+  const supabase = createClient()
+  const { data, error } = await supabase.auth.exchangeCodeForSession(code)
+
+  if (error) {
+    console.log("email signin code exchange failed with error", error)
+    return NextResponse.redirect(errRedirectUrl, { status: 302 })
+  }
+
+  const accessToken = data.session.access_token
+
+  const origin = process?.env?.NEXT_PUBLIC_API_BASE_URL
+
+  const res = await fetch(`${origin}/teams`, {
+    headers: {
+      "Authorization": `Bearer ${accessToken}`
+    }
+  })
+
+  if (!res.ok) {
+    console.log(`GET /teams failed during email signin redirection returned ${res.status} response`)
+    return NextResponse.redirect(errRedirectUrl, { status: 302 })
+  }
+
+  const teams = await res.json()
+  if (!teams.length) {
+    console.log(`no teams found for user: ${data.user?.id}`)
+    return NextResponse.redirect(errRedirectUrl, { status: 302 })
+  }
+
+  type Team = {
+    teamId: string,
+    name: string,
+    role: string,
+  }
+
+  const ownTeam = teams.find((team: Team) => team.role === "owner")
+
+  if (!ownTeam) {
+    console.log(`user ${data.user?.id} does not own any team`)
+    return NextResponse.redirect(errRedirectUrl, { status: 302 })
+  }
+
+  return NextResponse.redirect(`${requestUrl.origin}/${ownTeam.teamId}/overview`, { status: 302 })
 }
