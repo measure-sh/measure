9cd2ce013ddab97a55091f8a7efa9aa991d29a37
Abhay Sood <soodabhay23@gmail.com>
2023-11-20 19:23:15 +0530

    fix(backend): use correct table name in CH migrations

--- self-host/clickhouse/20231120060716_alter-events-relation.sql
+++ self-host/clickhouse/20231120060716_alter-events-relation.sql
@@ -1,5 +1,5 @@
 -- migrate:up
-ALTER TABLE events
+ALTER TABLE default.events
 ADD COLUMN anr.network_type LowCardinality(FixedString(16)),
 ADD COLUMN anr.network_generation LowCardinality(FixedString(8)),
 ADD COLUMN anr.network_provider FixedString(64),
@@ -8,7 +8,7 @@ ADD COLUMN exception.network_generation LowCardinality(FixedString(8)),
 ADD COLUMN exception.network_provider FixedString(64);
 
 -- migrate:down
-ALTER TABLE events
+ALTER TABLE default.events
 DROP COLUMN anr.network_type,
 DROP COLUMN anr.network_generation,
 DROP COLUMN anr.network_provider,
