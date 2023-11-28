a1dacc87579f05e4d4450fe3f516e109a874f2cd
Anup Cowkur <anupcowkur89@gmail.com>
2023-11-28 13:11:23 +0530

    chore(webapp): update ui + add comments to overview page ui

--- measure-web-app/app/[teamId]/overview/page.tsx
+++ measure-web-app/app/[teamId]/overview/page.tsx
@@ -125,30 +125,54 @@ export default function Overview({ params }: { params: { teamId: string } }) {
       <p className="font-display font-regular text-black text-4xl max-w-6xl text-center">Overview</p>
       <div className="py-4" />
       <div className="flex flex-wrap gap-8 items-center">
+        {/* Loading message for apps */}
         {appsApiStatus === AppsApiStatus.Loading && <p className="text-lg font-display">Updating Apps...</p>}
+
+        {/* Error message for apps fetch error */}
         {appsApiStatus === AppsApiStatus.Error && <p className="text-lg font-display">Error fetching apps, please refresh page to try again</p>}
+        
+        {/* Create app when no apps exist */}
+        {appsApiStatus === AppsApiStatus.NoApps && <p className="text-lg font-display">Looks like you don&apos;t have any apps yet. Get started by creating your first app!</p>}
+        {appsApiStatus === AppsApiStatus.NoApps && <div className="py-4"/>}
         {appsApiStatus === AppsApiStatus.NoApps && <CreateApp teamId={params.teamId}/>}
+        
+        {/* Show app selector dropdown if apps fetch succeeds */}
         {appsApiStatus === AppsApiStatus.Success && <Dropdown items={apps.map((e) => e.name)} onChangeSelectedItem={(item) => setSelectedApp(apps.find((e) => e.name === item)!)} />}
+        
+        {/* Show date selector if apps and filters fetch succeeds */}
         {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && 
           <div className="flex flex-row items-center">
             <input type="date" defaultValue={startDate} max={endDate} className="font-display text-black border border-black rounded-md p-2" onChange={(e) => setStartDate(e.target.value)} />
             <p className="text-black font-display px-2">to</p>
             <input type="date" defaultValue={endDate} min={startDate} className="font-display text-black border border-black rounded-md p-2" onChange={(e) => setEndDate(e.target.value)} />
           </div>}
+
+          {/* Loading message for filters */}
         {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Loading && <p className="text-lg font-display">Updating filters...</p>}
+
+        {/* Show versions selector if apps and filters fetch succeeds  */}
         {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <Dropdown items={versions} onChangeSelectedItem={(item) => setSelectedVersion(item)} />}
       </div>
+      
       {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <div className="py-4" />}
+
+      {/* Show filter pills if apps and filters fetch succeeds  */}
       <div className="flex flex-wrap gap-2 items-center w-5/6">
         {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <FilterPill title={selectedApp.name} />}
         {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <FilterPill title={`${formattedStartDate} to ${formattedEndDate}`} />}
         {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <FilterPill title={selectedVersion} />}
       </div>
       <div className="py-8" />
+      {/* Filters fetch error message  */}
       {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Error && <p className="text-lg font-display">Error fetching filters, please refresh page or select a different app to try again</p>}
+
+      {/* Create app when app exists but has no data */}
       {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.NoData && <CreateApp teamId={params.teamId} existingAppName={selectedApp.name} existingApiKey={selectedApp.api_key}/>}
+
+      {/* Show user flow if apps and filters fetch succeeds  */}
       {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <UserFlow appId={selectedApp.id} startDate={startDate} endDate={endDate} appVersion={selectedVersion} />}
       <div className="py-8" />
+      {/* Show metrics if apps and filters fetch succeeds  */}
       {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <MetricsOverview appId={selectedApp.id} startDate={startDate} endDate={endDate} appVersion={selectedVersion} />}
     </div>
   )
