a00d4bfba0e40b8c544e8b4b7fedcf94e019f1d0
Debjeet Biswas <debjeet12@gmail.com>
2023-12-04 04:40:25 +0530

    chore(webapp): add logout
    - add logout handler
    - modify existing auth utils

--- measure-web-app/app/[teamId]/team/page.tsx
+++ measure-web-app/app/[teamId]/team/page.tsx
@@ -3,6 +3,30 @@
 import Dropdown from "@/app/components/dropdown";
 import { useState } from "react";
 
+const getSelectedTeam = () => {
+  const { pathname } = new URL(document.location.href)
+  const fragments = pathname.split("/").filter(e => Boolean(e))
+  if (!fragments.length) return null
+  if (fragments.at(1) !== "team") return null
+  return fragments.at(0)
+}
+
+async function inviteMember(email: string, teamId: string, role: string) {
+  const res = await fetch("/auth/invite", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ email, teamId, role })
+  })
+  const json = await res.json()
+  if (!res.ok) {
+    alert(json.error)
+    return
+  }
+  alert(json.ok)
+}
+
 const teamMembers = [
   {
     id: 'asldkfjlk34343',
@@ -45,37 +69,48 @@ const teamMembers = [
 export default function Team({ params }: { params: { teamId: string } }) {
   const defaultTeamName = "Anup's team"
   const [saveTeamNameButtonDisabled, setSaveTeamNameButtonDisabled] = useState(true);
+  const [role, setRole] = useState("Owner")
+  const [email, setEmail] = useState<string>()
+
+  const handleInvite = (e: React.FormEvent) => {
+    e.preventDefault()
+    const teamId = getSelectedTeam()
+    inviteMember(email as string, teamId as string, role.toLowerCase())
+    setEmail(() => "")
+  }
 
   return (
     <div className="flex flex-col selection:bg-yellow-200/75 items-start p-24 pt-8">
-      <div className="py-4"/>
+      <div className="py-4" />
       <p className="font-display font-regular text-black text-4xl max-w-6xl text-center">Team</p>
-      <div className="py-4"/>
+      <div className="py-4" />
       <p className="font-sans text-black max-w-6xl text-center">Team name</p>
-      <div className="py-1"/>
+      <div className="py-1" />
       <div className="flex flex-row items-center">
-        <input id="change-team-name-input" type="text" defaultValue={defaultTeamName} onChange={(event) => event.target.value === defaultTeamName? setSaveTeamNameButtonDisabled(true): setSaveTeamNameButtonDisabled(false)} className="w-96 border border-black rounded-md outline-none focus-visible:outline-yellow-300 text-black py-2 px-4 font-sans placeholder:text-neutral-400"/>
+        <input id="change-team-name-input" type="text" defaultValue={defaultTeamName} onChange={(event) => event.target.value === defaultTeamName ? setSaveTeamNameButtonDisabled(true) : setSaveTeamNameButtonDisabled(false)} className="w-96 border border-black rounded-md outline-none focus-visible:outline-yellow-300 text-black py-2 px-4 font-sans placeholder:text-neutral-400" />
         <button disabled={saveTeamNameButtonDisabled} className="m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black disabled:border-gray-400 rounded-md font-display text-black disabled:text-gray-400 transition-colors duration-100 py-2 px-4">Save</button>
       </div>
-      <div className="py-4"/>
+      <div className="py-4" />
       <p className="font-sans text-black max-w-6xl text-center">Invite team members</p>
-      <div className="py-1"/>
-      <div className="flex flex-row items-center">
-        <input id="invite-email-input" type="text" placeholder="Enter email" className="w-96 border border-black rounded-md outline-none focus-visible:outline-yellow-300 text-black py-2 px-4 font-sans placeholder:text-neutral-400"/>
-        <div className="px-2"/>
-        <Dropdown items={['Owner', 'Admin','Developer', 'Viewer']}/>
-        <button className="m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4">Invite</button>
-      </div>
-      <div className="py-8"/>
+      <div className="py-1" />
+      <form name="invite-form" id="invite-form" autoComplete="on" onSubmit={handleInvite}>
+        <div className="flex flex-row items-center">
+          <input id="invite-email-input" name="invite-email-input" type="text" placeholder="Enter email" className="w-96 border border-black rounded-md outline-none focus-visible:outline-yellow-300 text-black py-2 px-4 font-sans placeholder:text-neutral-400" onInput={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} defaultValue={email} />
+          <div className="px-2" />
+          <Dropdown items={['Owner', 'Admin', 'Developer', 'Viewer']} onChangeSelectedItem={(item) => setRole(item)} initialItemIndex={0} />
+          <button form="invite-form" type="submit" className="m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4">Invite</button>
+        </div>
+      </form>
+      <div className="py-8" />
       <p className="font-display font-regular text-black text-2xl max-w-6xl text-center">Members</p>
-      <div className="py-2"/>
+      <div className="py-2" />
       <div className="table-row-group">
-        {teamMembers.map(({ id, email}) => (
-            <div key={id} className="table-row font-sans text-black">
-              <div className="table-cell p-4 pl-0 text-lg">{email}</div>
-              <div className="table-cell p-4 w-52"><Dropdown items={['Owner', 'Admin','Developer', 'Viewer']}/></div>
-              <div className="table-cell p-4"><button className="m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4">Remove</button></div>
-            </div>
+        {teamMembers.map(({ id, email }) => (
+          <div key={id} className="table-row font-sans text-black">
+            <div className="table-cell p-4 pl-0 text-lg">{email}</div>
+            <div className="table-cell p-4 w-52"><Dropdown items={['Owner', 'Admin', 'Developer', 'Viewer']} /></div>
+            <div className="table-cell p-4"><button className="m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4">Remove</button></div>
+          </div>
         ))}
       </div>
     </div>
