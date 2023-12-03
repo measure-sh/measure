e2910155655415eced4d85dd54ebd3631bb5b2ec
Debjeet Biswas <debjeet12@gmail.com>
2023-12-04 04:40:01 +0530

    chore(webapp): modify github signin

--- measure-web-app/app/auth/login/github-sign-in.tsx
+++ measure-web-app/app/auth/login/github-sign-in.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from "@/utils/supabase/browser";
 
 async function doGitHubLogin() {
   const url = new URL(window.location.href)
-  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+  const supabase = createBrowserClient()
   const { error } = await supabase.auth.signInWithOAuth({
     provider: 'github',
     options: {
