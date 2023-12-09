af492e9333eff0b9bc241a33e0329cfaa2af9d89
Abhay Sood <soodabhay23@gmail.com>
2023-12-09 11:56:55 +0530

    chore(backend): remove all clickhouse migrations
    merge all migration updates into a single create table migration

--- self-host/clickhouse/20231120133851_alter-events-relation.sql
+++
@@ -1,7 +0,0 @@
--- migrate:up
-ALTER TABLE default.events
-ADD COLUMN resource.device_locale FixedString(64);
-
--- migrate:down
-ALTER TABLE default.events
-DROP COLUMN resource.device_locale;
--- self-host/clickhouse/20231120185455_alter-events-relation.sql
+++
@@ -1,9 +0,0 @@
--- migrate:up
-ALTER TABLE default.events
-ADD COLUMN anr.device_locale FixedString(64),
-ADD COLUMN exception.device_locale FixedString(64);
-
--- migrate:down
-ALTER TABLE default.events
-DROP COLUMN anr.device_locale,
-DROP COLUMN exception.device_locale;
