4775cdc4bb05f248179c390068d37977e39cc474
Debjeet Biswas <debjeet12@gmail.com>
2023-12-13 07:37:30 +0530

    refactor(backend): update cipher pkg
    - move fnv1 checksum func inside cipher package
    - update func call site

--- measure-backend/measure-go/cipher/cipher.go
+++ measure-backend/measure-go/cipher/cipher.go
@@ -3,6 +3,9 @@ package cipher
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
+	"hash/fnv"
+	"io"
 )
 
 func InviteCode() (string, error) {
@@ -13,4 +16,14 @@ func InviteCode() (string, error) {
 	}
 
 	return hex.EncodeToString(bytes), nil
+}
+
+func ChecksumFnv1(r io.Reader) (string, error) {
+	h := fnv.New64()
+
+	if _, err := io.Copy(h, r); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", h.Sum(nil)), nil
 }
