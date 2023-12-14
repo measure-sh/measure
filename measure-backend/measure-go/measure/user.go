cac9051f06a8a0c2c1d956535afb40d96680ecc3
Debjeet Biswas <debjeet12@gmail.com>
2023-12-15 03:38:25 +0530

    fix(backend): handle existing & new invitee(s)
    - return error if any of invitee(s) are already a member of team
    - if present, make confirmed non-member users member of team
    - if present, return non-registered invitees for later invite execution
    
    close #277

--- measure-backend/measure-go/measure/user.go
+++ measure-backend/measure-go/measure/user.go
@@ -5,12 +5,16 @@ import (
 	"errors"
 	"fmt"
 	"measure-backend/measure-go/server"
+	"strings"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
+	"github.com/leporo/sqlf"
 )
 
 type User struct {
-	id string
+	id    string
+	email string
 }
 
 func (u *User) getTeams() ([]map[string]string, error) {
@@ -53,4 +57,63 @@ func (u *User) getRole(teamId string) (rank, error) {
 	}
 
 	return roleMap[role], nil
+}
+
+// GetUsersByInvitees provides existing & new invitess by matching
+// each user's and invitee's email
+func GetUsersByInvitees(invitees []Invitee) ([]Invitee, []Invitee, error) {
+	var oldUsers []Invitee
+	var newUsers []Invitee
+	var emailList string
+
+	var emails []string
+	for _, invitee := range invitees {
+		emails = append(emails, fmt.Sprintf("'%s'", invitee.Email))
+	}
+	emailList = strings.Join(emails, ", ")
+
+	stmt := sqlf.PostgreSQL.
+		Select("id, email").
+		From("auth.users").
+		Where(fmt.Sprintf("email in (%s)", emailList)).
+		Where("confirmed_at is not null").
+		Where("deleted_at is null").
+		Where("banned_until is null")
+
+	defer stmt.Close()
+
+	rows, _ := server.Server.PgPool.Query(context.Background(), stmt.String())
+	users, err := pgx.CollectRows(rows, func(row pgx.CollectableRow) (User, error) {
+		var user User
+		err := row.Scan(&user.id, &user.email)
+		return user, err
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	for _, invitee := range invitees {
+		for _, user := range users {
+			if strings.EqualFold(user.email, invitee.Email) {
+				uuid, err := uuid.Parse(user.id)
+				if err != nil {
+					return nil, nil, err
+				}
+				invitee.ID = uuid
+				oldUsers = append(oldUsers, invitee)
+			}
+		}
+		newInvitee := true
+		for _, user := range users {
+			if strings.EqualFold(user.email, invitee.Email) {
+				newInvitee = false
+			}
+		}
+
+		if newInvitee {
+			newUsers = append(newUsers, invitee)
+		}
+	}
+
+	return oldUsers, newUsers, nil
 }
