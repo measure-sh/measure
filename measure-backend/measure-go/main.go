971219c5d2eaa1f1eb0bd7b1a2dc032b41ce5498
Debjeet Biswas <debjeet12@gmail.com>
2024-01-02 16:14:18 +0530

    chore(backend): get anr filters api
    - add get `/apps/:id/filters/anrs` api

--- measure-backend/measure-go/main.go
+++ measure-backend/measure-go/main.go
@@ -57,6 +57,7 @@ func main() {
 	r.Use(cors).GET("/apps/:id/metrics", measure.ValidateAccessToken(), measure.GetAppMetrics)
 	r.Use(cors).GET("/apps/:id/filters", measure.ValidateAccessToken(), measure.GetAppFilters)
 	r.Use(cors).GET("/apps/:id/filters/crashes", measure.ValidateAccessToken(), measure.GetCrashFilters)
+	r.Use(cors).GET("/apps/:id/filters/anrs", measure.ValidateAccessToken(), measure.GetANRFilters)
 	r.Use(cors).GET("/apps/:id/crashGroups", measure.ValidateAccessToken(), measure.GetCrashGroups)
 	r.Use(cors).GET("/apps/:id/anrGroups", measure.ValidateAccessToken(), measure.GetANRGroups)
 	r.Use(cors).GET("/teams", measure.ValidateAccessToken(), measure.GetTeams)
