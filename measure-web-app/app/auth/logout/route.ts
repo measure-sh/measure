8741c42f2c356d77d9ece2f16a4ae141314eb391
Anup Cowkur <anupcowkur89@gmail.com>
2024-01-10 14:37:11 +0530

    chore(webapp): remove unnecessary logout call from route
    We are calling supabase logout API from client side. We do not
    need to call this on server logout route.

--- measure-web-app/app/auth/logout/route.ts
+++ measure-web-app/app/auth/logout/route.ts
@@ -1,17 +1,9 @@
-import { createRouteClient } from '@/utils/supabase/route'
 import { NextResponse } from 'next/server'
 
 export const dynamic = 'force-dynamic'
 
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
-  const supabase = createRouteClient()
-
-  const { error } = await supabase.auth.signOut()
-
-  if (error) {
-    console.log(`logout failed with error`, error)
-  }
 
   return NextResponse.redirect(`${requestUrl.origin}/auth/login`, {
     // using temporary redirect, so that browsers don't cache this
