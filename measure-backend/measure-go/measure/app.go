99e32e6f17174f40343f6b69010e621f52a7d0e0
Debjeet Biswas <debjeet12@gmail.com>
2024-01-12 13:59:02 +0530

    chore(backend): modify exception group query
    - only apply time range to exception group query when a time range is
      available

--- measure-backend/measure-go/measure/app.go
+++ measure-backend/measure-go/measure/app.go
@@ -92,8 +92,10 @@ func (a App) GetExceptionGroups(af *AppFilter) ([]ExceptionGroup, error) {
 	args := []any{a.ID}
 
 	if af != nil {
-		stmt.Where("created_at >= ? and created_at <= ?", nil, nil)
-		args = append(args, af.From, af.To)
+		if af.hasTimeRange() {
+			stmt.Where("created_at >= ? and created_at <= ?", nil, nil)
+			args = append(args, af.From, af.To)
+		}
 
 		if af.Version != "" {
 			stmt.Where("app_version = ?", nil)
