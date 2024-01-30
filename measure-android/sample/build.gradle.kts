91007a560ea1d36e48ae2660401991584e3fa59a
Abhay Sood <soodabhay23@gmail.com>
2024-01-30 21:52:03 +0530

    chore(android): update dependencies

--- measure-android/sample/build.gradle.kts
+++ measure-android/sample/build.gradle.kts
@@ -33,11 +33,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -51,7 +51,7 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor")
 
     implementation(project(":measure"))
-    debugImplementation ("com.squareup.leakcanary:leakcanary-android:2.12")
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.12")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
