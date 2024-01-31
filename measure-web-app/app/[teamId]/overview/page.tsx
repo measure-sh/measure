ced3a545646e84b8a955f85720080a361851b925
Anup Cowkur <anupcowkur89@gmail.com>
2024-01-31 19:48:56 +0530

    chore(webapp): refactor UI rendering in response to API statuses in Apps, Crashes & Overview pages

--- measure-web-app/app/[teamId]/overview/page.tsx
+++ measure-web-app/app/[teamId]/overview/page.tsx
@@ -92,60 +92,47 @@ export default function Overview({ params }: { params: { teamId: string } }) {
       <div className="py-4" />
       <p className="font-display font-regular text-4xl max-w-6xl text-center">Overview</p>
       <div className="py-4" />
-      <div className="flex flex-wrap gap-8 items-center">
-        {/* Loading message for apps */}
-        {appsApiStatus === AppsApiStatus.Loading && <p className="text-lg font-display">Updating Apps...</p>}
 
-        {/* Error message for apps fetch error */}
-        {appsApiStatus === AppsApiStatus.Error && <p className="text-lg font-display">Error fetching apps, please check if Team ID is valid or refresh page to try again</p>}
+      {/* Error states for apps fetch */}
+      {appsApiStatus === AppsApiStatus.Error && <p className="text-lg font-display">Error fetching apps, please check if Team ID is valid or refresh page to try again</p>}
+      {appsApiStatus === AppsApiStatus.NoApps &&
+        <div>
+          <p className="text-lg font-display">Looks like you don&apos;t have any apps yet. Get started by creating your first app!</p>
+          <div className="py-4" />
+          <CreateApp teamId={params.teamId} />
+        </div>}
 
-        {/* Create app when no apps exist */}
-        {appsApiStatus === AppsApiStatus.NoApps && <p className="text-lg font-display">Looks like you don&apos;t have any apps yet. Get started by creating your first app!</p>}
-        {appsApiStatus === AppsApiStatus.NoApps && <div className="py-4" />}
-        {appsApiStatus === AppsApiStatus.NoApps && <CreateApp teamId={params.teamId} />}
+      {/* Main UI */}
+      {appsApiStatus === AppsApiStatus.Success &&
+        <div>
+          <div className="flex flex-wrap gap-8 items-center">
+            <Dropdown items={apps.map((e) => e.name)} onChangeSelectedItem={(item) => setSelectedApp(apps.find((e) => e.name === item)!)} />
+            {filtersApiStatus === FiltersApiStatus.Success &&
+              <div className="flex flex-row items-center">
+                <input type="date" defaultValue={startDate} max={endDate} className="font-display border border-black rounded-md p-2" onChange={(e) => setStartDate(e.target.value)} />
+                <p className="font-display px-2">to</p>
+                <input type="date" defaultValue={endDate} min={startDate} className="font-display border border-black rounded-md p-2" onChange={(e) => setEndDate(e.target.value)} />
+              </div>}
+            {filtersApiStatus === FiltersApiStatus.Success && <Dropdown items={versions} onChangeSelectedItem={(item) => setSelectedVersion(item)} />}
+          </div>
+          <div className="py-4" />
 
-        {/* Show app selector dropdown if apps fetch succeeds */}
-        {appsApiStatus === AppsApiStatus.Success && <Dropdown items={apps.map((e) => e.name)} onChangeSelectedItem={(item) => setSelectedApp(apps.find((e) => e.name === item)!)} />}
+          {/* Error states for filters fetch */}
+          {filtersApiStatus === FiltersApiStatus.Error && <p className="text-lg font-display">Error fetching filters, please refresh page or select a different app to try again</p>}
+          {filtersApiStatus === FiltersApiStatus.NoData && <p className="text-lg font-display">We don&apos;t seem to have any data for this app. It could have been removed due to exceeding data retention period. Please contact <a href="mailto:support@measure.sh" className="underline text-blue-500">Measure support.</a></p>}
+          {filtersApiStatus === FiltersApiStatus.NotOnboarded && <CreateApp teamId={params.teamId} existingAppName={selectedApp.name} existingApiKey={selectedApp.api_key.key} />}
 
-        {/* Show date selector if apps and filters fetch succeeds */}
-        {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success &&
-          <div className="flex flex-row items-center">
-            <input type="date" defaultValue={startDate} max={endDate} className="font-display border border-black rounded-md p-2" onChange={(e) => setStartDate(e.target.value)} />
-            <p className="font-display px-2">to</p>
-            <input type="date" defaultValue={endDate} min={startDate} className="font-display border border-black rounded-md p-2" onChange={(e) => setEndDate(e.target.value)} />
-          </div>}
-
-        {/* Loading message for filters */}
-        {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Loading && <p className="text-lg font-display">Updating filters...</p>}
-
-        {/* Show versions selector if apps and filters fetch succeeds  */}
-        {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <Dropdown items={versions} onChangeSelectedItem={(item) => setSelectedVersion(item)} />}
-      </div>
-
-      {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <div className="py-4" />}
-
-      {/* Show filter pills if apps and filters fetch succeeds  */}
-      <div className="flex flex-wrap gap-2 items-center w-5/6">
-        {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <FilterPill title={selectedApp.name} />}
-        {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <FilterPill title={`${formattedStartDate} to ${formattedEndDate}`} />}
-        {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <FilterPill title={selectedVersion} />}
-      </div>
-      <div className="py-8" />
-
-      {/* Filters fetch error message  */}
-      {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Error && <p className="text-lg font-display">Error fetching filters, please refresh page or select a different app to try again</p>}
-
-      {/* Filters fetch no data message  */}
-      {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.NoData && <p className="text-lg font-display">We don&apos;t seem to have any data for this app. It could have been removed due to exceeding data retention period. Please contact <a href="mailto:support@measure.sh" className="underline text-blue-500">Measure support.</a></p>}
-
-      {/* Create app when app exists but is not onboarded */}
-      {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.NotOnboarded && <CreateApp teamId={params.teamId} existingAppName={selectedApp.name} existingApiKey={selectedApp.api_key.key} />}
-
-      {/* Show user flow if apps and filters fetch succeeds  */}
-      {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <Journey appId={selectedApp.id} startDate={startDate} endDate={endDate} appVersion={selectedVersion} />}
-      <div className="py-8" />
-      {/* Show metrics if apps and filters fetch succeeds  */}
-      {appsApiStatus === AppsApiStatus.Success && filtersApiStatus === FiltersApiStatus.Success && <MetricsOverview appId={selectedApp.id} startDate={startDate} endDate={endDate} appVersion={selectedVersion} />}
+          {filtersApiStatus === FiltersApiStatus.Success &&
+            <div className="flex flex-wrap gap-2 items-center w-5/6">
+              <FilterPill title={selectedApp.name} />
+              <FilterPill title={`${formattedStartDate} to ${formattedEndDate}`} />
+              <FilterPill title={selectedVersion} />
+            </div>}
+          <div className="py-8" />
+          {filtersApiStatus === FiltersApiStatus.Success && <Journey appId={selectedApp.id} startDate={startDate} endDate={endDate} appVersion={selectedVersion} />}
+          <div className="py-8" />
+          {filtersApiStatus === FiltersApiStatus.Success && <MetricsOverview appId={selectedApp.id} startDate={startDate} endDate={endDate} appVersion={selectedVersion} />}
+        </div>}
     </div>
   )
 }
