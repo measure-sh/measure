52be2b6686f4c21d9cce6e6f1762e6015eb79b80
Anup Cowkur <anupcowkur89@gmail.com>
2024-02-12 13:18:51 +0530

    feat(webapp): use paginated crashes API in Crashes page

--- measure-web-app/app/components/paginator.tsx
+++ measure-web-app/app/components/paginator.tsx
@@ -3,18 +3,24 @@
 interface PaginatorProps {
   rangeStart: number,
   rangeEnd: number,
-  prevDisabled: boolean,
-  nextDisabled: boolean,
+  prevEnabled: boolean,
+  nextEnabled: boolean,
   onNext: () => void,
   onPrev: () => void;
 }
 
-const Paginator: React.FC<PaginatorProps> = ({ rangeStart, rangeEnd, prevDisabled, nextDisabled, onNext, onPrev }) => {
+export enum PaginationDirection {
+  Forward,
+  Backward,
+  None
+}
+
+const Paginator: React.FC<PaginatorProps> = ({ rangeStart, rangeEnd, prevEnabled, nextEnabled, onNext, onPrev }) => {
   return (
     <div className="flex flex-row items-center justify-self-center">
-      <button disabled={prevDisabled} className="m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black disabled:border-gray-400 rounded-md font-display disabled:text-gray-400 transition-colors duration-100 py-2 px-4" onClick={onPrev}>Previous</button>
+      <button disabled={!prevEnabled} className="m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black disabled:border-gray-400 rounded-md font-display disabled:text-gray-400 transition-colors duration-100 py-2 px-4" onClick={onPrev}>Previous</button>
       <p className="font-display">{rangeStart} - {rangeEnd}</p>
-      <button disabled={nextDisabled} className="m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black disabled:border-gray-400 rounded-md font-display disabled:text-gray-400 transition-colors duration-100 py-2 px-4" onClick={onNext}>Next</button>
+      <button disabled={!nextEnabled} className="m-4 outline-none flex justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black disabled:border-gray-400 rounded-md font-display disabled:text-gray-400 transition-colors duration-100 py-2 px-4" onClick={onNext}>Next</button>
     </div>
   );
 };
