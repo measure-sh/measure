8d5b6571b07de6411b727a888e93529cf0ebf053
Abhay Sood <soodabhay23@gmail.com>
2024-02-14 17:18:11 +0530

    chore(android): fix typo in compose theme name

--- measure-android/sample/src/main/java/sh/measure/sample/ui/theme/Theme.kt
+++ measure-android/sample/src/main/java/sh/measure/sample/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun MeasureandroidTheme(
+fun MeasureTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
