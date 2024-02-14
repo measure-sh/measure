5f90b49d46c16d1002cd2eef27970e977051eb60
Abhay Sood <soodabhay23@gmail.com>
2024-02-14 17:18:11 +0530

    chore(android): fix typo in compose theme name

--- measure-android/sample/src/main/java/sh/measure/sample/ComposeNavigationActivity.kt
+++ measure-android/sample/src/main/java/sh/measure/sample/ComposeNavigationActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import sh.measure.sample.ui.theme.MeasureandroidTheme
+import sh.measure.sample.ui.theme.MeasureTheme
 
 class ComposeNavigationActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MeasureandroidTheme {
+            MeasureTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MeasureandroidTheme {
+    MeasureTheme {
         Greeting("Android")
     }
 }
