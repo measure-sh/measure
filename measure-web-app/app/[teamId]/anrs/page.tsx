de4b757b8c45defb07acc3f570af9e3e203eab2a
Anup Cowkur <anupcowkur89@gmail.com>
2024-02-21 16:35:26 +0530

    feat(webapp): implement ANRs overview and details

--- measure-web-app/app/[teamId]/anrs/page.tsx
+++ measure-web-app/app/[teamId]/anrs/page.tsx
@@ -1,11 +1,11 @@
-export default function ANRs({ params }: { params: { teamId: string } }) {
+"use client"
+
+import React from 'react';
+import { CrashesOrAnrsOverview } from '@/app/components/crashes_or_anrs_overview';
+import { CrashOrAnrType } from '@/app/api/api_calls';
+
+export default function AnrsOverview({ params }: { params: { teamId: string } }) {
   return (
-    <main className="flex flex-col items-center justify-between selection:bg-yellow-200/75">
-      <div className="flex flex-col items-center md:w-4/5 2xl:w-3/5 px-16">
-        <div className="py-24" />
-        <p>ANRs</p>
-        <div className="py-24" />
-      </div>
-    </main>
+    <CrashesOrAnrsOverview crashOrAnrType={CrashOrAnrType.Anr} teamId={params.teamId} />
   )
 }
