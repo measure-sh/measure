61cb7370ce21b03a7dfd9082b4d4143055d7bf9b
Anup Cowkur <anupcowkur89@gmail.com>
2024-02-29 16:54:38 +0530

    fix(webapp): remove unneeded IDs in DangerConfirmationModal comoponent

--- measure-web-app/app/components/danger_confirmation_modal.tsx
+++ measure-web-app/app/components/danger_confirmation_modal.tsx
@@ -13,15 +13,15 @@ interface DangerConfirmationModalProps {
 
 const DangerConfirmationModal: React.FC<DangerConfirmationModalProps> = ({ body, open, affirmativeText, cancelText, onAffirmativeAction, onCancelAction }) => {
     return (
-        <div id="popup-DangerConfirmationmodal" tabIndex={-1} className={`flex overflow-y-auto bg-neutral-950 bg-opacity-90 overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-full ${open ? '' : 'hidden'}`}>
+        <div tabIndex={-1} className={`flex overflow-y-auto bg-neutral-950 bg-opacity-90 overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-full ${open ? '' : 'hidden'}`}>
             <div className="w-fit">
                 <div className="flex flex-col bg-white rounded-lg border border-black py-4 px-8">
                     {body}
                     <div className="py-2" />
                     <div className="flex flex-row">
-                        <button data-DangerConfirmationmodal-hide="popup-DangerConfirmationmodal" type="button" className="outline-none bg-red-600 hover:bg-red-700 focus-visible:bg-red-700 active:bg-red-800 font-display text-white rounded-md border border-black transition-colors duration-100 py-2 px-4" onClick={onAffirmativeAction}>{affirmativeText}</button>
+                        <button type="button" className="outline-none bg-red-600 hover:bg-red-700 focus-visible:bg-red-700 active:bg-red-800 font-display text-white rounded-md border border-black transition-colors duration-100 py-2 px-4" onClick={onAffirmativeAction}>{affirmativeText}</button>
                         <div className="px-1" />
-                        <button data-DangerConfirmationmodal-hide="popup-DangerConfirmationmodal" type="button" className="outline-none hover:bg-yellow-200 focus-visible:bg-yellow-200 active:bg-yellow-300 font-display border border-black rounded-md transition-colors duration-100 py-2 px-4" onClick={onCancelAction}>{cancelText}</button>
+                        <button type="button" className="outline-none hover:bg-yellow-200 focus-visible:bg-yellow-200 active:bg-yellow-300 font-display border border-black rounded-md transition-colors duration-100 py-2 px-4" onClick={onCancelAction}>{cancelText}</button>
                     </div>
                 </div>
             </div>
