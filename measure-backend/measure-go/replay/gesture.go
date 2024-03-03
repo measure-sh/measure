06e589f609af24b3a62c7224bb2df3cd054991d3
Debjeet Biswas <debjeet12@gmail.com>
2024-03-04 03:19:39 +0530

    chore(backend): fix a typo

--- measure-backend/measure-go/replay/gesture.go
+++ measure-backend/measure-go/replay/gesture.go
@@ -10,7 +10,7 @@ import (
 type GestureClick struct {
 	*event.GestureClick
 	ThreadName string            `json:"-"`
-	Timestmap  time.Time         `json:"timestamp"`
+	Timestamp  time.Time         `json:"timestamp"`
 	Attributes map[string]string `json:"attributes"`
 }
 
