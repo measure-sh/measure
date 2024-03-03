93b67293a1c01d1112b8d3a7d285317d300f477a
Debjeet Biswas <debjeet12@gmail.com>
2024-03-03 19:40:50 +0530

    chore(backend): add `lifecycle_fragment` event
    - add `lifecycle_fragment` events grouped by threads in session replay
      api response
    
    fixes #439

--- measure-backend/measure-go/replay/lifecycle.go
+++ measure-backend/measure-go/replay/lifecycle.go
@@ -20,6 +20,21 @@ func (la LifecycleActivity) GetThreadName() string {
 	return la.ThreadName
 }
 
+// LifecycleFragment represents lifecycle
+// fragment events suitable for session replay.
+type LifecycleFragment struct {
+	*event.LifecycleFragment
+	ThreadName string            `json:"-"`
+	Timestamp  time.Time         `json:"timestamp"`
+	Attributes map[string]string `json:"attributes"`
+}
+
+// GetThreadName provides the name of the thread
+// where lifecycle fragment event took place.
+func (lf LifecycleFragment) GetThreadName() string {
+	return lf.ThreadName
+}
+
 // ComputeLifecycleActivities computes lifecycle
 // activity events for session replay.
 func ComputeLifecycleActivities(events []event.EventField) (result []ThreadGrouper) {
@@ -32,6 +47,23 @@ func ComputeLifecycleActivities(events []event.EventField) (result []ThreadGroup
 			event.Attributes,
 		}
 		result = append(result, activities)
+	}
+
+	return
+}
+
+// ComputeLifecycleFragments computes lifecycle
+// fragment events for session replay.
+func ComputeLifecycleFragments(events []event.EventField) (result []ThreadGrouper) {
+	for _, event := range events {
+		event.LifecycleFragment.Trim()
+		fragments := LifecycleFragment{
+			&event.LifecycleFragment,
+			event.ThreadName,
+			event.Timestamp,
+			event.Attributes,
+		}
+		result = append(result, fragments)
 	}
 
 	return
