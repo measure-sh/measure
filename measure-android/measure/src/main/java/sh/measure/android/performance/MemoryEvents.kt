162f7ca1fc80888df095f14b98865fd02b9ac8cc
Abhay Sood <soodabhay23@gmail.com>
2024-03-06 11:30:34 +0530

    chore(android): track memory along with low memory event
    Introduces a new MemoryReader class
    which extracts the logic to read memory
    properties from multiple sources and
    allows reusing the logic in both MemoryUsageCollector & ComponentCallbacksCollector

--- measure-android/measure/src/main/java/sh/measure/android/performance/MemoryEvents.kt
+++ measure-android/measure/src/main/java/sh/measure/android/performance/MemoryEvents.kt
@@ -19,6 +19,13 @@ internal data class MemoryUsage(
 
 @Serializable
 internal data class LowMemory(
+    val java_max_heap: Long,
+    val java_total_heap: Long,
+    val java_free_heap: Long,
+    val total_pss: Int,
+    val rss: Long?,
+    val native_total_heap: Long,
+    val native_free_heap: Long,
     @Transient
     val timestamp: Long = 0L,
     @Transient
