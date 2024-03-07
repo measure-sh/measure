8272dd47092c5c38848b12d4f78f9d07c89b46cc
Abhay Sood <soodabhay23@gmail.com>
2024-03-07 19:52:22 +0530

    perf(android): improve perf of converting current time to ISO format

--- measure-android/measure/src/main/java/sh/measure/android/utils/TimeProvider.kt
+++ measure-android/measure/src/main/java/sh/measure/android/utils/TimeProvider.kt
@@ -1,7 +1,11 @@
 package sh.measure.android.utils
 
+import android.os.Build
 import android.os.SystemClock
 import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 import java.util.Calendar
 import java.util.Locale
 import java.util.TimeZone
@@ -54,10 +58,31 @@ internal class AndroidTimeProvider : TimeProvider {
     }
 }
 
+private val simpleDateFormat by lazy(LazyThreadSafetyMode.NONE) {
+    SimpleDateFormat(
+        "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSS'Z'",
+        Locale.getDefault(),
+    )
+}
+
+private val dateTimeFormatter by lazy(LazyThreadSafetyMode.NONE) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSS'Z'").withZone(ZoneOffset.UTC)
+    } else {
+        throw IllegalStateException("DateTimeFormatter is not supported on this platform")
+    }
+}
+
 internal fun Long.iso8601Timestamp(): String {
-    val calendar: Calendar = Calendar.getInstance(TimeZone.getTimeZone(TimeZone.getDefault().id))
-    calendar.timeInMillis = this
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSS'Z'", Locale.getDefault())
-    dateFormat.timeZone = TimeZone.getTimeZone("UTC")
-    return dateFormat.format(calendar.time)
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val formatter = dateTimeFormatter
+        val instant = Instant.ofEpochMilli(this)
+        formatter.format(instant)
+    } else {
+        val calendar: Calendar =
+            Calendar.getInstance(TimeZone.getTimeZone(TimeZone.getDefault().id))
+        calendar.timeInMillis = this
+        simpleDateFormat.timeZone = TimeZone.getTimeZone("UTC")
+        simpleDateFormat.format(calendar.time)
+    }
 }
