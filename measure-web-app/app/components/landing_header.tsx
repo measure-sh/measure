8744b6fbf499624f8fe2e50feed4047857189343
Anup Cowkur <anupcowkur89@gmail.com>
2024-03-07 20:51:26 +0530

    chore(webapp): remove unused imports

--- measure-web-app/app/components/landing_header.tsx
+++ measure-web-app/app/components/landing_header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef, useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/navigation'
 import { useScrollDirection } from '../utils/scroll_utils';
 
