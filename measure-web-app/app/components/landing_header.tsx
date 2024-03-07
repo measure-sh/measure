af0edf024a0e8880fcef1f646201cad8362efe17
Anup Cowkur <anupcowkur89@gmail.com>
2024-03-07 20:50:39 +0530

    chore(webapp): extract scroll direction detection into a util function

--- measure-web-app/app/components/landing_header.tsx
+++ measure-web-app/app/components/landing_header.tsx
@@ -2,41 +2,7 @@
 
 import { useRef, useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation'
-
-function useScrollDirection() {
-  const [scrollDir, setScrollDir] = useState('scrolling up');
-
-  useEffect(() => {
-    const threshold = 0;
-    let lastScrollY = window.pageYOffset;
-    let animating = false;
-
-    const updateScrollDir = () => {
-      const scrollY = window.pageYOffset;
-
-      if (Math.abs(scrollY - lastScrollY) < threshold) {
-        animating = false;
-        return;
-      }
-      setScrollDir(scrollY > lastScrollY ? 'scrolling down' : 'scrolling up');
-      lastScrollY = scrollY > 0 ? scrollY : 0;
-      animating = false;
-    };
-
-    const onScroll = () => {
-      if (!animating) {
-        window.requestAnimationFrame(updateScrollDir);
-        animating = true;
-      }
-    };
-
-    window.addEventListener('scroll', onScroll);
-
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
-
-  return scrollDir;
-}
+import { useScrollDirection } from '../utils/scroll_utils';
 
 export default function LandingHeader() {
   const scrollDir = useScrollDirection();
