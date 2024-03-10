9bd5ad09f847ecac6b4a198230f4e1ed7b154615
Abhay Sood <soodabhay23@gmail.com>
2024-03-10 10:27:38 +0530

    chore(android): add android test

---
+++ measure-android/measure/src/androidTest/java/sh/measure/android/utils/ProcessImportanceTest.kt
@@ -0,0 +1,28 @@
+package sh.measure.android.utils
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import sh.measure.android.TestActivity
+
+@RunWith(AndroidJUnit4::class)
+class ProcessImportanceTest {
+
+    @Test
+    fun returnsFalseWhenAppIsInBackground() {
+        ActivityScenario.launch(TestActivity::class.java)
+        Espresso.pressBackUnconditionally()
+        val result = isForegroundProcess()
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun returnsTrueWhenAppIsInForeground() {
+        ActivityScenario.launch(TestActivity::class.java)
+        val result = isForegroundProcess()
+        assertEquals(true, result)
+    }
+}
