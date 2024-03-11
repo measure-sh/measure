8368d1de77145ef6a9b634e25dc62f39f677ad03
Debjeet Biswas <debjeet12@gmail.com>
2024-03-11 14:25:27 +0530

    chore(backend): add `build_type` column
    - add `build_type` column in `public.build_sizes` relation
    - update schema dump

--- self-host/postgres/20240311054505_create-build_sizes-relation.sql
+++ self-host/postgres/20240311054505_create-build_sizes-relation.sql
@@ -5,6 +5,7 @@ create table if not exists public.build_sizes (
     version_name varchar(256) not null,
     version_code varchar(256) not null,
     build_size int default 0,
+    build_type varchar(64) not null,
     created_at timestamptz not null default now()
 );
 
@@ -13,6 +14,7 @@ comment on column public.build_sizes.app_id is 'linked app id';
 comment on column public.build_sizes.version_name is 'user visible version number of the app';
 comment on column public.build_sizes.version_code is 'incremental build number of the app';
 comment on column public.build_sizes.build_size is 'build size of the app';
+comment on column public.build_sizes.build_type is 'type of build. can be `aab` or `apk`';
 comment on column public.build_sizes.created_at is 'utc timestamp at the time of record creation';
 
 -- migrate:down
