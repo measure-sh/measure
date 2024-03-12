9c29b559e21c7e6b8a539815a1e9644c35d68db2
Debjeet Biswas <debjeet12@gmail.com>
2024-03-12 09:54:15 +0530

    chore(backend): support build info in sessionator
    - modify sessionator to read build info from `build.toml` files
    - refactor all doc comments and code to reflect the idea of build info
      instead of singular mapping files
    - improve general validation and error handling
    
    fixes #463

---
+++ self-host/sessionator/cmd/build.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"fmt"
+)
+
+// BuildInfo represents build metadata for an
+// app version's build.
+type BuildInfo struct {
+	Size uint32 `toml:"size"`
+	Type string `toml:"type"`
+}
+
+// GetSize returns string representation of
+// build's size.
+func (b BuildInfo) GetSize() string {
+	return fmt.Sprintf(`%d`, b.Size)
+}
