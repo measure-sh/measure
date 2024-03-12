8824f3ef028ab4a1e76f28325e3d7f60210ee961
Abhay Sood <soodabhay23@gmail.com>
2024-03-12 16:42:50 +0530

    feat(android): add gradle task to calculate app size

--- measure-android/measure-android-gradle/src/main/kotlin/sh/measure/AppSizeTask.kt
+++ measure-android/measure-android-gradle/src/main/kotlin/sh/measure/AppSizeTask.kt
@@ -1,25 +1,110 @@
 package sh.measure
 
+import com.android.SdkConstants
+import com.android.build.gradle.internal.SdkLocator
+import com.android.builder.errors.DefaultIssueReporter
+import com.android.prefs.AndroidLocationsSingleton
+import com.android.repository.api.ProgressIndicatorAdapter
+import com.android.sdklib.repository.AndroidSdkHandler
+import com.android.tools.apk.analyzer.internal.GzipSizeCalculator
+import com.android.tools.build.bundletool.androidtools.Aapt2Command
+import com.android.tools.build.bundletool.commands.BuildApksCommand
+import com.android.tools.build.bundletool.commands.GetSizeCommand
+import com.android.utils.StdLogger
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+import java.io.File
+import java.io.PrintStream
+import java.nio.file.Path
 
-abstract class AppSizeTask: DefaultTask() {
-
+/**
+ * Task to measure the size of an app bundle or APK.
+ *
+ * APK size is measured using Apk Analyzer, while AAB size is measured using bundletool.
+ * APK size represents the download size of the APK, while AAB size represents the minimum and
+ * maximum size of the APK splits generated from the AAB.
+ */
+abstract class AppSizeTask : DefaultTask() {
     @get:InputFile
     abstract val bundleFileProperty: RegularFileProperty
 
+    @get:InputFile
+    abstract val apkFileProperty: RegularFileProperty
+
+    @get:OutputDirectory
+    abstract val buildApksOutputDirProperty: DirectoryProperty
+
     @get:OutputFile
-    abstract val appSizeFileProperty: RegularFileProperty
+    abstract val appSizeOutputFileProperty: RegularFileProperty
 
     @TaskAction
-    fun measureAppSize() {
-        val bundleFile = bundleFileProperty.get().asFile
-        val appSizeFile = appSizeFileProperty.get().asFile
+    fun calculateAppSize() {
+        val apkFile = apkFileProperty.getOrNull()?.asFile
+        val bundleFile = bundleFileProperty.getOrNull()?.asFile
+        val appSizeOutputFile = appSizeOutputFileProperty.get().asFile
 
-        val appSize = bundleFile.length()
-        appSizeFile.writeText(appSize.toString())
+        if (bundleFile != null) {
+            writeAabSize(bundleFile, appSizeOutputFile)
+        }
+        if (apkFile != null) {
+            writeApkSize(apkFile, appSizeOutputFile)
+        }
+    }
+
+    private fun writeApkSize(apkFile: File, apkSizeOutputFile: File) {
+        val size = GzipSizeCalculator().getFullApkDownloadSize(apkFile.toPath())
+        apkSizeOutputFile.writeText("$size")
+    }
+
+    private fun writeAabSize(bundleFile: File, appSizeOutputFile: File) {
+        // Effectively runs the following command:
+        // bundletool build-apks --bundle=app.aab --output=bundle.apks
+        val path = BuildApksCommand.builder().setBundlePath(bundleFile.toPath())
+            .setOutputFile(buildApksOutputDirProperty.asFile.get().toPath())
+            .setAapt2Command(Aapt2Command.createFromExecutablePath(getAapt2Location()))
+            .setOutputFormat(BuildApksCommand.OutputFormat.APK_SET).build().execute()
+
+        // Effectively runs the following command:
+        // bundletool get-size total --apks=~bundle.apks
+        // The output is of the format:
+        // MIN,MAX
+        // 323,456
+        GetSizeCommand.builder().setApksArchivePath(path)
+            .setGetSizeSubCommand(GetSizeCommand.GetSizeSubcommand.TOTAL).build()
+            .getSizeTotal(PrintStream(appSizeOutputFile.outputStream()))
+        rewriteOutputToMaxSize(appSizeOutputFile)
+    }
+
+    /**
+     * Rewrites the output from GetSizeCommand to only include the maximum size.
+     */
+    private fun rewriteOutputToMaxSize(appSizeOutputFile: File) {
+        val lines = appSizeOutputFile.readLines()
+        appSizeOutputFile.writeText(lines[1].split(",")[1])
+    }
+
+    /**
+     * Finds and returns the location of the aapt2 executable.
+     */
+    private fun getAapt2Location(): Path {
+        val sdkLocation = getAndroidSdkLocation()
+        val sdkHandler = AndroidSdkHandler.getInstance(AndroidLocationsSingleton, sdkLocation)
+        val progressIndicator = object : ProgressIndicatorAdapter() {}
+        val buildToolInfo = sdkHandler.getLatestBuildTool(progressIndicator, true)
+        return buildToolInfo.location.resolve(SdkConstants.FN_AAPT2)
+    }
+
+    /**
+     * Finds and returns the location of the Android SDK.
+     */
+    private fun getAndroidSdkLocation(): Path {
+        val logger = StdLogger(StdLogger.Level.WARNING)
+        val issueReporter = DefaultIssueReporter(logger)
+        return SdkLocator.getSdkDirectory(project.rootDir, issueReporter).toPath()
     }
 }
