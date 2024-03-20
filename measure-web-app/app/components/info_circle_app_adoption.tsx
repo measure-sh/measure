a985ccf6a39aa524b1cef78a4ab4efbe29c9da59
Anup Cowkur <anupcowkur89@gmail.com>
2024-03-20 15:43:35 +0530

    feat(webapp): update overview page to show session metrics
    - change all 'user' labels to 'sessions'
    - remove multiple Crash free users and multiple ANR free user metrics

--- measure-web-app/app/components/info_circle_app_adoption.tsx
+++ measure-web-app/app/components/info_circle_app_adoption.tsx
@@ -4,14 +4,14 @@ import { MetricsApiStatus } from '../api/api_calls';
 interface InfoCircleAppAdoptionProps {
   status: MetricsApiStatus,
   value: number,
-  users: number,
-  totalUsers: number,
+  sessions: number,
+  totalSessions: number,
   title: string
 }
 
 const formatter = Intl.NumberFormat('en', { notation: 'compact' });
 
-const InfoCircleAppAdoption = ({ status, value, users, totalUsers, title }: InfoCircleAppAdoptionProps) => {
+const InfoCircleAppAdoption = ({ status, value, sessions, totalSessions, title }: InfoCircleAppAdoptionProps) => {
   return (
     <div className="flex flex-col items-center">
       <div className={`flex flex-col group relative items-center justify-center w-64 aspect-square rounded-full border border-black border-4 hover:bg-neutral-800/25 transition-colors duration-100`}>
@@ -19,9 +19,9 @@ const InfoCircleAppAdoption = ({ status, value, users, totalUsers, title }: Info
         {status === MetricsApiStatus.Error && <p className="font-display text-lg">Error</p>}
         {status === MetricsApiStatus.Success && <p className="font-sans text-xl"> {value}%</p>}
         <div className="py-1" />
-        {status === MetricsApiStatus.Success && <p className="font-sans text-sm">{formatter.format(users)}/{formatter.format(totalUsers)} users</p>}
+        {status === MetricsApiStatus.Success && <p className="font-sans text-sm">{formatter.format(sessions)}/{formatter.format(totalSessions)} users</p>}
         <span className="pointer-events-none z-50 max-w-xl absolute font-sans text-sm text-white rounded-md p-4 bg-neutral-800 -top-28 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100">
-          Adoption =  (Users of selected app version / Users of all app versions) * 100
+          Adoption =  (Sessions of selected app version / Sessions of all app versions) * 100
         </span>
       </div>
       <div className="py-2" />
