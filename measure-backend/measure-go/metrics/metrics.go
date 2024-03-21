a0032ebeb1b1fb32cb8eecc0963aa10c756f224f
Debjeet Biswas <debjeet12@gmail.com>
2024-03-21 11:54:53 +0530

    chore(backend): modify adoption metric

--- measure-backend/measure-go/metrics/metrics.go
+++ measure-backend/measure-go/metrics/metrics.go
@@ -1,11 +1,11 @@
 package metrics
 
-// AdoptionMetric represents compute result of an
-// app's adoption metrics.
-type AdoptionMetric struct {
-	AllAppVersions     uint64  `json:"all_app_versions"`
-	SelectedAppVersion uint64  `json:"selected_app_version"`
-	Adoption           float64 `json:"adoption"`
+// SessionAdoption represents computation result of an
+// app's session adoption metrics.
+type SessionAdoption struct {
+	AllVersions     uint64  `json:"all_versions"`
+	SelectedVersion uint64  `json:"selected_version"`
+	Adoption        float64 `json:"adoption"`
 }
 
 // SizeMetric represents compute result of an app's
