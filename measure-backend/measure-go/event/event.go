a7b9d20961376145cf8ea0d106ff37404febbc46
Debjeet Biswas <debjeet12@gmail.com>
2024-03-24 03:20:27 +0530

    refactor(backend): trim gesture scroll
    - trim gesture scroll event at query time
    
    fixes #563

--- measure-backend/measure-go/event/event.go
+++ measure-backend/measure-go/event/event.go
@@ -227,14 +227,6 @@ type GestureScroll struct {
 	Direction     string  `json:"direction"`
 }
 
-// Trim removes null bytes from the scroll
-// event's string fields.
-func (gs *GestureScroll) Trim() {
-	gs.Target = text.TrimFixedString(gs.Target)
-	gs.TargetID = text.TrimFixedString(gs.TargetID)
-	gs.Direction = text.TrimFixedString(gs.Direction)
-}
-
 type GestureClick struct {
 	Target        string  `json:"target"`
 	TargetID      string  `json:"target_id"`
