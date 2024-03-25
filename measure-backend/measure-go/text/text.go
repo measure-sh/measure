b3c01a0e8bc17d1f0b30e987618d110dc2eb06be
Debjeet Biswas <debjeet12@gmail.com>
2024-03-25 14:52:18 +0530

    refactor(backend): update text pkg
    - remove trim fixed string function from text pkg
    
    fixes #563

--- measure-backend/measure-go/text/text.go
+++ measure-backend/measure-go/text/text.go
@@ -14,10 +14,3 @@ func JoinNonEmptyStrings(delim string, strs ...string) string {
 	}
 	return strings.Join(nonEmptyStrs, delim)
 }
-
-// TrimFixedString trims null characters from
-// right side of the string and returns the
-// trimmed string.
-func TrimFixedString(s string) string {
-	return strings.TrimRight(s, "\x00")
-}
