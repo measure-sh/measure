f64d1d8660615e5df50980cb3f6ffa899e633f94
Anup Cowkur <anupcowkur89@gmail.com>
2024-03-25 16:39:47 +0530

    feat(webapp): update journey to flow graph in overview page

--- measure-web-app/app/components/journey.tsx
+++ measure-web-app/app/components/journey.tsx
@@ -1,10 +1,16 @@
 "use client"
 
-import React, { useState, useEffect, FunctionComponent } from 'react';
-import { NodeTooltipProps } from '@nivo/network';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 import { AppVersion, JourneyApiStatus, emptyJourney, fetchJourneyFromServer } from '../api/api_calls';
-import { ResponsiveNetwork } from '@nivo/network';
+import Dagre from '@dagrejs/dagre';
+import ReactFlow, {
+  useNodesState,
+  useEdgesState,
+  Position,
+  Handle,
+} from 'reactflow';
+import 'reactflow/dist/style.css';
 
 interface JourneyProps {
   appId: string,
@@ -14,60 +20,159 @@ interface JourneyProps {
 }
 
 type Node = {
-  id: string;
-  nodeColor: string;
-  height: number;
-  size: number;
-  issues: {
-    crashes: { title: string; count: number }[];
-    anrs: { title: string; count: number }[];
-  };
+  id: string
+  type: string | undefined
+  width: number
+  height: number
+  position: { x: number, y: number }
+  sourcePosition: Position | undefined
+  targetPosition: Position | undefined
+  data: {
+    label: string
+    issues: {
+      crashes: { title: string; count: number }[];
+      anrs: { title: string; count: number }[];
+    }
+  }
+}
+
+type Edge = {
+  id: string
+  source: string
+  target: string
+  sourceHandle: string
+  style: {
+    strokeWidth: number
+    stroke: string
+  }
+  label: string
+  animated: boolean
+}
+
+const nodeTypes = {
+  measureNode: MeasureNode,
 };
 
-const NodeTooltip: React.FC<NodeTooltipProps<Node>> = ({ node }) => {
-  const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+const formatter = Intl.NumberFormat('en', { notation: 'compact' });
 
+{/* @ts-ignore */ }
+function MeasureNode({ data, isConnectable }) {
   return (
-    <div className="pointer-events-none z-50 rounded-md p-4 bg-neutral-800">
-      <p className="font-sans text-white">{node.id}</p>
-      {node.data.issues.crashes.length > 0 && (
-        <div>
-          <div className="py-2" />
-          <p className="font-sans text-white">Crashes:</p>
-          <ul className="list-disc">
-            {node.data.issues.crashes.map(({ title, count }) => (
-              <li key={title}>
-                <span className="font-sans text-white text-xs">
-                  {title} - {formatter.format(count)}
-                </span>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-      {node.data.issues.anrs.length > 0 && (
-        <div>
-          <div className="py-2" />
-          <p className="font-sans text-white">ANRs:</p>
-          <ul className="list-disc">
-            {node.data.issues.anrs.map(({ title, count }) => (
-              <li key={title}>
-                <span className="font-sans text-white text-xs">
-                  {title} - {formatter.format(count)}
-                </span>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+    <div className={`group p-4 border-black rounded-md transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 ${data.issues.crashes.length > 0 || data.issues.anrs.length > 0 ? 'bg-red-400' : 'bg-emerald-400'}`}>
+      <Handle type="target" id="a" position={Position.Left} isConnectable={isConnectable} />
+      <Handle type="source" id="b" position={Position.Right} isConnectable={isConnectable} />
+      <p className="font-sans text-white text-center group-hover:text-left">{data.label}</p>
+      <div className='h-0 group-hover:h-full'>
+        {data.issues.crashes.length > 0 && (
+          <div>
+            <div className="py-2" />
+            <p className="font-sans text-white">Crashes:</p>
+            <ul className="list-disc list-inside text-white p-2">
+              {/* @ts-ignore */}
+              {data.issues.crashes.map(({ title, count }) => (
+                <li key={title}>
+                  <span className="font-sans text-xs">
+                    {title} - {formatter.format(count)}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {data.issues.anrs.length > 0 && (
+          <div>
+            <div className="py-2" />
+            <p className="font-sans text-white">ANRs:</p>
+            <ul className="list-disc list-inside text-white p-2">
+              {/* @ts-ignore */}
+              {data.issues.anrs.map(({ title, count }) => (
+                <li key={title}>
+                  <span className="font-sans text-xs">
+                    {title} - {formatter.format(count)}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
+};
+
+const getReactFlowFromJourney = (journey: typeof emptyJourney) => {
+  const nodes = journey.nodes.map(node => {
+    const { id, nodeColor, issues } = node;
+    return {
+      id,
+      position: { x: 0, y: 0 },
+      width: 200,
+      height: 100,
+      type: 'measureNode',
+      sourcePosition: undefined,
+      targetPosition: undefined,
+      data: { label: id, issues: issues }
+    };
+  });
+
+  const maxLinkValue = Math.max(...journey.links.map(link => link.value));
+  const minLinkValue = Math.min(...journey.links.map(link => link.value));
+
+  const edges = journey.links.map(link => {
+    const { source, target, value } = link;
+    const strokeWidth = 2 + ((value - minLinkValue) * 16) / (maxLinkValue - minLinkValue);
+
+    return {
+      id: source + '-' + target,
+      source,
+      target,
+      style: {
+        strokeWidth: strokeWidth,
+        stroke: "#9CA3AF"
+      },
+      sourceHandle: 'b',
+      label: formatter.format(value) + ' sessions',
+      animated: true
+    };
+  });
+
+  return {
+    nodes,
+    edges
+  };
+}
+
+const dagreGraph = new Dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const getLayoutedElements = (nodes: Node[], edges: Edge[]) => {
+  dagreGraph.setGraph({ rankdir: 'LR', nodesep: 100, edgesep: 200 });
+
+  nodes.forEach((node) => dagreGraph.setNode(node.id, node));
+  edges.forEach((edge) => dagreGraph.setEdge(edge.source, edge.target));
+
+  Dagre.layout(dagreGraph);
+
+  nodes.forEach((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.targetPosition = Position.Left;
+    node.sourcePosition = Position.Right;
+    node.position = {
+      x: nodeWithPosition.x,
+      y: nodeWithPosition.y
+    };
+    return node;
+  });
+
+  return { nodes, edges };
 };
 
 const Journey: React.FC<JourneyProps> = ({ appId, startDate, endDate, appVersion }) => {
 
   const [journeyApiStatus, setJourneyApiStatus] = useState(JourneyApiStatus.Loading);
   const [journey, setJourney] = useState(emptyJourney);
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
   const router = useRouter()
 
@@ -83,6 +188,10 @@ const Journey: React.FC<JourneyProps> = ({ appId, startDate, endDate, appVersion
       case JourneyApiStatus.Success:
         setJourneyApiStatus(JourneyApiStatus.Success)
         setJourney(result.data)
+        let flow = getReactFlowFromJourney(result.data)
+        const layoutedFlow = getLayoutedElements(flow.nodes as Node[], flow.edges as Edge[]);
+        setNodes(layoutedFlow.nodes)
+        setEdges(layoutedFlow.edges)
         break
     }
   }
@@ -91,71 +200,23 @@ const Journey: React.FC<JourneyProps> = ({ appId, startDate, endDate, appVersion
     getJourney(appId, startDate, endDate, appVersion)
   }, [appId, startDate, endDate, appVersion]);
 
-  function mapJourneyToNetwork(journey: typeof emptyJourney) {
-    const nodes = journey.nodes.map(node => {
-      const { id, nodeColor, issues } = node;
-      const height = 0;
-      const size = 4 * Math.floor(Math.random() * 11) + 8; // Assign a random size between 8 and 48 as a multiple of 4
-
-      return {
-        id,
-        nodeColor,
-        height,
-        size,
-        issues
-      };
-    });
-
-    const maxLinkValue = Math.max(...journey.links.map(link => link.value));
-    const minLinkValue = Math.min(...journey.links.map(link => link.value));
-
-    const links = journey.links.map(link => {
-      const { source, target, value } = link;
-      const distance = 50 + ((value - minLinkValue) * 500) / (maxLinkValue - minLinkValue);
-
-      return {
-        source,
-        target,
-        distance: Math.round(distance)
-      };
-    });
-
-    return {
-      nodes,
-      links
-    };
-  }
-
   return (
     <div className="flex items-center justify-center border border-black text-black font-sans text-sm w-5/6 h-screen">
       {journeyApiStatus === JourneyApiStatus.Loading && <p className="text-lg">Updating journey...</p>}
       {journeyApiStatus === JourneyApiStatus.Error && <p className="text-lg">Error fetching journey. Please refresh page or change filters to try again.</p>}
       {journeyApiStatus === JourneyApiStatus.Success
-        && <ResponsiveNetwork
-          data={mapJourneyToNetwork(journey)}
-          margin={{ top: 80, right: 120, bottom: 80, left: 120 }}
-          nodeBorderWidth={1}
-          nodeBorderColor={{
-            from: 'color',
-            modifiers: [
-              [
-                'darker',
-                0.8
-              ]
-            ]
-          }}
-          linkDistance={e => e.distance}
-          centeringStrength={0}
-          repulsivity={10}
-          linkColor={"rgb(161 161 170)"}
-          nodeSize={n => n.size}
-          activeNodeSize={n => 1.5 * n.size}
-          nodeColor={node => node.nodeColor}
-          linkThickness={n => n.data.distance / 100}
-          linkBlendMode="multiply"
-          motionConfig="wobbly"
+        && <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          nodesDraggable={true}
+          nodesConnectable={false}
+          edgesUpdatable={false}
+          proOptions={{ hideAttribution: true }}
           // @ts-ignore
-          nodeTooltip={NodeTooltip}
+          nodeTypes={nodeTypes}
+          fitView
         />}
     </div>
   );
