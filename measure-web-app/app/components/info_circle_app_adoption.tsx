2f7466575e6934778251425a3671a4c22b01b3bb
Anup Cowkur <anupcowkur89@gmail.com>
2024-03-26 16:44:56 +0530

    chore(webapp): handle no data cases in metrics API

--- measure-web-app/app/components/info_circle_app_adoption.tsx
+++ measure-web-app/app/components/info_circle_app_adoption.tsx
@@ -3,6 +3,7 @@ import { MetricsApiStatus } from '../api/api_calls';
 
 interface InfoCircleAppAdoptionProps {
   status: MetricsApiStatus,
+  noData: boolean,
   value: number,
   sessions: number,
   totalSessions: number,
@@ -11,15 +12,16 @@ interface InfoCircleAppAdoptionProps {
 
 const formatter = Intl.NumberFormat('en', { notation: 'compact' });
 
-const InfoCircleAppAdoption = ({ status, value, sessions, totalSessions, title }: InfoCircleAppAdoptionProps) => {
+const InfoCircleAppAdoption = ({ status, noData, value, sessions, totalSessions, title }: InfoCircleAppAdoptionProps) => {
   return (
     <div className="flex flex-col items-center">
       <div className={`flex flex-col group relative items-center justify-center w-64 aspect-square rounded-full border border-black border-4 hover:bg-neutral-800/25 transition-colors duration-100`}>
         {status === MetricsApiStatus.Loading && <p className="font-display text-lg">Updating...</p>}
         {status === MetricsApiStatus.Error && <p className="font-display text-lg">Error</p>}
-        {status === MetricsApiStatus.Success && <p className="font-sans text-xl"> {value}%</p>}
-        <div className="py-1" />
-        {status === MetricsApiStatus.Success && <p className="font-sans text-sm">{formatter.format(sessions)}/{formatter.format(totalSessions)} sessions</p>}
+        {status === MetricsApiStatus.Success && noData === true && <p className="font-sans text-lg"> No data</p>}
+        {status === MetricsApiStatus.Success && noData === false && <p className="font-sans text-xl"> {value}%</p>}
+        {status === MetricsApiStatus.Success && noData === false && <div className="py-1" />}
+        {status === MetricsApiStatus.Success && noData === false && <p className="font-sans text-sm">{formatter.format(sessions)}/{formatter.format(totalSessions)} sessions</p>}
         <span className="pointer-events-none z-50 max-w-xl absolute font-sans text-sm text-white rounded-md p-4 bg-neutral-800 -top-28 left-0 w-max opacity-0 transition-opacity group-hover:opacity-100">
           Adoption =  (Sessions of selected app version / Sessions of all app versions) * 100
         </span>
