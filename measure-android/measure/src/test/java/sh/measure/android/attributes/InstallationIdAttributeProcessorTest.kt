7637b29daaa0fced3976a56b05b4866b2d3b146e
Abhay Sood <soodabhay23@gmail.com>
2024-04-02 16:26:07 +0530

    chore(android): add tests InstallationIdAttributeProcessor

---
+++ measure-android/measure/src/test/java/sh/measure/android/attributes/InstallationIdAttributeProcessorTest.kt
@@ -0,0 +1,51 @@
+package sh.measure.android.attributes
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.never
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+import sh.measure.android.events.EventType
+import sh.measure.android.fakes.FakeEventFactory
+import sh.measure.android.fakes.FakeEventFactory.toEvent
+import sh.measure.android.fakes.FakeIdProvider
+import sh.measure.android.storage.PrefsStorage
+
+class InstallationIdAttributeProcessorTest {
+    private val prefsStorage = mock<PrefsStorage>()
+    private val idProvider = FakeIdProvider()
+    private val installationIdAttributeProcessor = InstallationIdAttributeProcessor(
+        prefsStorage = prefsStorage, idProvider = idProvider
+    )
+
+    @Test
+    fun `given installation ID is not set, then creates, stores and returns installation ID in shared preferences`() {
+        val installationIdKey = "installation_id"
+        val installationId = idProvider.createId()
+        `when`(prefsStorage.getInstallationId()).thenReturn(null)
+        val event = FakeEventFactory.getExceptionData().toEvent(type = EventType.EXCEPTION)
+
+        // When
+        event.appendAttributes(listOf(installationIdAttributeProcessor))
+
+        // Then
+        verify(prefsStorage).setInstallationId(installationId)
+        assertEquals(installationId, event.attributes[installationIdKey])
+    }
+
+    @Test
+    fun `given installation ID is already set, then returns the stored installation ID`() {
+        val installationIdKey = "installation_id"
+        val installationId = idProvider.createId()
+        `when`(prefsStorage.getInstallationId()).thenReturn(installationId)
+        val event = FakeEventFactory.getExceptionData().toEvent(type = EventType.EXCEPTION)
+
+        // When
+        event.appendAttributes(listOf(installationIdAttributeProcessor))
+
+        // Then
+        verify(prefsStorage, never()).setInstallationId(installationId)
+        assertEquals(installationId, event.attributes[installationIdKey])
+    }
+}
