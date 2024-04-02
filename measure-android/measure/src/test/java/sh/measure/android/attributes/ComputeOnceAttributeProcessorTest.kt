6a162beb36404b2f257ee4c0678497eaf8ebecc6
Abhay Sood <soodabhay23@gmail.com>
2024-04-02 16:17:35 +0530

    chore(android): add tests for ComputeOnceAttributeProcessor

---
+++ measure-android/measure/src/test/java/sh/measure/android/attributes/ComputeOnceAttributeProcessorTest.kt
@@ -0,0 +1,32 @@
+package sh.measure.android.attributes
+
+import org.junit.Assert
+import org.junit.Test
+import sh.measure.android.events.EventType
+import sh.measure.android.fakes.FakeEventFactory
+import sh.measure.android.fakes.FakeEventFactory.toEvent
+
+class ComputeOnceAttributeProcessorTest {
+
+    @Test
+    fun `compute attributes is only called once when appending attributes`() {
+        var computeAttributesCalledCount = 0
+
+        // Given
+        val processor = object : ComputeOnceAttributeProcessor() {
+            override fun computeAttributes(): Map<String, Any?> {
+                computeAttributesCalledCount++
+                return mapOf("key" to "value")
+            }
+        }
+        val event = FakeEventFactory.getExceptionData().toEvent(type = EventType.EXCEPTION)
+
+        // When
+        processor.appendAttributes(event)
+        processor.appendAttributes(event)
+        processor.appendAttributes(event)
+
+        // Then
+        Assert.assertEquals(1, computeAttributesCalledCount)
+    }
+}
