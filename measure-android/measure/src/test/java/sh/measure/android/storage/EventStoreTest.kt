db80901c8f4966016597d815509446b2b53873f4
Abhay Sood <soodabhay23@gmail.com>
2024-04-02 15:54:46 +0530

    chore(android): use fake event factory for EventProcessorTest

--- measure-android/measure/src/test/java/sh/measure/android/storage/EventStoreTest.kt
+++ measure-android/measure/src/test/java/sh/measure/android/storage/EventStoreTest.kt
@@ -10,22 +10,16 @@ import sh.measure.android.applaunch.HotLaunchData
 import sh.measure.android.applaunch.WarmLaunchData
 import sh.measure.android.events.Event
 import sh.measure.android.events.EventType
-import sh.measure.android.exceptions.ExceptionFactory
+import sh.measure.android.fakes.FakeEventFactory
 import sh.measure.android.fakes.FakeIdProvider
 import sh.measure.android.gestures.ClickData
 import sh.measure.android.gestures.LongClickData
 import sh.measure.android.gestures.ScrollData
 import sh.measure.android.lifecycle.ActivityLifecycleData
-import sh.measure.android.lifecycle.ActivityLifecycleType
-import sh.measure.android.lifecycle.AppLifecycleType
 import sh.measure.android.lifecycle.ApplicationLifecycleData
 import sh.measure.android.lifecycle.FragmentLifecycleData
-import sh.measure.android.lifecycle.FragmentLifecycleType
-import sh.measure.android.lifecycle.TestFragment
-import sh.measure.android.lifecycle.TestLifecycleActivity
 import sh.measure.android.navigation.NavigationData
 import sh.measure.android.networkchange.NetworkChangeData
-import sh.measure.android.networkchange.NetworkType
 import sh.measure.android.okhttp.HttpData
 import sh.measure.android.performance.CpuUsageData
 import sh.measure.android.performance.LowMemoryData
@@ -46,20 +40,12 @@ internal class EventStoreTest {
         val filePath = "file_path"
         val eventId = idProvider.id
         `when`(fileStorage.createExceptionFile(eventId)).thenReturn(filePath)
-        val exception = IllegalArgumentException("Test exception")
-        val thread = Thread.currentThread()
-        val exceptionData = ExceptionFactory.createMeasureException(
-            exception,
-            handled = true,
-            thread = thread,
-            foreground = true,
-        )
 
         // Given
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = exceptionData,
+            data = FakeEventFactory.getExceptionData(),
             type = EventType.EXCEPTION
         )
 
@@ -85,20 +71,12 @@ internal class EventStoreTest {
         val filePath = "file_path"
         val eventId = idProvider.id
         `when`(fileStorage.createAnrPath(eventId)).thenReturn(filePath)
-        val exception = IllegalArgumentException("Test exception")
-        val thread = Thread.currentThread()
-        val exceptionData = ExceptionFactory.createMeasureException(
-            exception,
-            handled = true,
-            thread = thread,
-            foreground = true,
-        )
 
         // Given
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = exceptionData,
+            data = FakeEventFactory.getExceptionData(),
             type = EventType.ANR
         )
 
@@ -125,16 +103,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = ClickData(
-                target = "target",
-                target_id = "target_id",
-                width = 100,
-                height = 200,
-                x = 50F,
-                y = 50F,
-                touch_down_time = 987549876L,
-                touch_up_time = 234567609L,
-            ),
+            data = FakeEventFactory.getClickData(),
             type = EventType.CLICK
         )
 
@@ -159,16 +128,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = LongClickData(
-                target = "target",
-                target_id = "target_id",
-                width = 100,
-                height = 200,
-                x = 50F,
-                y = 50F,
-                touch_down_time = 987549876L,
-                touch_up_time = 234567609L,
-            ),
+            data = FakeEventFactory.getLongClickData(),
             type = EventType.LONG_CLICK
         )
 
@@ -193,17 +153,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = ScrollData(
-                target = "target",
-                target_id = "target-id",
-                x = 50F,
-                y = 50F,
-                end_x = 100F,
-                end_y = 100F,
-                direction = "left",
-                touch_down_time = 123123213L,
-                touch_up_time = 876545678L,
-            ),
+            data = FakeEventFactory.getScrollData(),
             type = EventType.SCROLL
         )
 
@@ -228,12 +178,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = ActivityLifecycleData(
-                type = ActivityLifecycleType.CREATED,
-                class_name = TestLifecycleActivity::class.java.name,
-                intent = null,
-                saved_instance_state = false,
-            ),
+            data = FakeEventFactory.getActivityLifecycleData(),
             type = EventType.LIFECYCLE_ACTIVITY
         )
 
@@ -258,11 +203,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = FragmentLifecycleData(
-                type = FragmentLifecycleType.ATTACHED,
-                parent_activity = TestLifecycleActivity::class.java.name,
-                class_name = TestFragment::class.java.name,
-            ),
+            data = FakeEventFactory.getFragmentLifecycleData(),
             type = EventType.LIFECYCLE_FRAGMENT
         )
 
@@ -287,9 +228,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = ApplicationLifecycleData(
-                type = AppLifecycleType.FOREGROUND,
-            ),
+            data = FakeEventFactory.getApplicationLifecycleData(),
             type = EventType.LIFECYCLE_APP
         )
 
@@ -317,16 +256,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = ColdLaunchData(
-                process_start_uptime = 100,
-                process_start_requested_uptime = 200,
-                content_provider_attach_uptime = 300,
-                on_next_draw_uptime = 400,
-                launched_activity = "launched_activity",
-                has_saved_state = true,
-                intent_data = "intent_data",
-
-                ),
+            data = FakeEventFactory.getColdLaunchData(),
             type = EventType.COLD_LAUNCH
         )
 
@@ -351,13 +281,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = WarmLaunchData(
-                app_visible_uptime = 100,
-                on_next_draw_uptime = 200,
-                launched_activity = "launched_activity",
-                has_saved_state = true,
-                intent_data = "intent_data",
-            ),
+            data = FakeEventFactory.getWarmLaunchData(),
             type = EventType.WARM_LAUNCH
         )
 
@@ -382,13 +306,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = HotLaunchData(
-                app_visible_uptime = 100,
-                on_next_draw_uptime = 200,
-                launched_activity = "launched_activity",
-                has_saved_state = true,
-                intent_data = "intent_data",
-            ),
+            data = FakeEventFactory.getHotLaunchData(),
             type = EventType.HOT_LAUNCH
         )
 
@@ -413,13 +331,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = NetworkChangeData(
-                previous_network_type = null,
-                network_type = NetworkType.CELLULAR,
-                previous_network_generation = null,
-                network_generation = null,
-                network_provider = "Test Provider",
-            ),
+            data = FakeEventFactory.getNetworkChangeData(),
             type = EventType.NETWORK_CHANGE
         )
 
@@ -444,20 +356,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = HttpData(
-                url = "url",
-                method = "method",
-                status_code = 200,
-                start_time = 98764567L,
-                end_time = 567890987L,
-                failure_reason = "failure-reason",
-                failure_description = "failure-description",
-                request_headers = emptyMap(),
-                response_headers = emptyMap(),
-                request_body = "request-body",
-                response_body = "response-body",
-                client = "client",
-            ),
+            data = FakeEventFactory.getHttpData(),
             type = EventType.HTTP
         )
 
@@ -482,16 +381,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = MemoryUsageData(
-                java_max_heap = 100,
-                java_total_heap = 200,
-                java_free_heap = 300,
-                total_pss = 400,
-                rss = 500,
-                native_total_heap = 600,
-                native_free_heap = 700,
-                interval_config = 800,
-            ),
+            data = FakeEventFactory.getMemoryUsageData(),
             type = EventType.MEMORY_USAGE
         )
 
@@ -516,15 +406,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = LowMemoryData(
-                java_max_heap = 100,
-                java_total_heap = 200,
-                java_free_heap = 300,
-                total_pss = 400,
-                rss = 500,
-                native_total_heap = 600,
-                native_free_heap = 700,
-            ),
+            data = FakeEventFactory.getLowMemoryData(),
             type = EventType.LOW_MEMORY
         )
 
@@ -549,9 +431,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = TrimMemoryData(
-                level = "level",
-            ),
+            data = FakeEventFactory.getTrimMemoryData(),
             type = EventType.TRIM_MEMORY
         )
 
@@ -576,17 +456,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = CpuUsageData(
-                num_cores = 4,
-                clock_speed = 10,
-                start_time = 123456789L,
-                uptime = 987654321L,
-                utime = 1234L,
-                cutime = 9876L,
-                cstime = 1234L,
-                stime = 9876L,
-                interval_config = 1000,
-            ),
+            data = FakeEventFactory.getCpuUsageData(),
             type = EventType.CPU_USAGE
         )
 
@@ -611,9 +481,7 @@ internal class EventStoreTest {
         val event = Event(
             timestamp = 1,
             sessionId = "session_id",
-            data = NavigationData(
-                route = "route",
-            ),
+            data = FakeEventFactory.getNavigationData(),
             type = EventType.NAVIGATION
         )
 
