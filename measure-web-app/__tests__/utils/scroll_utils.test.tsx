abbad561daa3d956c0f06f98c71d536f19c9d45b
Anup Cowkur <anupcowkur89@gmail.com>
2024-04-24 14:31:53 +0530

    chore(webapp): add unit tests for scroll_utils

---
+++ measure-web-app/__tests__/utils/scroll_utils.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { expect, it, describe } from '@jest/globals'
+import { useScrollDirection } from '@/app/utils/scroll_utils'
+import '@testing-library/jest-dom/jest-globals';
+
+const TestComponent = () => {
+  const scrollDirection = useScrollDirection()
+  return <div>{scrollDirection}</div>
+}
+
+describe('useScrollDirection', () => {
+  it('should return "scrolling up" initially', () => {
+    render(<TestComponent />)
+    expect(screen.getByText('scrolling up')).toBeInTheDocument()
+  })
+
+  it('should update the scroll direction correctly when scrolling down', async () => {
+    render(<TestComponent />)
+
+    fireEvent.scroll(window, { target: { scrollY: 100 } })
+
+    await screen.findByText('scrolling down')
+
+    expect(screen.getByText('scrolling down')).toBeInTheDocument()
+  })
+
+  it('should update the scroll direction correctly when scrolling up', async () => {
+    render(<TestComponent />)
+
+    fireEvent.scroll(window, { target: { scrollY: 100 } })
+    fireEvent.scroll(window, { target: { scrollY: 50 } })
+
+    await screen.findByText('scrolling up')
+
+    expect(screen.getByText('scrolling up')).toBeInTheDocument()
+  })
+})
--- measure-web-app/jest.config.ts
+++ measure-web-app/jest.config.ts
@@ -152,7 +152,7 @@ const config: Config = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
