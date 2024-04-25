0d01b46bc50fa4ac35b0c0bafff3464f319d0c3b
Abhay Sood <soodabhay23@gmail.com>
2024-04-25 19:47:41 +0530

    chore(android): rename to attribute in events request

--- measure-android/measure/src/main/java/sh/measure/android/exporter/NetworkClient.kt
+++ measure-android/measure/src/main/java/sh/measure/android/exporter/NetworkClient.kt
@@ -126,6 +126,6 @@ internal class NetworkClientImpl(
         } else {
             throw IllegalStateException("EventPacket must have either serializedData or serializedDataFilePath")
         }
-        return "{\"id\":\"$eventId\",\"session_id\":\"$sessionId\",\"timestamp\":$timestamp,\"type\":\"$type\",\"$type\":$data,\"attachments\":$serializedAttachments,\"attributes\":$serializedAttributes}"
+        return "{\"id\":\"$eventId\",\"session_id\":\"$sessionId\",\"timestamp\":$timestamp,\"type\":\"$type\",\"$type\":$data,\"attachments\":$serializedAttachments,\"attribute\":$serializedAttributes}"
     }
 }
