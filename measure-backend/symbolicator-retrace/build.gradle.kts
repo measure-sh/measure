3077e25a2a0aafc4548418684b39fb2a46b9c90f
Abhay Sood <soodabhay23@gmail.com>
2024-06-10 01:09:25 +0530

    chore(backend): update dependencies in symbolicator
    r8: 8.1.60 to 8.3.37
    gradle: 8.4 -> 8.6
    kotlin: 1.9.10 -> 1.9.21

--- measure-backend/symbolicator-retrace/build.gradle.kts
+++ measure-backend/symbolicator-retrace/build.gradle.kts
@@ -3,9 +3,9 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.21"
     id("io.ktor.plugin") version "2.3.5"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21"
 }
 
 group = "sh.measure"
@@ -36,7 +36,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.2.4")
     implementation("io.ktor:ktor-server-netty-jvm:2.2.4")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("com.android.tools:r8:8.1.60")
+    implementation("com.android.tools:r8:8.3.37")
     implementation("com.amazonaws:aws-java-sdk-s3:1.12.566")
     testImplementation("io.ktor:ktor-server-tests-jvm:2.2.4")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
