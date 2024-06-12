14494c236f9355a76b0128c3688e113e832f5931
Abhay Sood <soodabhay23@gmail.com>
2024-06-12 21:10:35 +0530

    feat(backend): implement new schema for navigation event

--- measure-backend/measure-go/measure/app.go
+++ measure-backend/measure-go/measure/app.go
@@ -972,7 +972,9 @@ func (a *App) GetSessionEvents(ctx context.Context, sessionId uuid.UUID) (*Sessi
 		`cpu_usage.stime`,
 		`cpu_usage.cstime`,
 		`cpu_usage.interval_config`,
-		`toString(navigation.route)`,
+		`toString(navigation.to)`,
+		`toString(navigation.from)`,
+		`toString(navigation.source)`,
 	}
 
 	stmt := sqlf.From("default.events")
@@ -1214,7 +1216,7 @@ func (a *App) GetSessionEvents(ctx context.Context, sessionId uuid.UUID) (*Sessi
 			&cpuUsage.IntervalConfig,
 
 			// navigation
-			&navigation.Route,
+			&navigation.To,
 		}
 
 		if err := rows.Scan(dest...); err != nil {
