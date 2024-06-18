77a8a285667c41b2c0fa9cfa286c157dc0d500b5
Debjeet Biswas <debjeet12@gmail.com>
2024-06-18 13:56:20 +0530

    chore(backend): modify unhandled exception groups table
    - add new column named `first_event_timestamp` in
    `unhandled_exception_groups` table
    
    fixes #788

--- self-host/postgres/20231228033348_create-unhandled_exception_groups-relation.sql
+++ self-host/postgres/20231228033348_create-unhandled_exception_groups-relation.sql
@@ -5,6 +5,7 @@ create table if not exists public.unhandled_exception_groups (
     name text not null,
     fingerprint varchar(16) not null,
     event_ids uuid[] not null,
+    first_event_timestamp timestamptz not null,
     created_at timestamptz not null default now(),
     updated_at timestamptz not null default now()
 );
@@ -14,6 +15,7 @@ comment on column public.unhandled_exception_groups.app_id is 'linked app id';
 comment on column public.unhandled_exception_groups.name is 'name of the exception for easy identification';
 comment on column public.unhandled_exception_groups.fingerprint is 'fingerprint of the unhandled exception';
 comment on column public.unhandled_exception_groups.event_ids is 'list of associated event ids';
+comment on column public.unhandled_exception_groups.first_event_timestamp is 'utc timestamp of the oldest event in the group';
 comment on column public.unhandled_exception_groups.created_at is 'utc timestamp at the time of record creation';
 comment on column public.unhandled_exception_groups.updated_at is 'utc timestamp at the time of record updation';
 
