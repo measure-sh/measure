6080f5a95a1829fbbfe0918eface1a2565423c7f
Debjeet Biswas <debjeet12@gmail.com>
2024-06-19 01:09:10 +0530

    chore(backend): modify anr groups table
    - add new column named `first_event_timestamp` in `anr_groups` table
    
    fixes #788

--- self-host/postgres/20231228044339_create-anr_groups-relation.sql
+++ self-host/postgres/20231228044339_create-anr_groups-relation.sql
@@ -5,6 +5,7 @@ create table if not exists public.anr_groups (
     name text not null,
     fingerprint varchar(16) not null,
     event_ids uuid[] not null,
+    first_event_timestamp timestamptz not null,
     created_at timestamptz not null default now(),
     updated_at timestamptz not null default now()
 );
@@ -14,6 +15,7 @@ comment on column public.anr_groups.app_id is 'linked app id';
 comment on column public.anr_groups.name is 'name of the anr for easy identification';
 comment on column public.anr_groups.fingerprint is 'fingerprint of the anr';
 comment on column public.anr_groups.event_ids is 'list of associated event ids';
+comment on column public.anr_groups.first_event_timestamp is 'utc timestamp of the oldest event in the group';
 comment on column public.anr_groups.created_at is 'utc timestamp at the time of record creation';
 comment on column public.anr_groups.updated_at is 'utc timestamp at the time of record updation';
 
