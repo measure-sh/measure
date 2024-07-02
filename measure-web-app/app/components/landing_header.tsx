9b79992280d248ef2719f1450ecfa75a5c024c76
Anup Cowkur <anupcowkur89@gmail.com>
2024-07-02 14:09:29 +0530

    chore(webapp): add login button to landing page header
    Login button only shows up when host is not measure.sh
    or www.measure.sh (not running in local or self hosted).
    It shows github link otherwise.
    
    closes #853

--- measure-web-app/app/components/landing_header.tsx
+++ measure-web-app/app/components/landing_header.tsx
@@ -1,22 +1,32 @@
 'use client'
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation'
-import { useScrollDirection } from '../utils/scroll_utils';
-import Link from 'next/link';
+import { useScrollDirection } from '../utils/scroll_utils'
+import Link from 'next/link'
 import Image from 'next/image'
 
 export default function LandingHeader() {
   const scrollDir = useScrollDirection();
-  const [isFocused, setIsFocused] = useState(false);
-  const router = useRouter();
+  const [isFocused, setIsFocused] = useState(false)
+  const router = useRouter()
+  const [selfHosted, setSelfHosted] = useState(false)
+
+  useEffect(() => {
+    if (typeof window !== undefined && (window.location.hostname === 'measure.sh' || window.location.hostname === 'www.measure.sh')) {
+      setSelfHosted(true)
+    } else {
+      setSelfHosted(false)
+    }
+  }, [])
 
   return (
     <header onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} className={`w-full flex flex-col z-50 bg-white fixed top-0 transition-transform duration-100 ease-in-out ${scrollDir === 'scrolling down' && isFocused === false ? '-translate-y-full' : 'translate-y-0'}`}>
-      <div className="w-full flex flex-col md:flex-row space-between items-center py-4 px-4">
+      <div className="w-full flex flex-col md:flex-row space-between items-center py-4 pl-4 pr-2">
         <button className="outline-none border border-black rounded-full hover:bg-yellow-200 focus-visible:bg-yellow-200 active:bg-yellow-300 font-display font-display transition-colors duration-100 py-1 px-2.5" onClick={() => router.push('/')}>m</button>
         <div className="py-2 md:py-0 md:flex md:grow" />
-        <Link href="https://github.com/measure-sh/measure" className='outline-none flex flex-row place-items-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display transition-colors duration-100 py-2 px-4'>
+        {selfHosted && <Link href="/auth/login" className='outline-none flex flex-row place-items-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display transition-colors duration-100 py-2 px-4'>Login</Link>}
+        {!selfHosted && <Link href="https://github.com/measure-sh/measure" className='outline-none flex flex-row place-items-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display transition-colors duration-100 py-2 px-4'>
           <Image
             src='/images/github_logo.svg'
             width={24}
@@ -24,7 +34,7 @@ export default function LandingHeader() {
             alt={'Github logo'} />
           <div className='px-1' />
           <p className='mt-1'>Get Started</p>
-        </Link>
+        </Link>}
       </div>
       <div className="w-full border-[0.1px] border-stone-900" />
     </header>
