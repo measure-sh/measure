7511ab109836dc600afa42de7bc64ab5cb7f771d
Anup Cowkur <anupcowkur89@gmail.com>
2024-07-02 13:47:25 +0530

    chore(webapp): replace email waitlist with Github link
    closes #854, closes #855

--- measure-web-app/app/page.tsx
+++ measure-web-app/app/page.tsx
@@ -1,10 +1,9 @@
 "use client"
 
 import Image from 'next/image'
-import osiLogo from '/images/osi_logo.svg'
-import EmailWaitlist from './components/email_waitlist'
 import LandingHeader from './components/landing_header'
 import PhoneECG from './components/phone_ecg'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -101,14 +100,21 @@ export default function Home() {
           </div>
         </div>
         <div className="py-12 md:py-24" />
-        <p className="font-display font-regular text-black text-6xl max-w-4xl text-center">Early access</p>
+        <p className="font-display font-regular text-black text-6xl max-w-4xl text-center">Set up your first app</p>
         <div className="py-2" />
-        <p className="font-sans text-black text-xl leading-relaxed max-w-4xl text-center">We are building measure for mobile devs like you. Help us make it the best tool for the job!</p>
-        <div className="py-4" />
-        <EmailWaitlist />
+        <p className="font-sans text-black text-xl leading-relaxed max-w-4xl text-center">Check out our detailed guides on Github to get up and running with Measure!</p>
+        <div className="py-2" />
+        <Link href="https://github.com/measure-sh/measure" className='m-4 outline-none flex flex-row place-items-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display transition-colors duration-100 py-2 px-4'>
+          <Image
+            src='/images/github_logo.svg'
+            width={24}
+            height={24}
+            alt={'Github logo'} />
+          <div className='px-1' />
+          <p className='mt-1'>Get Started</p>
+        </Link>
         <div className="py-24" />
       </div>
-
     </main>
   )
 }
