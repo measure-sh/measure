6c25dca5504e9430936bf45d64c75c48caa8609a
Anup Cowkur <anupcowkur89@gmail.com>
2024-07-12 14:26:20 +0530

    chore(webapp): update Github sign in button text

--- measure-web-app/app/auth/login/github-sign-in.tsx
+++ measure-web-app/app/auth/login/github-sign-in.tsx
@@ -29,5 +29,5 @@ function GitHubLogo() {
 }
 
 export default function GitHubSignIn() {
-  return <button className="justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4 w-full" onClick={() => doGitHubLogin()}><GitHubLogo /><span>Login with GitHub</span></button>
+  return <button className="justify-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display text-black transition-colors duration-100 py-2 px-4 w-full" onClick={() => doGitHubLogin()}><GitHubLogo /><span> Sign in with GitHub</span></button>
 }
