b0e3258375be3ddea5a6dd332f1aeca9adb4d362
Debjeet Biswas <debjeet12@gmail.com>
2024-07-12 11:09:52 +0530

    chore(webapp): remove supabase related pieces
    fixes #843

--- measure-web-app/utils/supabase/browser.ts
+++
@@ -1,5 +0,0 @@
-"use client";
-
-import { createBrowserClient } from "@supabase/ssr"
-
-export const supabase = createBrowserClient(process?.env?.NEXT_PUBLIC_SUPABASE_URL!, process?.env?.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
