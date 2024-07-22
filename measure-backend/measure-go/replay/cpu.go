4dc6744d34bccb1b8b8cd9cfcacb0c822d198423
Anup Cowkur <anupcowkur89@gmail.com>
2024-07-22 14:52:22 +0530

    chore(backend): refactor cpu usage calculation for clarity

--- measure-backend/measure-go/replay/cpu.go
+++ measure-backend/measure-go/replay/cpu.go
@@ -23,8 +23,20 @@ func ComputeCPUUsage(events []event.EventField) (result []CPUUsage) {
 }
 
 func calculate(u *event.CPUUsage) (usage float64) {
+	// Sum the CPU times
 	total := float64(u.UTime + u.STime + u.CUTime + u.CSTime)
+
+	// Convert interval configuration from milliseconds to seconds
 	elapsed := float64(u.IntervalConfig) / 1000
 
-	return (total / float64(u.ClockSpeed)) / (elapsed * float64(u.NumCores)) * 100.0
+	// Divide total CPU time by the clock speed to get time in seconds
+	usageTime := total / float64(u.ClockSpeed)
+
+	// Divide by the elapsed time to get usage over the interval
+	usageOverInterval := usageTime / elapsed
+
+	// Divide by the number of cores to get the average usage per core
+	averageUsage := (usageOverInterval / float64(u.NumCores)) * 100.0
+
+	return averageUsage
 }
