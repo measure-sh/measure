be628948785fb51b1bee0257236e1272cabd0f48
Anup Cowkur <anupcowkur89@gmail.com>
2024-07-29 14:22:03 +0530

    fix(webapp): use &apos; instead of apostrophe

--- measure-web-app/app/page.tsx
+++ measure-web-app/app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
           </div>
         </div>
         <div className="py-12 md:py-16" />
-        <p className="font-sans text-black text-lg leading-relaxed max-w-4xl text-center">Let's get to the root cause:</p>
+        <p className="font-sans text-black text-lg leading-relaxed max-w-4xl text-center">Let&apos;s get to the root cause:</p>
         <div className="py-2" />
         <Link href="https://github.com/measure-sh/measure" className='m-4 outline-none flex flex-row place-items-center hover:bg-yellow-200 active:bg-yellow-300 focus-visible:bg-yellow-200 border border-black rounded-md font-display transition-colors duration-100 py-2 px-4'>
           <Image
