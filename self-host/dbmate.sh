435f07f3c6b303093695d5a31e85baf24a284504
Debjeet Biswas <debjeet12@gmail.com>
2024-08-01 20:00:07 +0530

    chore(backend): update dbmate.sh
    - increase reliability of dbmate migrations

--- self-host/dbmate.sh
+++ self-host/dbmate.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 # postgres
-dbmate --env POSTGERS_DSN --migrations-dir /opt/postgres status
-dbmate --env POSTGRES_DSN --migrations-dir /opt/postgres migrate
+dbmate --url "$POSTGRES_DSN" --migrations-dir /opt/postgres status
+dbmate --url "$POSTGRES_DSN" --migrations-dir /opt/postgres migrate
 
 # clickhouse
-dbmate --url=$CLICKHOUSE_DSN --migrations-dir=/opt/clickhouse status
-dbmate --url=$CLICKHOUSE_DSN --migrations-dir=/opt/clickhouse migrate
+dbmate --url "$CLICKHOUSE_DSN" --migrations-dir=/opt/clickhouse status
+dbmate --url "$CLICKHOUSE_DSN" --migrations-dir=/opt/clickhouse migrate
 
 exec "$@"
