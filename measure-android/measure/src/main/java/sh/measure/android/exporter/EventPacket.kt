f7f2cf66f99d07825311df3d8f4567570b905185
Abhay Sood <soodabhay23@gmail.com>
2024-08-05 14:11:50 +0530

    fix(android): fix serialization of event packets

--- measure-android/measure/src/main/java/sh/measure/android/exporter/EventPacket.kt
+++ measure-android/measure/src/main/java/sh/measure/android/exporter/EventPacket.kt
@@ -12,8 +12,3 @@ internal data class EventPacket(
     val serializedAttributes: String,
     val serializedUserDefinedAttributes: String?,
 )
-
-internal fun EventPacket.asFormDataPart(): String {
-    require(serializedData?.isNotEmpty() == true) { "serializedData is required for converting the event packet to form data" }
-    return "{\"id\":\"$eventId\",\"session_id\":\"$sessionId\",\"user_triggered\":$userTriggered,\"timestamp\":\"$timestamp\",\"type\":\"$type\",\"$type\":$serializedData,\"attachments\":$serializedAttachments,\"attribute\":$serializedAttributes}"
-}
