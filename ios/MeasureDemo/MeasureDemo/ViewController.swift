a475cff8024add3be1b104153f6079b42c002c09
Adwin Ronald Ross <adwinross@gmail.com>
2024-08-14 10:46:00 +0530

    feat(ios): Initial project setup  (#1034)
    * feat(ios): initial project setup
    
    * chore: objective-c demo project removed

---
+++ ios/MeasureDemo/MeasureDemo/ViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ViewController.swift
+//  MeasureDemo
+//
+//  Created by Adwin Ross on 12/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let crashTypes = [
+        "Abort",
+        "Bad Pointer",
+        "Corrupt Memory",
+        "Corrupt Object",
+        "Deadlock",
+        "NSException",
+        "Stack Overflow",
+        "Zombie",
+        "Zombie NSException"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        view.addSubview(tableView)
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crashTypes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = crashTypes[indexPath.row]
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCrashType = crashTypes[indexPath.row]
+        triggerCrash(type: selectedCrashType)
+    }
+    
+    // MARK: - Crash Triggers
+    
+    func triggerCrash(type: String) {
+        switch type {
+        case "Abort":
+            abort()
+        case "Bad Pointer":
+            let pointer = UnsafeMutableRawPointer(bitPattern: 0xdeadbeef)!
+            pointer.storeBytes(of: 0, as: Int.self)
+        case "Corrupt Memory":
+            let array = [1, 2, 3]
+            array.withUnsafeBufferPointer {
+                _ = $0.baseAddress!.advanced(by: 4).pointee
+            }
+        case "Corrupt Object":
+            let object: AnyObject = NSArray()
+            let _ = object.perform(Selector(("invalidSelector")))
+        case "Deadlock":
+            let queue = DispatchQueue(label: "deadlockQueue")
+            queue.sync {
+                queue.sync {}
+            }
+        case "NSException":
+            let array = NSArray()
+            print(array[1])
+        case "Stack Overflow":
+            func recurse() {
+                recurse()
+            }
+            recurse()
+        case "Zombie":
+            var object: NSObject? = NSObject()
+            let __weakObject = object
+            object = nil
+            print(__weakObject!.description)
+        case "Zombie NSException":
+            var exception: NSException? = NSException(name: .genericException, reason: "Test", userInfo: nil)
+            let __weakException = exception
+            exception = nil
+            __weakException?.raise()
+        default:
+            break
+        }
+    }
+}
