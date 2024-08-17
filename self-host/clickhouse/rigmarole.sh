755675b23a945706214c694c4387e8f96d90dfff
Debjeet Biswas <debjeet12@gmail.com>
2024-08-17 17:15:36 +0530

    chore(backend): update rigmarole script
    - update `self-host/postgres/rigmarole.sh` script
    - update `self-host/clickhouse/rigmarole.sh` script

--- self-host/clickhouse/rigmarole.sh
+++ self-host/clickhouse/rigmarole.sh
@@ -5,9 +5,8 @@ set -euo pipefail
 # read count of applied migrations
 count=$(dbmate status | tail -2 | head -1 | cut -d ' ' -f 2)
 
-
 # rollback `count` times
-for ((i=1; i<=count; i++))
+for i in $(seq 1 "$count")
 do
   dbmate rollback
 done
