9613d82738128860f35983abec63c0c763a1a74a
Adwin Ronald Ross <adwin@measure.sh>
2024-08-22 17:11:51 +0530

    feat(ios): swiftlint integration

--- ios/MeasureDemo/MeasureDemo/ViewController.swift
+++ ios/MeasureDemo/MeasureDemo/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
     let crashTypes = [
         "Abort",
         "Bad Pointer",
@@ -20,40 +19,40 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         "Zombie",
         "Zombie NSException"
     ]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let tableView = UITableView(frame: view.bounds, style: .plain)
         tableView.delegate = self
         tableView.dataSource = self
-        
+
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
+
         view.addSubview(tableView)
     }
-    
+
     // MARK: - UITableViewDataSource
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return crashTypes.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = crashTypes[indexPath.row]
         return cell
     }
-    
+
     // MARK: - UITableViewDelegate
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selectedCrashType = crashTypes[indexPath.row]
         triggerCrash(type: selectedCrashType)
     }
-    
+
     // MARK: - Crash Triggers
-    
+
     func triggerCrash(type: String) {
         switch type {
         case "Abort":
@@ -68,7 +67,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
         case "Corrupt Object":
             let object: AnyObject = NSArray()
+            // swiftlint:disable redundant_discardable_let
             let _ = object.perform(Selector(("invalidSelector")))
+            // swiftlint:enable redundant_discardable_let
         case "Deadlock":
             let queue = DispatchQueue(label: "deadlockQueue")
             queue.sync {
@@ -83,6 +84,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
             recurse()
         case "Zombie":
+            // swiftlint:disable identifier_name
             var object: NSObject? = NSObject()
             let __weakObject = object
             object = nil
@@ -90,6 +92,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         case "Zombie NSException":
             var exception: NSException? = NSException(name: .genericException, reason: "Test", userInfo: nil)
             let __weakException = exception
+            // swiftlint:enable identifier_name
             exception = nil
             __weakException?.raise()
         default:
--- ios/MeasureSDK/MeasureSDK/CrashReporter/CrashReportManager.swift
+++ ios/MeasureSDK/MeasureSDK/CrashReporter/CrashReportManager.swift
@@ -10,9 +10,9 @@ import Foundation
 class CrashReportManager {
     static let shared = CrashReportManager()
     let crashReporter = MeasureCrashReporter()
-    
+
     private init() {}
-    
+
     func start() {
         crashReporter.initializeCrashReporter(with: UUID().uuidString)
         crashReporter.handleCrashReport()
