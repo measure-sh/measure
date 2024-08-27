d7e79d28aa2b71f9b9e8247c938254afaabe403b
Adwin Ronald Ross <adwin@measure.sh>
2024-08-27 12:23:17 +0530

    chore(ios): update demo app with objective c measure integration

---
+++ ios/MeasureDemo/MeasureDemo/MeasureManager.m
@@ -0,0 +1,17 @@
+//
+//  MeasureManager.m
+//  MeasureDemo
+//
+//  Created by Adwin Ross on 27/08/24.
+//
+
+#import "MeasureManager.h"
+
+@implementation MeasureManager
+
+- (void)initializeMeasureSDKWithConfig:(nullable BaseMeasureConfig *)config {
+    _measure = [Measure shared];
+    [_measure initializeWith:config];
+}
+
+@end
