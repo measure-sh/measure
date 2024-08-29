80fb6176504583bdd534d294b89748cf6a519c36
Abhay Sood <soodabhay23@gmail.com>
2024-08-29 11:27:35 +0530

    chore(android): add docs

--- android/measure/src/main/java/sh/measure/android/Attributes.kt
+++ android/measure/src/main/java/sh/measure/android/Attributes.kt
@@ -20,16 +20,47 @@ import kotlinx.serialization.json.doubleOrNull
 import kotlinx.serialization.json.int
 import kotlinx.serialization.json.intOrNull
 
+/**
+ * Represents a map of attributes that describe an event.
+ *
+ * Attributes can have values of type strings, booleans, integers, or doubles. To create
+ * attributes, use the [buildAttributes] function.
+ *
+ * ```kotlin
+ * val attributes = buildAttributes {
+ *   "key1" to "value1"
+ *   "key2" to 42
+ *   "key3" to 3.14
+ *   "key4" to true
+ * }
+ */
 typealias Attributes = Map<String, AttributeValue>
 
+/**
+ * Creates a map of attributes. It provides a type-safe way to create attributes with the supported
+ * value types: strings, booleans, integers, and doubles.
+ *
+ * Example:
+ *
+ * ```kotlin
+ * val attributes: Attributes = buildAttributes {
+ *  "key1" to "value1"
+ *  "key2" to 42
+ *  "key3" to 3.14
+ *  "key4" to true
+ * }
+ */
+fun buildAttributes(block: EventAttributesBuilder.() -> Unit): Attributes =
+    EventAttributesBuilder().apply(block).build()
+
+/**
+ * Represents a value of an attribute. It can be a string, boolean, integer, or double.
+ * To create attributes, use the [buildAttributes] function.
+ */
 sealed interface AttributeValue {
     val value: Any
 
     companion object
-}
-
-internal fun AttributeValue.Companion.serializer(): KSerializer<@Contextual AttributeValue> {
-    return AttributeValueSerializer
 }
 
 @JvmInline
@@ -44,18 +75,25 @@ value class IntAttr(override val value: Int) : AttributeValue
 @JvmInline
 value class DoubleAttr(override val value: Double) : AttributeValue
 
+/**
+ * Builder for creating attributes. Use the [buildAttributes] function to create a set of
+ * attributes.
+ */
 class EventAttributesBuilder {
     private val attributes = mutableMapOf<String, AttributeValue>()
 
     infix fun String.to(value: String) {
         attributes[this] = StringAttr(value)
     }
+
     infix fun String.to(value: Boolean) {
         attributes[this] = BooleanAttr(value)
     }
+
     infix fun String.to(value: Int) {
         attributes[this] = IntAttr(value)
     }
+
     infix fun String.to(value: Double) {
         attributes[this] = DoubleAttr(value)
     }
@@ -63,8 +101,11 @@ class EventAttributesBuilder {
     fun build(): Attributes = attributes.toMap()
 }
 
-fun buildAttributes(block: EventAttributesBuilder.() -> Unit): Attributes =
-    EventAttributesBuilder().apply(block).build()
+// This is required because we don't want to annotate AttributeValue with @Serializable as it's a
+// public class. Doing so will leak the serialization implementation details to the public API.
+internal fun AttributeValue.Companion.serializer(): KSerializer<@Contextual AttributeValue> {
+    return AttributeValueSerializer
+}
 
 internal object AttributeValueSerializer : KSerializer<AttributeValue> {
     @OptIn(InternalSerializationApi::class, ExperimentalSerializationApi::class)
