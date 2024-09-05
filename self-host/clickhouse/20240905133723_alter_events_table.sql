4357c3e4f264f9f21035a8d26af83e6922f54073
Debjeet Biswas <debjeet12@gmail.com>
2024-09-05 20:24:36 +0530

    chore(backend): add clickhouse migration
    - add clickhouse migration to update columns from `uint32` to `uint64`
    
    fixes #1184

---
+++ self-host/clickhouse/20240905133723_alter_events_table.sql
@@ -0,0 +1,38 @@
+-- migrate:up
+alter table default.events
+modify column if exists `gesture_long_click.touch_down_time` UInt64,
+modify column if exists `gesture_long_click.touch_up_time` UInt64,
+modify column if exists `gesture_click.touch_down_time` UInt64,
+modify column if exists `gesture_click.touch_up_time` UInt64,
+modify column if exists `gesture_scroll.touch_down_time` UInt64,
+modify column if exists `gesture_scroll.touch_up_time` UInt64,
+modify column if exists `cold_launch.process_start_uptime` UInt64,
+modify column if exists `cold_launch.process_start_requested_uptime` UInt64,
+modify column if exists `cold_launch.content_provider_attach_uptime` UInt64,
+modify column if exists `cold_launch.on_next_draw_uptime` UInt64,
+modify column if exists `warm_launch.app_visible_uptime` UInt64,
+modify column if exists `warm_launch.on_next_draw_uptime` UInt64,
+modify column if exists `hot_launch.app_visible_uptime` UInt64,
+modify column if exists `hot_launch.on_next_draw_uptime` UInt64,
+modify column if exists `memory_usage.interval` UInt64,
+modify column if exists `cpu_usage.interval` UInt64;
+
+-- migrate:down
+alter table default.events
+modify column if exists `gesture_long_click.touch_down_time` UInt32,
+modify column if exists `gesture_long_click.touch_up_time` UInt32,
+modify column if exists `gesture_click.touch_down_time` UInt32,
+modify column if exists `gesture_click.touch_up_time` UInt32,
+modify column if exists `gesture_scroll.touch_down_time` UInt32,
+modify column if exists `gesture_scroll.touch_up_time` UInt32,
+modify column if exists `cold_launch.process_start_uptime` UInt32,
+modify column if exists `cold_launch.process_start_requested_uptime` UInt32,
+modify column if exists `cold_launch.content_provider_attach_uptime` UInt32,
+modify column if exists `cold_launch.on_next_draw_uptime` UInt32,
+modify column if exists `warm_launch.app_visible_uptime` UInt32,
+modify column if exists `warm_launch.on_next_draw_uptime` UInt32,
+modify column if exists `hot_launch.app_visible_uptime` UInt32,
+modify column if exists `hot_launch.on_next_draw_uptime` UInt32,
+modify column if exists `memory_usage.interval` UInt32,
+modify column if exists `cpu_usage.interval` UInt32;
+
