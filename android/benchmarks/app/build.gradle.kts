e9417a5c8fc16149ab8f8582c7d5f9a6f2d4ba69
Abhay Sood <soodabhay23@gmail.com>
2024-09-16 14:34:17 +0530

    chore(android): update sdk version to 35 and agp to 8.6
    chore(android): fix nullable warning
    
    chore(android): update agp to 8.6
    
    chore(android): run functional tests for new AGP versions
    
    chore(android): fix android tests build

--- android/benchmarks/app/build.gradle.kts
+++ android/benchmarks/app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "sh.measure.test.benchmark"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "sh.measure.test.benchmark"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- android/measure/src/androidTest/java/sh/measure/android/TestMeasureInitializer.kt
+++ android/measure/src/androidTest/java/sh/measure/android/TestMeasureInitializer.kt
@@ -133,12 +133,14 @@ internal class TestMeasureInitializer(
     private val procProvider: ProcProvider = ProcProviderImpl(),
     private val debugProvider: DebugProvider = DefaultDebugProvider(),
     private val runtimeProvider: RuntimeProvider = DefaultRuntimeProvider(),
+    private val osSysConfProvider: OsSysConfProvider = OsSysConfProviderImpl(),
     private val memoryReader: MemoryReader = DefaultMemoryReader(
         logger = logger,
         processInfo = processInfoProvider,
         procProvider = procProvider,
         debugProvider = debugProvider,
         runtimeProvider = runtimeProvider,
+        osSysConfProvider = osSysConfProvider,
     ),
     private val localeProvider: LocaleProvider = LocaleProviderImpl(),
     private val prefsStorage: PrefsStorage = PrefsStorageImpl(context = application),
@@ -166,6 +168,7 @@ internal class TestMeasureInitializer(
         logger,
         context = application,
         localeProvider = localeProvider,
+        osSysConfProvider = osSysConfProvider,
     ),
     private val appAttributeProcessor: AppAttributeProcessor = AppAttributeProcessor(
         context = application,
@@ -243,7 +246,6 @@ internal class TestMeasureInitializer(
         processInfoProvider = processInfoProvider,
     ),
     override val periodicEventExporter: PeriodicEventExporter = NoopPeriodicEventExporter(),
-    private val osSysConfProvider: OsSysConfProvider = OsSysConfProviderImpl(),
     override val unhandledExceptionCollector: UnhandledExceptionCollector = UnhandledExceptionCollector(
         logger = logger,
         timeProvider = timeProvider,
