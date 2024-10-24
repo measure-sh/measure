716681bfe4b1e4124d96ca8d1c84c739edd75def
Abhay Sood <soodabhay23@gmail.com>
2024-10-24 12:18:41 +0530

    fix(android): handle session management when elapsed time gets reset
    We're using elapsed time since boot to store createdAt and lastEventTime
    for recent session. This allows us to ensure that session management does
    not get affected by any change in wall clock time.
    
    However, we didn't handle the case where the device get rebooted and the
    atomic clock is reset. This PR fixes that.
    
    closes #1393

--- android/measure/src/main/java/sh/measure/android/RecentSession.kt
+++ android/measure/src/main/java/sh/measure/android/RecentSession.kt
@@ -10,9 +10,21 @@ import kotlinx.serialization.Serializable
  */
 @Serializable
 internal data class RecentSession(
+    /**
+     * The session id.
+     */
     val id: String,
+    /**
+     * The time since boot when the session was created at.
+     */
     val createdAt: Long,
+    /**
+     * The time since boot when the last event was tracked.
+     */
     val lastEventTime: Long = 0,
+    /**
+     * Whether this session crashed or not.
+     */
     val crashed: Boolean = false,
 ) {
     fun hasTrackedEvent(): Boolean {
