204d270a668451dd7a63d937dba01a25440aae75
Debjeet Biswas <detj@users.noreply.github.com>
2024-10-31 23:12:55 +0530

    chore(backend): add data skipping indexes for fingerprints (#1455)
    - add data skipping indexes for exception.fingerprint
    - add data skipping indexes for anr.fingerprint
    - add clickhouse migration and rollback
    
    fixes #1454
    
    Signed-off-by: detj <detj@users.noreply.github.com>

---
+++ self-host/clickhouse/20241031172411_alter_events_table.sql
@@ -0,0 +1,12 @@
+-- migrate:up
+alter table default.events
+  add index if not exists exception_fingerprint_bloom_idx `exception.fingerprint` type bloom_filter granularity 4,
+  add index if not exists anr_fingerprint_bloom_idx `anr.fingerprint` type bloom_filter granularity 4,
+  materialize index if exists exception_fingerprint_bloom_idx,
+  materialize index if exists anr_fingerprint_bloom_idx;
+
+-- migrate:down
+alter table default.events
+  drop index if exists exception_fingerprint_bloom_idx,
+  drop index if exists anr_fingerprint_bloom_idx;
+
