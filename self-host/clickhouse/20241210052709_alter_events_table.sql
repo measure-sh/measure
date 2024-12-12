08061c38ab555087b2a2bef06f99752b457d8d81
Debjeet Biswas <detj@users.noreply.github.com>
2024-12-12 11:59:56 +0530

    fix(backend): improve ios support (#1599)
    - support ipa build type in PUT `/builds` API
    - increase `lifecycle_view_controller.class_name` character size to 256
    - increase `lifecycle_swift_ui.class_name` chracterer size to 256
    
    Signed-off-by: detj <detj@users.noreply.github.com>

--- self-host/clickhouse/20241210052709_alter_events_table.sql
+++ self-host/clickhouse/20241210052709_alter_events_table.sql
@@ -2,9 +2,9 @@
 alter table events
     add column if not exists `attribute.device_cpu_arch` LowCardinality(FixedString(16)) after `attribute.device_thermal_throttling_enabled`,
     add column if not exists `lifecycle_view_controller.type` LowCardinality(FixedString(32)) after `lifecycle_fragment.tag`,
-    add column if not exists `lifecycle_view_controller.class_name` LowCardinality(FixedString(128)) after `lifecycle_view_controller.type`,
+    add column if not exists `lifecycle_view_controller.class_name` LowCardinality(FixedString(256)) after `lifecycle_view_controller.type`,
     add column if not exists `lifecycle_swift_ui.type` LowCardinality(FixedString(16)) after `lifecycle_view_controller.class_name`,
-    add column if not exists `lifecycle_swift_ui.class_name` LowCardinality(FixedString(128)) after `lifecycle_swift_ui.type`,
+    add column if not exists `lifecycle_swift_ui.class_name` LowCardinality(FixedString(256)) after `lifecycle_swift_ui.type`,
     add column if not exists `memory_usage_absolute.max_memory` UInt64 after `memory_usage.interval`,
     add column if not exists `memory_usage_absolute.used_memory` UInt64 after `memory_usage_absolute.max_memory`,
     add column if not exists `memory_usage_absolute.interval` UInt64 after `memory_usage_absolute.used_memory`,
