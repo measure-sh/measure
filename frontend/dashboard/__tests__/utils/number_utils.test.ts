1e56df2370161ba110a0b0e123cefd54ae23d371
detj <detj@users.noreply.github.com>
2025-01-02 11:37:55 +0530

    test(frontend): update incorrect values
    Signed-off-by: detj <detj@users.noreply.github.com>

--- frontend/dashboard/__tests__/utils/number_utils.test.ts
+++ frontend/dashboard/__tests__/utils/number_utils.test.ts
@@ -3,7 +3,7 @@ import { expect, it, describe } from '@jest/globals';
 
 describe('kilobytesToMegabytes', () => {
   it('should convert kilobytes to megabytes', () => {
-    expect(kilobytesToMegabytes(10485760)).toBe(10)
-    expect(kilobytesToMegabytes(1024 * 1024)).toBe(1)
+    expect(kilobytesToMegabytes(10485760)).toBe(10240)
+    expect(kilobytesToMegabytes(1024)).toBe(1)
   })
 })
