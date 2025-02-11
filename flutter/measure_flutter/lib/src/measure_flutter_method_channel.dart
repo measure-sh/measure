d2334aa1880f2f379d3bff455169c24f850ce416
Abhay Sood <soodabhay23@gmail.com>
2025-02-11 18:40:14 +0530

    chore(flutter): remove unneeded comments in generated code

--- flutter/measure_flutter/lib/src/measure_flutter_method_channel.dart
+++ flutter/measure_flutter/lib/src/measure_flutter_method_channel.dart
@@ -3,9 +3,7 @@ import 'package:flutter/services.dart';
 
 import 'measure_flutter_platform_interface.dart';
 
-/// An implementation of [MeasureFlutterPlatform] that uses method channels.
 class MethodChannelMeasureFlutter extends MeasureFlutterPlatform {
-  /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('measure_flutter');
 
