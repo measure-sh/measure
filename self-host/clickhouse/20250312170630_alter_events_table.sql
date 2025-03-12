f1bcd01fbeb52d17a85ccad09b42bd2701e34d44
detj <detj@users.noreply.github.com>
2025-03-12 22:43:02 +0530

    chore(backend): reorder clickhouse migration
    Signed-off-by: detj <detj@users.noreply.github.com>

--- self-host/clickhouse/20250312170630_alter_events_table.sql
+++ self-host/clickhouse/20250312170630_alter_events_table.sql
@@ -3,6 +3,7 @@ alter table events
     add column if not exists `exception.binary_images` String after `exception.foreground`,
     comment column `exception.binary_images` 'list of apple crash binary images';
 
+
 -- migrate:down
 alter table events
   drop column if exists `exception.binary_images`;
