e475ae367b22d4bc5f4c3702dd53166ae69963a2
Abhay Sood <soodabhay23@gmail.com>
2025-03-31 21:31:43 +0530

    chore(android): extract attribute keys
    Move attribute keys from AppAttributeProcessor
    to the centrally defined attributes in Attribute
    object.

--- android/measure/src/main/java/sh/measure/android/attributes/AppAttributeProcessor.kt
+++ android/measure/src/main/java/sh/measure/android/attributes/AppAttributeProcessor.kt
@@ -12,17 +12,13 @@ internal class AppAttributeProcessor(
     private val packageInfoProvider: PackageInfoProvider,
     private val context: Context,
 ) : ComputeOnceAttributeProcessor() {
-    private val appVersionKey = "app_version"
-    private val appBuildKey = "app_build"
-    private val appUniqueIdKey = "app_unique_id"
-    private val measureSdkVersion = "measure_sdk_version"
 
     override fun computeAttributes(): Map<String, Any?> {
         return mapOf(
-            appVersionKey to packageInfoProvider.appVersion,
-            appBuildKey to packageInfoProvider.getVersionCode(),
-            appUniqueIdKey to context.packageName,
-            measureSdkVersion to BuildConfig.MEASURE_SDK_VERSION,
+            Attribute.APP_VERSION_KEY to packageInfoProvider.appVersion,
+            Attribute.APP_BUILD_KEY to packageInfoProvider.getVersionCode(),
+            Attribute.APP_UNIQUE_ID_KEY to context.packageName,
+            Attribute.MEASURE_SDK_VERSION to BuildConfig.MEASURE_SDK_VERSION,
         )
     }
 }
