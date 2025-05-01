7b26906fb6ff67d77a17e538647f7f672e30f018
Abhay Sood <soodabhay23@gmail.com>
2025-05-01 14:38:19 +0530

    chore(flutter): record events with flutter platform

--- flutter/measure_flutter/example/ios/Runner/AppDelegate.swift
+++ flutter/measure_flutter/example/ios/Runner/AppDelegate.swift
@@ -9,8 +9,11 @@ import Measure
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    let config = BaseMeasureConfig(enableLogging: true)
-    let clientInfo = ClientInfo(apiKey: "<apiKey>", apiUrl: "<apiUrl>")
+    let config = BaseMeasureConfig(
+        enableLogging: true,
+        samplingRateForErrorFreeSessions: 1.0
+    )
+    let clientInfo = ClientInfo(apiKey: "msrsh_49154dbe42ba4649414c62059c72bd80fb7848fd74ad554d27d89edc5a107a5a_27ab5454", apiUrl: "https://3719-49-207-224-123.ngrok-free.app")
     Measure.shared.initialize(with: clientInfo, config: config)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
