dd6565682d62f935bf97197577d207c2823c081a
Abhay Sood <soodabhay23@gmail.com>
2025-05-05 15:37:15 +0530

    chore(backend): add os_name to builds API
    keeping the platform field as is for now
    can probably be removed without any backward
    compativility issues

--- backend/api/measure/mapping.go
+++ backend/api/measure/mapping.go
@@ -50,7 +50,6 @@ type BuildMapping struct {
 	AppID        uuid.UUID
 	VersionName  string                  `form:"version_name" binding:"required"`
 	VersionCode  string                  `form:"version_code" binding:"required"`
-	Platform     string                  `form:"platform"` // Verify if this can be removed
 	OSName       string                  `form:"os_name"`
 	MappingType  string                  `form:"mapping_type" binding:"required_with=File"`
 	Files        []*multipart.FileHeader `form:"mapping_file" binding:"required_with=MappingType"`
