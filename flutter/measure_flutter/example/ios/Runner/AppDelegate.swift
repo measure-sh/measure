0705bc18564991156c83c4507b0e55b2cd4261e3
Abhay Sood <soodabhay23@gmail.com>
2025-05-17 16:10:55 +0530

    chore(backend): re-record ios sessions

--- flutter/measure_flutter/example/ios/Runner/AppDelegate.swift
+++ flutter/measure_flutter/example/ios/Runner/AppDelegate.swift
@@ -20,12 +20,12 @@ import Measure
     #if DEBUG
     clientInfo = ClientInfo(
       apiKey: "msrsh_7c22875ec57832fa068f8f74e8a4663dd38688e462ca9bb5f70e2028ac0d5cd7_4461dc8e",
-      apiUrl: "https://be1a-2401-4900-1c2b-5214-9494-4f78-a8d8-51ae.ngrok-free.app"
+      apiUrl: "https://humbly-natural-polliwog.ngrok-free.app"
     )
     #else
     clientInfo = ClientInfo(
       apiKey: "msrsh_49154dbe42ba4649414c62059c72bd80fb7848fd74ad554d27d89edc5a107a5a_27ab5454",
-      apiUrl: "https://be1a-2401-4900-1c2b-5214-9494-4f78-a8d8-51ae.ngrok-free.app"
+      apiUrl: "https://humbly-natural-polliwog.ngrok-free.app"
     )
     #endif
 
