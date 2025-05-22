a8603b4c99078657d941f283a91019aa43089c33
Anup Cowkur <anupcowkur89@gmail.com>
2025-05-22 15:07:41 +0530

    chore(frontend): update stack trace UI
    closes #2179

--- frontend/dashboard/tailwind.config.ts
+++ frontend/dashboard/tailwind.config.ts
@@ -17,7 +17,21 @@ const config: Config = {
       },
       colors: {
         'redd': '#ff6286',
-      }
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [require("@tailwindcss/forms")],
