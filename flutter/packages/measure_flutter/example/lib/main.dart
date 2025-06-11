1921cbf3f0a1c0bc47512d87352ad50f662f3852
Abhay Sood <abhay@measure.sh>
2025-06-11 19:12:51 +0530

    chore(flutter): add example app (#2301)
    - adds example app to measure_flutter plugin
    - this fixes how flutter tooling integrates
    with IDEs to allow opening plugin code in
    Xcode or Android Studio

---
+++ flutter/packages/measure_flutter/example/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('Example'),
+        ),
+      ),
+    );
+  }
+}
