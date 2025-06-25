daac73880c5bf24c06406b2da2e56e104f752dc2
detj <detj@users.noreply.github.com>
2025-06-25 11:16:40 +0530

    chore(backend): add database init scripts
    - add postgres init script
    - add clickhouse init script
    - modify initial dev config to be compatible
    - update compose.yml
    
    Signed-off-by: detj <detj@users.noreply.github.com>

--- self-host/clickhouse/init-clickhouse.sh
+++ self-host/clickhouse/init-clickhouse.sh
@@ -2,27 +2,17 @@
 
 set -e
 
-clickhouse-client -n --query "CREATE DATABASE IF NOT EXISTS measure;"
-
-# If there are any tables in the 'default' database, move them to the 'measure'
-# database.
-TABLE_COUNT_DEFAULT=$(clickhouse-client -n --query "SELECT count() from system.tables WHERE database = 'default';")
-TABLE_COUNT_MEASURE=$(clickhouse-client -n --query "SELECT count() from system.tables WHERE database = 'measure';")
-
-if [[ "$TABLE_COUNT_DEFAULT" -gt 0 && "$TABLE_COUNT_MEASURE" -eq 0 ]]; then
-  echo "Existing tables found in 'default' database. Migrating to 'measure' database."
-
-  # Get all tables in the 'default' database and
-  # rename them one by one.
-  clickhouse-client -n --query "SELECT name from system.tables WHERE database = 'default' FORMAT TabSeparated" | while read -r TBL; do
-    echo "Moving table: $TBL"
-    clickhouse-client -n --query "RENAME TABLE default.$TBL TO measure.$TBL;"
-  done
-fi
+clickhouse-client --query "CREATE DATABASE IF NOT EXISTS measure;"
 
 clickhouse-client -n <<-EOSQL
-  CREATE USER IF NOT EXISTS operator IDENTIFIED WITH sha256_password BY '${CLICKHOUSE_OPERATOR_PASSWORD}';
-  CREATE USER IF NOT EXISTS admin IDENTIFIED WITH sha256_password BY '${CLICKHOUSE_READER_PASSWORD}';
+  create role if not exists operator;
+  create role if not exists reader;
+  -- grant select, insert on measure.* to operator;
+  -- grant select on measure.* to reader;
+
+  create user if not exists ${CLICKHOUSE_OPERATOR_USER} identified with sha256_password by '${CLICKHOUSE_OPERATOR_PASSWORD}' default role operator default database measure;
+
+  create user if not exists ${CLICKHOUSE_READER_USER} identified with sha256_password by '${CLICKHOUSE_READER_PASSWORD}' default role reader default database measure;
 
   GRANT ALL ON measure.* TO operator;
   GRANT SELECT ON measure.* TO reader;
--- self-host/postgres/init-postgres.sh
+++ self-host/postgres/init-postgres.sh
@@ -6,39 +6,16 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 -- Create schema if not exists
 CREATE SCHEMA IF NOT EXISTS measure;
 
--- Check if apps table exists in 'public' schema
--- and if apps does not exists in 'measure' schema
--- then migrate all tables & sequences from 'public'
--- schema to 'measure' schema.
-DO \$\$
-BEGIN
-  IF EXISTS (SELECT 1 from information_schema.tables WHERE table_schema = 'public' AND table_name = 'apps') AND NOT EXISTS (SELECT 1 from information_schema.tables WHERE table_schema = 'measure' AND table_name = 'apps') THEN
-
-    -- Move all tables from 'public' to 'measure' schema
-    FOR TBL in SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'
-    LOOP
-      EXECUTE format('ALTER TABLE public.%I SET SCHEMA measure', TBL);
-    END LOOP;
-
-    -- Move all sequences from 'public' to 'measure' schema
-    FOR SEQ IN SELECT sequence_name FROM information_schema.sequences WHERE sequence_schema = 'public'
-    LOOP
-      EXECUTE format('ALTER SEQUENCE public.%I SET SCHEMA measure', SEQ);
-    END LOOP;
-  END IF;
-END;
-\$\$;
-
 -- Create roles, grant permissions and create users.
 -- Idempotent, safe to run on every startup.
 DO \$\$
 BEGIN
   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'operator') THEN
-    CREATE ROLE operator;
+    CREATE ROLE operator NOLOGIN;
   END IF;
 
   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'reader') THEN
-    CREATE ROLE reader;
+    CREATE ROLE reader NOLOGIN;
   END IF;
 END
 \$\$;
@@ -57,17 +34,6 @@ GRANT SELECT ON ALL TABLES IN SCHEMA measure TO reader;
 ALTER DEFAULT PRIVILEGES IN SCHEMA measure GRANT SELECT ON TABLES to reader;
 
 -- Create application users and assign them to roles
-DO \$\$
-BEGIN
-  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'measure_operator') THEN
-    CREATE USER measure_operator WITH PASSWORD '${POSTGRES_OPERATOR_PASSWORD}';
-  END IF;
-  GRANT operator TO measure_operator;
-
-  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'measure_reader') THEN
-    CREATE USER measure_reader WITH PASSWORD '${POSTGRES_READER_PASSWORD}';
-  END IF;
-  GRANT reader TO measure_reader;
-END
-\$\$;
+GRANT operator TO ${POSTGRES_USER};
+GRANT reader TO ${POSTGRES_USER};
 EOSQL
