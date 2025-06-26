3b404afc28527f000fc9ad70d13a813cbcba5a38
detj <detj@users.noreply.github.com>
2025-06-26 12:58:41 +0530

    chore(backend): update clickhouse init script
    Signed-off-by: detj <detj@users.noreply.github.com>

--- self-host/clickhouse/init-clickhouse.sh
+++ self-host/clickhouse/init-clickhouse.sh
@@ -4,15 +4,15 @@ set -e
 
 clickhouse-client --query "CREATE DATABASE IF NOT EXISTS measure;"
 
-clickhouse-client -n <<-EOSQL
+clickhouse-client -n --database measure <<-EOSQL
   create role if not exists operator;
   create role if not exists reader;
   -- grant select, insert on measure.* to operator;
   -- grant select on measure.* to reader;
 
-  create user if not exists ${CLICKHOUSE_OPERATOR_USER} identified with sha256_password by '${CLICKHOUSE_OPERATOR_PASSWORD}' default role operator default database measure;
+  create user if not exists '${CLICKHOUSE_OPERATOR_USER}' identified with sha256_password by '${CLICKHOUSE_OPERATOR_PASSWORD}' default role operator default database measure;
 
-  create user if not exists ${CLICKHOUSE_READER_USER} identified with sha256_password by '${CLICKHOUSE_READER_PASSWORD}' default role reader default database measure;
+  create user if not exists '${CLICKHOUSE_READER_USER}' identified with sha256_password by '${CLICKHOUSE_READER_PASSWORD}' default role reader default database measure;
 
   GRANT ALL ON measure.* TO operator;
   GRANT SELECT ON measure.* TO reader;
