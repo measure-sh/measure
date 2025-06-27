d104bd7d329030d16faf9a249dcc9bd92f13a7fb
detj <detj@users.noreply.github.com>
2025-06-27 14:16:49 +0530

    chore(backend): update clickhouse init script
    Signed-off-by: detj <detj@users.noreply.github.com>

--- self-host/clickhouse/init-clickhouse.sh
+++ self-host/clickhouse/init-clickhouse.sh
@@ -2,28 +2,29 @@
 
 set -euo pipefail
 
+# Primary database name
+DB_NAME=measure
+
 clickhouse-client \
   --user default \
-  --query "create user app_admin identified with sha256_password by '${CLICKHOUSE_ADMIN_PASSWORD}'" \
-  --query "grant all on *.* to app_admin with grant option;" \
-  --query "CREATE DATABASE IF NOT EXISTS measure;"
-  # --query "create role if not exists operator;" \
-  # --query "create role if not exists reader;"
+  --query "create user '${CLICKHOUSE_ADMIN_USER}' identified with sha256_password by '${CLICKHOUSE_ADMIN_PASSWORD}'" \
+  --query "grant all on *.* to '${CLICKHOUSE_ADMIN_USER}' with grant option;" \
+  --query "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 
 clickhouse-client -n \
-  --database measure \
+  --database "$DB_NAME" \
   --user "${CLICKHOUSE_ADMIN_USER}" \
   --password "${CLICKHOUSE_ADMIN_PASSWORD}" \
   <<-EOSQL
   create role if not exists operator;
   create role if not exists reader;
-  grant select, insert on measure.* to operator;
-  grant select on measure.* to reader;
+  grant select, insert on $DB_NAME.* to operator;
+  grant select on $DB_NAME.* to reader;
 
-  create user if not exists '${CLICKHOUSE_OPERATOR_USER}' identified with sha256_password by '${CLICKHOUSE_OPERATOR_PASSWORD}' default role operator default database measure;
+  create user if not exists '${CLICKHOUSE_OPERATOR_USER}' identified with sha256_password by '${CLICKHOUSE_OPERATOR_PASSWORD}' default role operator default database $DB_NAME;
 
-  create user if not exists '${CLICKHOUSE_READER_USER}' identified with sha256_password by '${CLICKHOUSE_READER_PASSWORD}' default role reader default database measure;
+  create user if not exists '${CLICKHOUSE_READER_USER}' identified with sha256_password by '${CLICKHOUSE_READER_PASSWORD}' default role reader default database $DB_NAME;
 
-  GRANT ALL ON measure.* TO operator;
-  GRANT SELECT ON measure.* TO reader;
+  GRANT ALL ON $DB_NAME.* TO operator;
+  GRANT SELECT ON $DB_NAME.* TO reader;
 EOSQL
