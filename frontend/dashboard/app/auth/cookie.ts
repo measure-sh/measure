f21695b72a721d90142ee8c809be48e23828e91b
detj <detj@users.noreply.github.com>
2025-07-11 21:59:30 +0530

    chore(backend): restore previous cookie behavior
    Signed-off-by: detj <detj@users.noreply.github.com>

--- frontend/dashboard/app/auth/cookie.ts
+++ frontend/dashboard/app/auth/cookie.ts
@@ -15,17 +15,13 @@ export function setCookiesFromJWT(accessToken: string, refreshToken: string, res
 
     // extract the domain from the NEXT_PUBLIC_SITE_URL
     // and set it as the domain for the cookies
-    let { hostname } = new URL(process.env.NEXT_PUBLIC_SITE_URL!)
-
-    if (!hostname.includes("localhost") && !hostname.includes("127.0.0.1")) {
-      hostname = "measure.sh"
-    }
+    const domain = new URL(process.env.NEXT_PUBLIC_SITE_URL!).hostname
 
     const isDev = process.env.NODE_ENV === 'development'
 
     response.cookies.set('access_token', accessToken, {
         path: '/',
-        domain: hostname,
+        domain: domain,
         maxAge: Math.floor((accessExp.getTime() - Date.now()) / 1000),
         httpOnly: true,
         secure: !isDev,
@@ -34,7 +30,7 @@ export function setCookiesFromJWT(accessToken: string, refreshToken: string, res
 
     response.cookies.set('refresh_token', refreshToken, {
         path: '/',
-        domain: hostname,
+        domain: domain,
         maxAge: Math.floor((refreshExp.getTime() - Date.now()) / 1000),
         httpOnly: true,
         secure: !isDev,
@@ -47,17 +43,13 @@ export function setCookiesFromJWT(accessToken: string, refreshToken: string, res
 export function clearCookies(response: NextResponse<any>): NextResponse<any> {
     // extract the domain from the NEXT_PUBLIC_SITE_URL
     // and set it as the domain for the cookies
-    let { hostname } = new URL(process.env.NEXT_PUBLIC_SITE_URL!)
-
-    if (!hostname.includes("localhost") && !hostname.includes("127.0.0.1")) {
-      hostname = "measure.sh"
-    }
+    const domain = new URL(process.env.NEXT_PUBLIC_SITE_URL!).hostname
 
     const isDev = process.env.NODE_ENV === 'development'
 
     response.cookies.set('access_token', '', {
         path: '/',
-        domain: hostname,
+        domain: domain,
         maxAge: -1,
         httpOnly: true,
         secure: !isDev,
@@ -66,7 +58,7 @@ export function clearCookies(response: NextResponse<any>): NextResponse<any> {
 
     response.cookies.set('refresh_token', '', {
         path: '/',
-        domain: hostname,
+        domain: domain,
         maxAge: -1,
         httpOnly: true,
         secure: !isDev,
