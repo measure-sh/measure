d13be3665c256b73a01434095ac249c862296e5b
Adwin Ross <adwinross@gmail.com>
2025-07-23 16:54:23 +0530

    chore(rn): add measure sdk to android

--- react-native/android/src/main/java/com/measure/MeasureModule.kt
+++ react-native/android/src/main/java/com/measure/MeasureModule.kt
@@ -1,19 +1,36 @@
 package com.measure
 
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import sh.measure.android.Measure
+import sh.measure.android.config.ClientInfo
+import sh.measure.android.config.MeasureConfig
+import sh.measure.android.config.ScreenshotMaskLevel
 
-class MeasureModule(reactContext: ReactApplicationContext) :
-  ReactContextBaseJavaModule(reactContext) {
+class MeasureModule(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
   override fun getName(): String = "MeasureModule"
 
   @ReactMethod
   fun initialize(apiKey: String, promise: Promise) {
     try {
-      // TODO: Call your actual SDK init method here if you have one
+      val context = reactContext.applicationContext
+      var clientInfo = ClientInfo(apiKey = apiKey, apiUrl = "http://localhost:8080")
+      val config = MeasureConfig(
+        enableLogging = true,
+        trackScreenshotOnCrash = true,
+        screenshotMaskLevel = ScreenshotMaskLevel.SensitiveFieldsOnly,
+        trackHttpHeaders = true,
+        trackHttpBody = true,
+        trackActivityIntentData = true,
+        httpUrlBlocklist = listOf("http://localhost:8080"),
+        samplingRateForErrorFreeSessions = 1f,
+        autoStart = true,
+        traceSamplingRate = 1.0f
+      )
+      Measure.init(context, measureConfig = config, clientInfo = clientInfo)
       println("📦 Measure SDK initialized with key: $apiKey")
 
       // Return success message to JS
