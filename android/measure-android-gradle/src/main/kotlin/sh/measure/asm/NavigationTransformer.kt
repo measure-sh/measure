592f6012743ef0429ca43c15ac43a97533691ca2
Abhay Sood <abhay@measure.sh>
2025-07-25 12:56:16 +0530

    feat(android): support 2.9.2 androidx compose navigation instrumentation

--- android/measure-android-gradle/src/main/kotlin/sh/measure/asm/NavigationTransformer.kt
+++ android/measure-android-gradle/src/main/kotlin/sh/measure/asm/NavigationTransformer.kt
@@ -15,8 +15,8 @@ class NavigationTransformer : AsmBytecodeTransformer() {
     // Tested from 2.4.0, 2.3.5 does not have navigation-compose package
     override val minVersion = SemVer(2, 4, 0)
 
-    // Tested up-to 2.9.0-alpha01
-    override val maxVersion = SemVer(2, 9, 0)
+    // Tested up-to 2.9.2
+    override val maxVersion = SemVer(2, 9, 2)
 }
 
 abstract class NavigationVisitorFactory :
