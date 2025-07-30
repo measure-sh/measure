d205190546514890b34c82f004bd91113cedfb65
Adwin Ross <adwinross@gmail.com>
2025-07-30 08:49:57 +0530

    chore(rn): add expo support for android

--- react-native/react-native.config.js
+++ react-native/react-native.config.js
@@ -6,6 +6,7 @@ module.exports = {
         },
         android: {
           packageInstance: 'new MeasureModule()',
+          packageImportPath: 'import com.measure.MeasurePackage;',
         },
       },
     },
