b129510f9d7978590dc173cae64feff064df84ae
Anup Cowkur <anupcowkur89@gmail.com>
2025-08-12 15:53:44 +0530

    chore(frontend): update milis to human readable display format
    closes #2528

--- frontend/dashboard/__tests__/utils/time_utils.test.ts
+++ frontend/dashboard/__tests__/utils/time_utils.test.ts
@@ -9,26 +9,26 @@ describe('formatMillisToHumanReadable', () => {
 
     it('should return seconds for values between 1 second and 1 minute', () => {
         expect(formatMillisToHumanReadable(5000)).toBe('5s')
-        expect(formatMillisToHumanReadable(59999)).toBe('59s, 999ms')
+        expect(formatMillisToHumanReadable(59999)).toBe('59.999s')
     })
 
     it('should return minutes for values between 1 minute and 1 hour', () => {
-        expect(formatMillisToHumanReadable(60000)).toBe('1min')
-        expect(formatMillisToHumanReadable(120000)).toBe('2min')
-        expect(formatMillisToHumanReadable(3599999)).toBe('59min, 59s, 999ms')
+        expect(formatMillisToHumanReadable(60000)).toBe('1m')
+        expect(formatMillisToHumanReadable(120000)).toBe('2m')
+        expect(formatMillisToHumanReadable(3599999)).toBe('59m 59.999s')
     })
 
     it('should return hours for values between 1 hour and 1 day', () => {
         expect(formatMillisToHumanReadable(3600000)).toBe('1h')
         expect(formatMillisToHumanReadable(7200000)).toBe('2h')
-        expect(formatMillisToHumanReadable(86399999)).toBe('23h, 59min, 59s, 999ms')
+        expect(formatMillisToHumanReadable(86399999)).toBe('23h 59m 59.999s')
     })
 
     it('should return days for values greater than 1 day', () => {
         expect(formatMillisToHumanReadable(86400000)).toBe('1d')
         expect(formatMillisToHumanReadable(172800000)).toBe('2d')
         expect(formatMillisToHumanReadable(259200000)).toBe('3d')
-        expect(formatMillisToHumanReadable(604799999)).toBe('6d, 23h, 59min, 59s, 999ms')
+        expect(formatMillisToHumanReadable(604799999)).toBe('6d 23h 59m 59.999s')
     })
 
     it('should round decimal milliseconds to nearest int', () => {
