54e8f6efee4f95030fdab356309390962d322e82
detj <detj@users.noreply.github.com>
2025-08-21 07:19:44 +0530

    chore(frontend): configue proxy timeout in next config
    Signed-off-by: detj <detj@users.noreply.github.com>

--- frontend/dashboard/next.config.js
+++ frontend/dashboard/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    proxyTimeout: 90000,
+  },
   images: {
     remotePatterns: [
       {
