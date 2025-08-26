3a5744a304198622367fa3227037d86f721f7e99
Adwin Ross <adwinross@gmail.com>
2025-08-26 16:41:31 +0530

    fix(ios): update process start time logic

--- ios/Sources/MeasureSDK/Swift/Utils/SysCtl.swift
+++ ios/Sources/MeasureSDK/Swift/Utils/SysCtl.swift
@@ -149,35 +149,30 @@ final class BaseSysCtl: SysCtl {
         var size = MemoryLayout<UInt64>.size
         if sysctlbyname("hw.memsize", &memorySize, &size, nil, 0) == 0 {
             self.maximumAvailableRam = memorySize / 1024
-            return self.maximumAvailableRam
         }
-        return 0
+        return self.maximumAvailableRam
     }
 
     func getProcessStartTime() -> UnsignedNumber? {
         var mib: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
-        var procInfo = kinfo_proc()
-        var size = MemoryLayout.size(ofValue: procInfo)
 
-        let result = sysctl(&mib, UInt32(mib.count), &procInfo, &size, nil, 0)
-        guard result == 0 else {
-            return nil
-        }
+        var kproc = kinfo_proc()
+        var size = MemoryLayout<kinfo_proc>.stride
 
-        let startTime = procInfo.kp_proc.p_un.__p_starttime
-        return UnsignedNumber(startTime.tv_sec) + UnsignedNumber(startTime.tv_usec) / 1_000_000
+        let result = sysctl(&mib, u_int(mib.count), &kproc, &size, nil, 0)
+        guard result == 0 else { return nil }
+
+        let procStartTime = kproc.kp_proc.p_un.__p_starttime
+        let procStartTimeMillis = UInt64(procStartTime.tv_sec) * 1000 + UInt64(procStartTime.tv_usec) / 1000
+        return procStartTimeMillis
     }
 
     func getSystemBootTime() -> UnsignedNumber? {
-        var cmd = [CTL_KERN, KERN_BOOTTIME]
-        var bootTimeVal = timeval(tv_sec: 0, tv_usec: 0)
-        var size = MemoryLayout.size(ofValue: bootTimeVal)
+        var bootTime = timeval()
+        var size = MemoryLayout<timeval>.stride
+        var mib: [Int32] = [CTL_KERN, KERN_BOOTTIME]
+        guard sysctl(&mib, 2, &bootTime, &size, nil, 0) == 0 else { return nil }
 
-        let result = sysctl(&cmd, UInt32(cmd.count), &bootTimeVal, &size, nil, 0)
-        if result != 0 {
-            return nil
-        }
-
-        return UnsignedNumber(bootTimeVal.tv_sec) + UnsignedNumber(bootTimeVal.tv_usec) / 1_000_000
+        return UnsignedNumber(bootTime.tv_sec) * 1000 + UnsignedNumber(bootTime.tv_usec) / 1000
     }
 }
