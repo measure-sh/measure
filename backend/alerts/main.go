1658cc75cbad0e985330b69ca587ebed03a77bf1
Anup Cowkur <anupcowkur89@gmail.com>
2025-09-05 15:16:12 +0530

    chore(backend): check for alerts at start of every hour
    closes #2616

--- backend/alerts/main.go
+++ backend/alerts/main.go
@@ -79,7 +79,7 @@ func main() {
 
 func initCron(ctx context.Context) *cron.Cron {
 	cron := cron.New()
-	cron.AddFunc("@hourly", func() { alerts.CreateCrashAndAnrAlerts(ctx) })
+	cron.AddFunc("0 0 * * * *", func() { alerts.CreateCrashAndAnrAlerts(ctx) })
 	cron.AddFunc("@daily", func() { alerts.CreateDailySummary(ctx) })
 	cron.AddFunc("@every 5m", func() { email.SendPendingAlertEmails(ctx) })
 	cron.Start()
