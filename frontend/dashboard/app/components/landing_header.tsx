5e362693b3a1f4c0cf6ae4099370c5b115aa818f
Debjeet Biswas <detj@users.noreply.github.com>
2025-10-06 18:28:47 +0530

    chore(frontend): always show login button on dashboard homepage (#2708)
    fixes #2707
    
    Signed-off-by: detj <detj@users.noreply.github.com>

--- frontend/dashboard/app/components/landing_header.tsx
+++ frontend/dashboard/app/components/landing_header.tsx
@@ -1,50 +1,51 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
-import { useScrollDirection } from '../utils/scroll_utils'
-import { cn } from '../utils/shadcn_utils'
-import { buttonVariants } from './button'
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useScrollDirection } from "../utils/scroll_utils";
+import { cn } from "../utils/shadcn_utils";
+import { buttonVariants } from "./button";
 
 export default function LandingHeader() {
-  const scrollDir = useScrollDirection()
-  const [isFocused, setIsFocused] = useState(false)
-  const router = useRouter()
-  const [selfHosted, setSelfHosted] = useState(false)
-
-  useEffect(() => {
-    if (typeof window !== undefined && (window.location.hostname === 'measure.sh' || window.location.hostname === 'www.measure.sh')) {
-      setSelfHosted(false)
-    } else {
-      setSelfHosted(true)
-    }
-  }, [])
+  const scrollDir = useScrollDirection();
+  const [isFocused, setIsFocused] = useState(false);
+  const router = useRouter();
 
   return (
-    <header onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} className={`w-full flex flex-col z-50 bg-white fixed top-0 transition-transform duration-100 ease-in-out ${scrollDir === 'scrolling down' && isFocused === false ? '-translate-y-full' : 'translate-y-0'}`}>
+    <header
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      className={`w-full flex flex-col z-50 bg-white fixed top-0 transition-transform duration-100 ease-in-out ${scrollDir === "scrolling down" && isFocused === false
+          ? "-translate-y-full"
+          : "translate-y-0"
+        }`}
+    >
       <div className="w-full flex flex-col md:flex-row space-between items-center py-4 pl-4 pr-2">
-        <button className="outline-hidden overflow-hidden p-1 border border-black rounded-full hover:bg-yellow-200 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] bg-black font-display font-display transition-colors duration-100" onClick={() => router.push('/')}>
+        <button
+          className="outline-hidden overflow-hidden p-1 border border-black rounded-full hover:bg-yellow-200 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] bg-black font-display font-display transition-colors duration-100"
+          onClick={() => router.push("/")}
+        >
           <Image
-            src='/images/measure_logo.svg'
+            src="/images/measure_logo.svg"
             width={24}
             height={24}
-            alt={'Measure logo'} />
+            alt={"Measure logo"}
+          />
         </button>
         <div className="py-2 md:py-0 md:flex md:grow" />
-        {selfHosted && <Link href="/auth/login" className={cn(buttonVariants({ variant: "outline" }), "font-display border border-black rounded-md select-none")}>Login</Link>}
-        {!selfHosted && <Link href="https://github.com/measure-sh/measure" className={cn(buttonVariants({ variant: "outline" }), "font-display border border-black rounded-md select-none")}>
-          <Image
-            src='/images/github_logo.svg'
-            width={24}
-            height={24}
-            alt={'Github logo'} />
-          <div className='px-1' />
-          <p className='mt-1'>Get Started</p>
-        </Link>}
+        <Link
+          href="/auth/login"
+          className={cn(
+            buttonVariants({ variant: "outline" }),
+            "font-display border border-black rounded-md select-none",
+          )}
+        >
+          Login
+        </Link>
       </div>
       <div className="w-full border-[0.1px] border-stone-900" />
     </header>
-  )
+  );
 }
