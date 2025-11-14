24b7da8bca86888b5c39218b6b511e7d82cfbc5a
detj <detj@users.noreply.github.com>
2025-11-14 09:14:15 +0530

    chore(backend): update events & spans table schema
    [skip ci]
    
    Signed-off-by: detj <detj@users.noreply.github.com>

--- self-host/clickhouse/20251106133229_alter_events_table.sql
+++ self-host/clickhouse/20251106133229_alter_events_table.sql
@@ -1,7 +1,7 @@
 -- migrate:up
 alter table events
 add column if not exists team_id UUID CODEC(ZSTD(3)) after session_id,
-add column if not exists inserted_at DateTime DEFAULT now() CODEC(Delta, ZSTD(3)) after app_id;
+add column if not exists inserted_at DateTime64(3, 'UTC') DEFAULT now64() CODEC(Delta, ZSTD(3)) after app_id;
 
 -- migrate:down
 alter table events
--- self-host/clickhouse/20251106133312_alter_spans_table.sql
+++ self-host/clickhouse/20251106133312_alter_spans_table.sql
@@ -1,7 +1,7 @@
 -- migrate:up
 alter table spans
 add column if not exists team_id UUID CODEC(ZSTD(3)) first,
-add column if not exists inserted_at DateTime DEFAULT now() CODEC(Delta, ZSTD(3)) after app_id;
+add column if not exists inserted_at DateTime64(3, 'UTC') DEFAULT now64() CODEC(Delta, ZSTD(3)) after app_id;
 
 -- migrate:down
 alter table events
