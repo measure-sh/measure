c54a6b52235a12b116186e8b6251fb87de914c9f
detj <detj@users.noreply.github.com>
2025-12-23 03:38:07 +0530

    docs(backend): improve code comment
    Signed-off-by: detj <detj@users.noreply.github.com>

--- backend/api/logcomment/settings.go
+++ backend/api/logcomment/settings.go
@@ -8,6 +8,9 @@ import (
 
 // WithSettingsPut puts the key & value in the logcomment, updates settings
 // and returns a usable context.
+//
+// NOTE: This method is unsafe when called from multiple goroutines
+// conncurrently.
 func WithSettingsPut(ctx context.Context, settings clickhouse.Settings, lc *Fields, key, val string) context.Context {
 	settings["log_comment"] = lc.MustPut(key, val).String()
 	return clickhouse.Context(ctx, clickhouse.WithSettings(settings))
