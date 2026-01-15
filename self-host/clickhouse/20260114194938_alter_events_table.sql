ff22c183bb66eb2237bde3a909fbcead39bf65d3
detj <detj@users.noreply.github.com>
2026-01-15 13:47:08 +0530

    fix(backend): update migration for device manufacturer attribute
    fixes #3080
    
    Signed-off-by: detj <detj@users.noreply.github.com>

--- self-host/clickhouse/20260114194938_alter_events_table.sql
+++ self-host/clickhouse/20260114194938_alter_events_table.sql
@@ -1,7 +1,22 @@
 -- migrate:up
 alter table events
-modify column if exists `attribute.device_manufacturer` FixedString(256);
+drop index if exists attribute_device_manufacturer_idx;
 
+-- migrate:down
+alter table events
+add index if not exists attribute_device_manufacturer_idx  `attribute.device_manufacturer` TYPE minmax GRANULARITY 2 after attribute_device_name_idx;
+
+-- migrate:up
+alter table events
+modify column if exists `attribute.device_manufacturer` FixedString(256);
 
 -- migrate:down
 select 1;
+
+-- migrate:up
+alter table events
+add index if not exists attribute_device_manufacturer_idx `attribute.device_manufacturer` type minmax granularity 2 after attribute_device_name_idx;
+
+-- migrate:down
+alter table events
+drop index if exists attribute_device_manufacturer_idx;
