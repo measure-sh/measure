b5693958565116ab3aa58cfdb01a4f4c9372907d
detj <detj@users.noreply.github.com>
2026-01-15 14:54:06 +0530

    chore(backend): migrate index changes synchronously
    Signed-off-by: detj <detj@users.noreply.github.com>

--- self-host/clickhouse/20260114194938_alter_events_table.sql
+++ self-host/clickhouse/20260114194938_alter_events_table.sql
@@ -1,10 +1,12 @@
 -- migrate:up
 alter table events
-drop index if exists attribute_device_manufacturer_idx;
+drop index if exists attribute_device_manufacturer_idx
+settings mutations_sync = 2;
 
 -- migrate:down
 alter table events
-add index if not exists attribute_device_manufacturer_idx  `attribute.device_manufacturer` TYPE minmax GRANULARITY 2 after attribute_device_name_idx;
+add index if not exists attribute_device_manufacturer_idx  `attribute.device_manufacturer` TYPE minmax GRANULARITY 2 after attribute_device_name_idx
+settings mutations_sync = 2;
 
 -- migrate:up
 alter table events
@@ -15,8 +17,10 @@ select 1;
 
 -- migrate:up
 alter table events
-add index if not exists attribute_device_manufacturer_idx `attribute.device_manufacturer` type minmax granularity 2 after attribute_device_name_idx;
+add index if not exists attribute_device_manufacturer_idx `attribute.device_manufacturer` type minmax granularity 2 after attribute_device_name_idx
+settings mutations_sync = 2;
 
 -- migrate:down
 alter table events
-drop index if exists attribute_device_manufacturer_idx;
+drop index if exists attribute_device_manufacturer_idx
+settings mutations_sync = 2;
