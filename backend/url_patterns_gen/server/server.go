ea35ff26c4d6f2e819ff464ad037334a4c5afde7
Abhay Sood <abhay@measure.sh>
2026-02-11 01:38:19 +0530

    chore(backend): add url_patterns_gen service scaffold
    Add a new cron-based service that runs every hour for URL patterns
    generation. Includes Postgres/ClickHouse connectivity, health check
    endpoint, and compose configuration on port 8085.

---
+++ backend/url_patterns_gen/server/server.go
@@ -0,0 +1,165 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"cloud.google.com/go/cloudsqlconn"
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/leporo/sqlf"
+)
+
+var Server *server
+
+type server struct {
+	PgPool  *pgxpool.Pool
+	ChPool  driver.Conn
+	RchPool driver.Conn
+	Config  *ServerConfig
+}
+
+type PostgresConfig struct {
+	/* connection string of the postgres instance */
+	DSN string
+}
+
+type ClickhouseConfig struct {
+	/* connection string of the clickhouse instance */
+	DSN       string
+	ReaderDSN string
+}
+
+type ServerConfig struct {
+	PG              PostgresConfig
+	CH              ClickhouseConfig
+	OtelServiceName string
+	CloudEnv        bool
+}
+
+// IsCloud is true if the service is assumed
+// running on a cloud environment.
+func (sc *ServerConfig) IsCloud() bool {
+	if sc.CloudEnv {
+		return true
+	}
+
+	return false
+}
+
+func NewConfig() *ServerConfig {
+	cloudEnv := false
+	if os.Getenv("K_SERVICE") != "" && os.Getenv("K_REVISION") != "" {
+		cloudEnv = true
+	}
+
+	postgresDSN := os.Getenv("POSTGRES_DSN")
+	if postgresDSN == "" {
+		log.Println("POSTGRES_DSN env var is not set, cannot start server")
+	}
+
+	clickhouseDSN := os.Getenv("CLICKHOUSE_DSN")
+	if clickhouseDSN == "" {
+		log.Println("CLICKHOUSE_DSN env var is not set, cannot start server")
+	}
+
+	clickhouseReaderDSN := os.Getenv("CLICKHOUSE_READER_DSN")
+	if clickhouseReaderDSN == "" {
+		log.Println("CLICKHOUSE_READER_DSN env var is not set, cannot start server")
+	}
+
+	otelServiceName := os.Getenv("OTEL_SERVICE_NAME")
+	if otelServiceName == "" {
+		log.Println("OTEL_SERVICE_NAME env var is not set, o11y will not work")
+	}
+
+	return &ServerConfig{
+		PG: PostgresConfig{
+			DSN: postgresDSN,
+		},
+		CH: ClickhouseConfig{
+			DSN:       clickhouseDSN,
+			ReaderDSN: clickhouseReaderDSN,
+		},
+		OtelServiceName: otelServiceName,
+		CloudEnv:        cloudEnv,
+	}
+}
+
+func Init(config *ServerConfig) {
+	ctx := context.Background()
+	var pgPool *pgxpool.Pool
+
+	// read/write pool
+	oConfig, err := pgxpool.ParseConfig(config.PG.DSN)
+	if err != nil {
+		log.Printf("Unable to parse postgres connection string: %v\n", err)
+	}
+
+	// See https://pkg.go.dev/github.com/jackc/pgx/v5#QueryExecMode
+	oConfig.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
+
+	if config.IsCloud() {
+		d, err := cloudsqlconn.NewDialer(ctx,
+			// Always use IAM authentication.
+			cloudsqlconn.WithIAMAuthN(),
+			// In Cloud Run CPU is throttled outside of a request
+			// context causing the backend refresh to fail, hence
+			// the need for `WithLazyRefresh()` option.
+			cloudsqlconn.WithLazyRefresh(),
+		)
+		if err != nil {
+			fmt.Println("Failed to dial postgres connection.")
+		}
+
+		csqlConnName := os.Getenv("CSQL_CONN_NAME")
+		if csqlConnName == "" {
+			fmt.Println("CSQL_CONN_NAME environment variable is not set.")
+		}
+
+		oConfig.ConnConfig.DialFunc = func(ctx context.Context, network string, address string) (net.Conn, error) {
+			fmt.Printf("Dialing network: %s, address: %s\n", network, address)
+			return d.Dial(ctx, csqlConnName, cloudsqlconn.WithPrivateIP())
+		}
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, oConfig)
+	if err != nil {
+		log.Printf("Unable to create PG connection pool: %v\n", err)
+	}
+	pgPool = pool
+
+	chOpts, err := clickhouse.ParseDSN(config.CH.DSN)
+	if err != nil {
+		log.Printf("Unable to parse CH connection string: %v\n", err)
+	}
+
+	rChOpts, err := clickhouse.ParseDSN(config.CH.ReaderDSN)
+	if err != nil {
+		log.Printf("unable to parse reader CH connection string %v\n", err)
+	}
+
+	chPool, err := clickhouse.Open(chOpts)
+	if err != nil {
+		log.Printf("Unable to create CH connection pool: %v\n", err)
+	}
+
+	rChPool, err := clickhouse.Open(rChOpts)
+	if err != nil {
+		log.Printf("Unable to create reader CH connection pool: %v\n", err)
+	}
+
+	sqlf.SetDialect(sqlf.PostgreSQL)
+
+	Server = &server{
+		PgPool:  pgPool,
+		ChPool:  chPool,
+		RchPool: rChPool,
+		Config:  config,
+	}
+}
---
+++ backend/url_patterns_gen/urlpatterns/urlpatterns.go
@@ -0,0 +1,14 @@
+package urlpatterns
+
+import (
+	"context"
+	"fmt"
+)
+
+func Generate(ctx context.Context) {
+	fmt.Println("Starting URL patterns generation...")
+
+	// TODO: implement URL patterns generation logic
+
+	fmt.Println("Finished URL patterns generation")
+}
