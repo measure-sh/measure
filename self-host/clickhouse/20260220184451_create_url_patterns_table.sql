a97cdaa5631d6225fb349a078a8233d21afa811b
Abhay Sood <abhay@measure.sh>
2026-02-23 10:15:02 +0530

    chore(backend): wip

--- self-host/clickhouse/20260220184451_create_url_patterns_table.sql
+++ self-host/clickhouse/20260220184451_create_url_patterns_table.sql
@@ -1,15 +1,15 @@
 -- migrate:up
 CREATE TABLE url_patterns
 (
-    `team_id` UUID,
-    `app_id` UUID,
-    `domain` String,
-    `path` String,
-    `last_accessed_at` DateTime
+    `team_id` UUID comment 'The team this URL pattern belongs to',
+    `app_id` UUID comment 'The app this URL pattern belongs to',
+    `domain` String comment 'The domain of the URL pattern, e.g. "example.com"',
+    `path` String comment 'The path of the URL pattern, e.g. "/users/*/profile"',
+    `last_updated_at` DateTime comment 'The last time this URL pattern was last updated'
 )
-ENGINE = ReplacingMergeTree(last_accessed_at)
+ENGINE = ReplacingMergeTree(last_updated_at)
 ORDER BY (team_id, app_id, domain, path)
-TTL last_accessed_at + INTERVAL 30 DAY
+TTL last_updated_at + INTERVAL 30 DAY
 
 -- migrate:down
 DROP TABLE IF EXISTS url_patterns
