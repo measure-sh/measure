4d6e4622e6757e0457a6625acc40ce76c1437bba
detj <detj@users.noreply.github.com>
2026-03-11 22:41:12 +0530

    chore(backend): update sessions reconciliation migration
    Signed-off-by: detj <detj@users.noreply.github.com>

--- self-host/clickhouse/20260309084443_alter_sessions_table.sql
+++ self-host/clickhouse/20260309084443_alter_sessions_table.sql
@@ -2,6 +2,8 @@
 alter table sessions
     drop index if exists user_id_bloom_idx,
     drop index if exists team_id_idx,
+    drop index if exists crash_count_minmax_idx,
+    drop index if exists anr_count_minmax_idx,
     drop column if exists start_time,
     drop column if exists end_time,
     drop column if exists user_id;
@@ -12,7 +14,9 @@ alter table sessions
     add column if not exists start_time SimpleAggregateFunction(min, DateTime64(9, 'UTC')) CODEC(DoubleDelta, ZSTD(3)) after last_event_timestamp,
     add column if not exists end_time SimpleAggregateFunction(max, DateTime64(9, 'UTC')) CODEC(DoubleDelta, ZSTD(3)) after start_time,
     add index if not exists team_id_idx `team_id` type bloom_filter(0.025) granularity 8,
-    add index if not exists user_id_bloom_idx `user_id` type bloom_filter granularity 2;
+    add index if not exists user_id_bloom_idx `user_id` type bloom_filter granularity 2,
+    add index if not exists crash_count_minmax_idx crash_count type minmax granularity 1,
+    add index if not exists anr_count_minmax_idx anr_count type minmax granularity 1;
 
 -- migrate:up
 alter table sessions
