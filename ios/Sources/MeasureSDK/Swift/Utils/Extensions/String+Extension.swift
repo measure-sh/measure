1cef4e99bbbe7d0d8e0354aa9cbfdcfff731c458
Adwin Ross <adwinross@gmail.com>
2026-03-23 13:23:27 +0530

    chore(ios): add correct url host to http events

--- ios/Sources/MeasureSDK/Swift/Utils/Extensions/String+Extension.swift
+++ ios/Sources/MeasureSDK/Swift/Utils/Extensions/String+Extension.swift
@@ -8,11 +8,6 @@
 import Foundation
 
 extension String {
-    func removeHttpPrefix() -> String {
-        return self.replacingOccurrences(of: "https://", with: "")
-                   .replacingOccurrences(of: "http://", with: "")
-    }
-
     func sanitizeRequestBody() -> String {
         return self.replacingOccurrences(of: "\r", with: "")
                    .replacingOccurrences(of: "\n", with: "")
