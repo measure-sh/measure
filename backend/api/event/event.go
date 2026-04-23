cf8b1668406e1eb388b92731b83468e18e6c46a8
detj <detj@users.noreply.github.com>
2026-04-23 14:02:09 +0530

    chore(backend): log correctly
    Signed-off-by: detj <detj@users.noreply.github.com>

--- backend/api/event/event.go
+++ backend/api/event/event.go
@@ -1345,6 +1345,10 @@ func (e EventField) HasAttachments() bool {
 // GetFramework returns the exception framework
 // in a backwards compatible way.
 func (e Exception) GetFramework() (f string) {
+	fmt.Println("len", len(e.Exceptions))
+	jsonBytes, _ := json.Marshal(e.Exceptions)
+	fmt.Println("ios exceptions")
+	fmt.Println(string(jsonBytes))
 	// if we have the framework, just use it
 	// no need to infer
 	// could be "dart"
@@ -1354,10 +1358,6 @@ func (e Exception) GetFramework() (f string) {
 
 	// Apple exception or error or error with exception
 	if e.HasExceptions() {
-		fmt.Println("len", len(e.Exceptions))
-		jsonBytes, _ := json.Marshal(e.Exceptions)
-		fmt.Println("ios exceptions")
-		fmt.Println(string(jsonBytes))
 		if e.Exceptions[0].ExceptionUnitiOS != nil && e.Exceptions[0].Signal != "" {
 			return FrameworkApple
 		}
