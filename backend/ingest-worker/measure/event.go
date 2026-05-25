8b992ebf42d699c9a7b75225f0e5d2b108c901c1
detj <detj@users.noreply.github.com>
2026-05-25 22:06:58 +0530

    fix(backend): handled exceptions were incorrectly grouped
    fixes #3707
    
    Signed-off-by: detj <detj@users.noreply.github.com>

--- backend/ingest-worker/measure/event.go
+++ backend/ingest-worker/measure/event.go
@@ -282,7 +282,7 @@ func (e eventreq) bucketExceptions(ctx context.Context) (err error) {
 			events[i].Exception.GetMethodName(),
 			events[i].Exception.GetFileName(),
 			events[i].Exception.GetLineNumber(),
-			events[i].Exception.Handled,
+			events[i].Exception.GetSeverity() == event.SeverityHandled,
 			events[i].Exception.GetSeverity() == event.SeverityFatal,
 			events[i].Exception.IsCustom,
 			events[i].Timestamp,
