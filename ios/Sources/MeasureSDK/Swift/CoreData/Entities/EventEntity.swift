a84ad6b3a2fa842e1663f20ba35c345c815432ba
Adwin Ronald Ross <adwinross@gmail.com>
2026-07-22 11:05:04 +0530

    chore(ios): reuse shared encoder and decoder (#4101)

--- ios/Sources/MeasureSDK/Swift/CoreData/Entities/EventEntity.swift
+++ ios/Sources/MeasureSDK/Swift/CoreData/Entities/EventEntity.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct EventEntity { // swiftlint:disable:this type_body_length
+struct EventEntity {
     let id: String
     let sessionId: String
     let timestamp: String
@@ -39,7 +39,27 @@ struct EventEntity { // swiftlint:disable:this type_body_length
     let sessionStartData: Data?
     let log: Data?
 
-    init<T: Codable>(_ event: Event<T>, needsReporting: Bool) { // swiftlint:disable:this cyclomatic_complexity function_body_length
+    private static let encoder = JSONEncoder()
+    private static func encode<V: Encodable>(_ value: V?) -> Data? {
+        guard let value else { return nil }
+        do {
+            return try encoder.encode(value)
+        } catch {
+            return nil
+        }
+    }
+
+    private static let decoder = JSONDecoder()
+    private static func decode<V: Decodable>(_ type: V.Type, from data: Data?) -> V? {
+        guard let data else { return nil }
+        do {
+            return try decoder.decode(type, from: data)
+        } catch {
+            return nil
+        }
+    }
+
+    init<T: Codable>(_ event: Event<T>, needsReporting: Bool) {
         self.id = event.id
         self.sessionId = event.sessionId
         self.timestamp = event.timestamp
@@ -49,228 +69,28 @@ struct EventEntity { // swiftlint:disable:this type_body_length
         self.batchId = nil
         self.userDefinedAttributes = event.userDefinedAttributes
         self.needsReporting = needsReporting
-
-        if let exception = event.exception {
-            do {
-                let data = try JSONEncoder().encode(exception)
-                self.exception = data
-            } catch {
-                self.exception = nil
-            }
-        } else {
-            self.exception = nil
-        }
-
-        if let gestureClick = event.gestureClick {
-            do {
-                let data = try JSONEncoder().encode(gestureClick)
-                self.gestureClick = data
-            } catch {
-                self.gestureClick = nil
-            }
-        } else {
-            self.gestureClick = nil
-        }
-
-        if let gestureLongClick = event.gestureLongClick {
-            do {
-                let data = try JSONEncoder().encode(gestureLongClick)
-                self.gestureLongClick = data
-            } catch {
-                self.gestureLongClick = nil
-            }
-        } else {
-            self.gestureLongClick = nil
-        }
-
-        if let gestureScroll = event.gestureScroll {
-            do {
-                let data = try JSONEncoder().encode(gestureScroll)
-                self.gestureScroll = data
-            } catch {
-                self.gestureScroll = nil
-            }
-        } else {
-            self.gestureScroll = nil
-        }
-
-        if let lifecycleApp = event.lifecycleApp {
-            do {
-                let data = try JSONEncoder().encode(lifecycleApp)
-                self.lifecycleApp = data
-            } catch {
-                self.lifecycleApp = nil
-            }
-        } else {
-            self.lifecycleApp = nil
-        }
-
-        if let lifecycleViewController = event.lifecycleViewController {
-            do {
-                let data = try JSONEncoder().encode(lifecycleViewController)
-                self.lifecycleViewController = data
-            } catch {
-                self.lifecycleViewController = nil
-            }
-        } else {
-            self.lifecycleViewController = nil
-        }
-
-        if let lifecycleSwiftUI = event.lifecycleSwiftUI {
-            do {
-                let data = try JSONEncoder().encode(lifecycleSwiftUI)
-                self.lifecycleSwiftUI = data
-            } catch {
-                self.lifecycleSwiftUI = nil
-            }
-        } else {
-            self.lifecycleSwiftUI = nil
-        }
-
-        if let cpuUsage = event.cpuUsage {
-            do {
-                let data = try JSONEncoder().encode(cpuUsage)
-                self.cpuUsage = data
-            } catch {
-                self.cpuUsage = nil
-            }
-        } else {
-            self.cpuUsage = nil
-        }
-
-        if let memoryUsage = event.memoryUsageAbsolute {
-            do {
-                let data = try JSONEncoder().encode(memoryUsage)
-                self.memoryUsage = data
-            } catch {
-                self.memoryUsage = nil
-            }
-        } else {
-            self.memoryUsage = nil
-        }
-
-        if let coldLaunch = event.coldLaunch {
-            do {
-                let data = try JSONEncoder().encode(coldLaunch)
-                self.coldLaunch = data
-            } catch {
-                self.coldLaunch = nil
-            }
-        } else {
-            self.coldLaunch = nil
-        }
-
-        if let warmLaunch = event.warmLaunch {
-            do {
-                let data = try JSONEncoder().encode(warmLaunch)
-                self.warmLaunch = data
-            } catch {
-                self.warmLaunch = nil
-            }
-        } else {
-            self.warmLaunch = nil
-        }
-
-        if let hotLaunch = event.hotLaunch {
-            do {
-                let data = try JSONEncoder().encode(hotLaunch)
-                self.hotLaunch = data
-            } catch {
-                self.hotLaunch = nil
-            }
-        } else {
-            self.hotLaunch = nil
-        }
-
-        if let http = event.http {
-            do {
-                let data = try JSONEncoder().encode(http)
-                self.http = data
-            } catch {
-                self.http = nil
-            }
-        } else {
-            self.http = nil
-        }
-
-        if let networkChange = event.networkChange {
-            do {
-                let data = try JSONEncoder().encode(networkChange)
-                self.networkChange = data
-            } catch {
-                self.networkChange = nil
-            }
-        } else {
-            self.networkChange = nil
-        }
-
-        if let customEvent = event.custom {
-            do {
-                let data = try JSONEncoder().encode(customEvent)
-                self.customEvent = data
-            } catch {
-                self.customEvent = nil
-            }
-        } else {
-            self.customEvent = nil
-        }
-
-        if let screenView = event.screenView {
-            do {
-                let data = try JSONEncoder().encode(screenView)
-                self.screenView = data
-            } catch {
-                self.screenView = nil
-            }
-        } else {
-            self.screenView = nil
-        }
-
-        if let attributes = event.attributes {
-            do {
-                let data = try JSONEncoder().encode(attributes)
-                self.attributes = data
-            } catch {
-                self.attributes = nil
-            }
-        } else {
-            self.attributes = nil
-        }
-
         self.attachments = event.attachments
 
-        if let bugReport = event.bugReport {
-            do {
-                let data = try JSONEncoder().encode(bugReport)
-                self.bugReport = data
-            } catch {
-                self.bugReport = nil
-            }
-        } else {
-            self.bugReport = nil
-        }
-
-        if event.type == .sessionStart {
-            do {
-                let data = try JSONEncoder().encode(SessionStartData())
-                self.sessionStartData = data
-            } catch {
-                self.sessionStartData = nil
-            }
-        } else {
-            self.sessionStartData = nil
-        }
-
-        if let log = event.log {
-            do {
-                let data = try JSONEncoder().encode(log)
-                self.log = data
-            } catch {
-                self.log = nil
-            }
-        } else {
-            self.log = nil
-        }
+        self.exception = Self.encode(event.exception)
+        self.gestureClick = Self.encode(event.gestureClick)
+        self.gestureLongClick = Self.encode(event.gestureLongClick)
+        self.gestureScroll = Self.encode(event.gestureScroll)
+        self.lifecycleApp = Self.encode(event.lifecycleApp)
+        self.lifecycleViewController = Self.encode(event.lifecycleViewController)
+        self.lifecycleSwiftUI = Self.encode(event.lifecycleSwiftUI)
+        self.cpuUsage = Self.encode(event.cpuUsage)
+        self.memoryUsage = Self.encode(event.memoryUsageAbsolute)
+        self.coldLaunch = Self.encode(event.coldLaunch)
+        self.warmLaunch = Self.encode(event.warmLaunch)
+        self.hotLaunch = Self.encode(event.hotLaunch)
+        self.http = Self.encode(event.http)
+        self.networkChange = Self.encode(event.networkChange)
+        self.customEvent = Self.encode(event.custom)
+        self.screenView = Self.encode(event.screenView)
+        self.attributes = Self.encode(event.attributes)
+        self.bugReport = Self.encode(event.bugReport)
+        self.sessionStartData = event.type == .sessionStart ? Self.encode(SessionStartData()) : nil
+        self.log = Self.encode(event.log)
     }
 
     init(id: String,
@@ -335,176 +155,35 @@ struct EventEntity { // swiftlint:disable:this type_body_length
         self.log = log
     }
 
-    func getEvent<T: Codable>() -> Event<T> { // swiftlint:disable:this cyclomatic_complexity function_body_length
-        var decodedData: T?
-        let eventType = EventType(rawValue: type)
-        switch eventType {
-        case .exception:
-            if let exceptionData = self.exception {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: exceptionData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .gestureClick:
-            if let gestureClickData = self.gestureClick {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: gestureClickData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .gestureLongClick:
-            if let gestureLongClickData = self.gestureLongClick {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: gestureLongClickData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .gestureScroll:
-            if let gestureScrollData = self.gestureScroll {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: gestureScrollData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .lifecycleApp:
-            if let lifecycleAppData = self.lifecycleApp {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: lifecycleAppData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .lifecycleViewController:
-            if let lifecycleViewControllerData = self.lifecycleViewController {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: lifecycleViewControllerData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .lifecycleSwiftUI:
-            if let lifecycleSwiftUIData = self.lifecycleSwiftUI {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: lifecycleSwiftUIData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .cpuUsage:
-            if let cpuUsageData = self.cpuUsage {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: cpuUsageData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .memoryUsageAbsolute:
-            if let memoryUsageData = self.memoryUsage {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: memoryUsageData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .coldLaunch:
-            if let coldLaunchData = self.coldLaunch {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: coldLaunchData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .warmLaunch:
-            if let warmLaunchData = self.warmLaunch {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: warmLaunchData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .hotLaunch:
-            if let hotLaunchData = self.hotLaunch {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: hotLaunchData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .http:
-            if let httpData = self.http {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: httpData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .networkChange:
-            if let networkChangeData = self.networkChange {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: networkChangeData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .custom:
-            if let customEventData = self.customEvent {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: customEventData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .log:
-            if let logData = self.log {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: logData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .screenView:
-            if let screenViewData = self.screenView {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: screenViewData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .bugReport:
-            if let bugReportData = self.bugReport {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: bugReportData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case .sessionStart:
-            if let sessionStartData = self.sessionStartData {
-                do {
-                    decodedData = try JSONDecoder().decode(T.self, from: sessionStartData)
-                } catch {
-                    decodedData = nil
-                }
-            }
-        case nil:
-            decodedData = nil
+    /// Returns the encoded payload `Data` for the event's `type`, or `nil` for an unknown type.
+    private var payloadData: Data? {
+        switch EventType(rawValue: type) {
+        case .exception: return exception
+        case .gestureClick: return gestureClick
+        case .gestureLongClick: return gestureLongClick
+        case .gestureScroll: return gestureScroll
+        case .lifecycleApp: return lifecycleApp
+        case .lifecycleViewController: return lifecycleViewController
+        case .lifecycleSwiftUI: return lifecycleSwiftUI
+        case .cpuUsage: return cpuUsage
+        case .memoryUsageAbsolute: return memoryUsage
+        case .coldLaunch: return coldLaunch
+        case .warmLaunch: return warmLaunch
+        case .hotLaunch: return hotLaunch
+        case .http: return http
+        case .networkChange: return networkChange
+        case .custom: return customEvent
+        case .log: return log
+        case .screenView: return screenView
+        case .bugReport: return bugReport
+        case .sessionStart: return sessionStartData
+        case nil: return nil
         }
+    }
 
-        let decodedAttributes: Attributes?
-        if let attributeData = self.attributes {
-            do {
-                decodedAttributes = try JSONDecoder().decode(Attributes.self, from: attributeData)
-            } catch {
-                decodedAttributes = nil
-            }
-        } else {
-            decodedAttributes = nil
-        }
+    func getEvent<T: Codable>() -> Event<T> {
+        let decodedData = Self.decode(T.self, from: payloadData)
+        let decodedAttributes = Self.decode(Attributes.self, from: self.attributes)
 
         return Event(id: self.id,
                      sessionId: self.sessionId,
@@ -517,4 +196,4 @@ struct EventEntity { // swiftlint:disable:this type_body_length
                      userTriggered: self.userTriggered,
                      userDefinedAttributes: self.userDefinedAttributes)
     }
-} // swiftlint:disable:this file_length
+}
