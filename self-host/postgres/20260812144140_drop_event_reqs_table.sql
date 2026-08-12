5b6d104e310612d5f843bdb67872483d936c3d38
Debjeet Biswas <detj@users.noreply.github.com>
2026-08-12 20:31:21 +0530

    chore(backend): drop unused postgres tables (#4232)
    - drop `measure.user_attribution` table
    - drop `measure.event_reqs` table

---
+++ self-host/postgres/20260812144140_drop_event_reqs_table.sql
@@ -0,0 +1,27 @@
+-- migrate:up
+drop table if exists measure.event_reqs;
+
+-- migrate:down
+create table if not exists measure.event_reqs (
+    id uuid primary key not null,
+    app_id uuid references measure.apps(id) on delete cascade,
+    event_count int default 0,
+    attachment_count int default 0,
+    session_count int default 0,
+    bytes_in int default 0,
+    symbolication_attempts_count int default 0,
+    created_at timestamptz not null default now(),
+    status int default 0,
+    span_count int default 0
+);
+
+comment on column measure.event_reqs.id is 'id of the event request';
+comment on column measure.event_reqs.app_id is 'id of the associated app';
+comment on column measure.event_reqs.event_count is 'number of events in the event request';
+comment on column measure.event_reqs.attachment_count is 'number of attachments in the event request';
+comment on column measure.event_reqs.session_count is 'number of sessions in the event request';
+comment on column measure.event_reqs.bytes_in is 'total payload size of the request';
+comment on column measure.event_reqs.symbolication_attempts_count is 'number of times symbolication was attempted for this event request';
+comment on column measure.event_reqs.created_at is 'utc timestamp at the time of record creation';
+comment on column measure.event_reqs.status is 'status of event request: 0 is pending, 1 is done';
+comment on column measure.event_reqs.span_count is 'number of spans in the event request';
