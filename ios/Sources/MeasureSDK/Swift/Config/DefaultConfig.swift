f109c5b0b8916b1588e0978a93aa3e405a4aea0c
Abhay Sood <abhay@measure.sh>
2026-09-23 10:19:55 +0530

    chore(ios): default memory sampling to 0.01%

--- ios/Sources/MeasureSDK/Swift/Config/DefaultConfig.swift
+++ ios/Sources/MeasureSDK/Swift/Config/DefaultConfig.swift
@@ -35,7 +35,7 @@ struct DefaultConfig {
     static let logIgnorePatterns: [String] = []
     static let cpuUsageInterval: Number = 5
     static let memoryUsageInterval: Number = 5
-    static let memoryUsageSessionSamplingRate: Float = 100
+    static let memoryUsageSessionSamplingRate: Float = 0.01
     static let errorFatalTakeScreenshot: Bool = true
     static let errorFatalReplayEnabled: Bool = true
     static let errorUnhandledReplayEnabled: Bool = false
