261b5921aba56b07f0312018f3fa0feaa04fcb2b
Abhay Sood <abhay@measure.sh>
2026-09-25 16:29:57 +0530

    chore(frontend): remove unused beta badge popup

--- frontend/dashboard/app/components/beta_badge.tsx
+++ frontend/dashboard/app/components/beta_badge.tsx
@@ -1,23 +1,11 @@
 import { Badge } from "@/app/components/badge";
-import SimpleTooltip from "@/app/components/simple_tooltip";
-import { cn } from "@/app/utils/shadcn_utils";
-import { ReactNode } from "react";
 
-interface BetaBadgeProps {
-  popup?: ReactNode;
-}
-
-export default function BetaBadge({ popup }: BetaBadgeProps) {
+export default function BetaBadge() {
   return (
-    <SimpleTooltip content={popup ? <div className="p-2">{popup}</div> : null}>
-      <sup>
-        <Badge
-          variant="outline"
-          className={cn("select-none", popup && "cursor-default")}
-        >
-          Beta
-        </Badge>
-      </sup>
-    </SimpleTooltip>
+    <sup>
+      <Badge variant="outline" className="select-none">
+        Beta
+      </Badge>
+    </sup>
   );
 }
