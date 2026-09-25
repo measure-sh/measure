d38112deb6d1e6c0fed9f9f7b9daef87476fcee6
Anup Cowkur <anupcowkur89@gmail.com>
2026-09-25 13:44:50 +0530

    chore(frontend): hide divider under expanded stack traces

--- frontend/dashboard/app/components/errors_details.tsx
+++ frontend/dashboard/app/components/errors_details.tsx
@@ -474,6 +474,7 @@ export const ErrorsDetailsView: React.FC<ErrorsDetailsViewProps> = ({
                   {stacktrace && (
                     <AccordionItem
                       value={"Thread: " + firstResult.attribute.thread_name}
+                      className="data-[state=open]:border-b-0"
                     >
                       <AccordionTrigger className="font-display">
                         {"Thread: " + firstResult.attribute.thread_name}
@@ -491,6 +492,7 @@ export const ErrorsDetailsView: React.FC<ErrorsDetailsViewProps> = ({
                     <AccordionItem
                       value={`${e.name}-${index}`}
                       key={`${e.name}-${index}`}
+                      className="data-[state=open]:border-b-0"
                     >
                       <AccordionTrigger className="font-display">
                         {"Thread: " + e.name}
